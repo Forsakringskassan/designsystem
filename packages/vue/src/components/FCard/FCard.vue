d75702f91a98e60bd31028a26f29306663cf3d67
KrQnan <kevin.ramlinger@forsakringskassan.se>
2024-10-09 09:23:02 +0200

    refactor: converted FCard to Composition API (refs SFKUI-6500)

--- packages/vue/src/components/FCard/FCard.vue
+++ packages/vue/src/components/FCard/FCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card card--default">
-        <div v-if="hasHeaderSlot" class="card__header">
+        <div v-if="$slots.header" class="card__header">
             <!--@slot Slot for the title. -->
             <slot name="header" v-bind="{ headingSlotClass: headingClass }"></slot>
         </div>
@@ -8,34 +8,15 @@
             <!--@slot Slot for the main content, e.g. paragraphs, input fields, etc. -->
             <slot name="default"></slot>
         </div>
-        <div v-if="hasFooterSlot" class="card__footer">
+        <div v-if="$slots.footer" class="card__footer">
             <!--@slot Slot the footer content, i.e. buttons. -->
             <slot name="footer"></slot>
         </div>
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import { hasSlot } from "../../utils";
+<script setup lang="ts">
+import { ref } from "vue";
 
-/**
- * Level: Beta
- */
-export default defineComponent({
-    name: "FCard",
-    data() {
-        return {
-            headingClass: ["card__header-label"],
-        };
-    },
-    computed: {
-        hasHeaderSlot(): boolean {
-            return hasSlot(this, "header");
-        },
-        hasFooterSlot(): boolean {
-            return hasSlot(this, "footer");
-        },
-    },
-});
+const headingClass = ref(["card__header-label"]);
 </script>
