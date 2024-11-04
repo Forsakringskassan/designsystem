ec868cdcba34f029c93d3a2d5e63236f2f7dc69f
David Sveningsson <david.sveningsson@forsakringskassan.se>
2024-11-05 00:34:40 +0100

    chore(deps): update @forsakringskassan/eslint monorepo to v11.4.6

--- packages/logic/src/types/PartialRecord.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @public
- */
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any -- technical
- * debt, should probably use keyof T or at least unknown, or try to migrate the
- * entire type to the builtin Partial */
-export type PartialRecord<K extends keyof any, T> = {
-    [P in K]?: T;
-};
