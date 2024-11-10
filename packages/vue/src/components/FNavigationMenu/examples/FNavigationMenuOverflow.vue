0c5578529fa567c927d5766a74ea3ddba7d38635
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2024-11-10 17:04:59 +0100

    docs: add live example to `FNavigationMenu` (refs SFKUI-6609)

--- packages/vue/src/components/FNavigationMenu/examples/FNavigationMenuOverflow.vue
+++ packages/vue/src/components/FNavigationMenu/examples/FNavigationMenuOverflow.vue
@@ -1,29 +1,8 @@
 <template>
-    <div>
-        <pre>selectedRoute: {{ selectedRoute }}</pre>
-        <f-navigation-menu
-            :routes="routes"
-            @selected-route="selectedRoute = $event"
-        ></f-navigation-menu>
-    </div>
+    <f-navigation-menu :routes="routes"></f-navigation-menu>
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup>
 import { FNavigationMenu } from "@fkui/vue";
-/* same routes as used by vue-router */
 import { routes } from "./router";
-
-export default defineComponent({
-    name: "FNavigationMenuExample",
-    components: { FNavigationMenu },
-    props: {},
-    data: () => {
-        return {
-            routes: routes,
-            selectedRoute: "",
-        };
-    },
-    methods: {},
-});
 </script>
