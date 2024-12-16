c08d5839539904bc9ec14fb4513ddd25ce4d0ee8
David Sveningsson <david.sveningsson@forsakringskassan.se>
2024-12-16 08:27:18 +0100

    feat: misc

--- internal/vue-sandbox/src/App.vue
+++ internal/vue-sandbox/src/App.vue
@@ -19,10 +19,12 @@ const layoutName = ref("simple");
 
 const leftOpen = ref(true);
 const leftVariant = ref<"static" | "toggle" | "expand">("static");
+const leftType = ref<"none" | "flat" | "layer" | "custom">("custom");
 const leftFlags = ref<"none" | "overlay" | "resizable">("none");
 
 const rightOpen = ref(true);
 const rightVariant = ref<"static" | "toggle" | "expand">("static");
+const rightType = ref<"none" | "flat" | "layer" | "custom">("custom");
 const rightFlags = ref<"none" | "overlay" | "resizable">("none");
 
 const right2Open = ref(true);
@@ -75,6 +77,8 @@ const layout = computed(() => {
     }
 });
 
+const lastSizes = ref<Record<string, number>>({});
+
 function maybeOverlay(sizes: Record<string, number>): {
     variant?: "static" | "toggle" | "expand";
     overlay?: boolean;
@@ -82,10 +86,11 @@ function maybeOverlay(sizes: Record<string, number>): {
 } {
     const { body, left = 0, right = 0 } = sizes;
     const width = body - left - right;
+    lastSizes.value = { body, left, right, width };
     if (body < 500) {
         return { variant: "toggle", fullscreen: true };
     }
-    if (width < 500) {
+    if (width < 600) {
         return { variant: "expand", overlay: true };
     }
     return {};
@@ -108,12 +113,17 @@ function maybeOverlay(sizes: Record<string, number>): {
             <f-panel
                 :variant="leftVariant"
                 v-model="leftOpen"
-                class="my-left-panel"
+                :class="leftType === 'custom' ? 'my-left-panel' : undefined"
+                :type="leftType !== 'custom' ? leftType : undefined"
                 :overlay="leftFlags === 'overlay'"
                 :resizable="leftFlags === 'resizable'"
                 :behaviour="leftFlags === 'responsive' ? maybeOverlay : undefined"
             >
-                left
+                <template #header> Panel header </template>
+
+                <template #default> Panel body </template>
+
+                <template #footer> Panel footer </template>
             </f-panel>
         </template>
 
@@ -121,7 +131,8 @@ function maybeOverlay(sizes: Record<string, number>): {
             <f-panel
                 :variant="rightVariant"
                 v-model="rightOpen"
-                class="my-right-panel"
+                :class="rightType === 'custom' ? 'my-right-panel' : undefined"
+                :type="rightType !== 'custom' ? rightType : undefined"
                 :overlay="rightFlags === 'overlay'"
                 :resizable="rightFlags === 'resizable'"
                 :behaviour="rightFlags === 'responsive' ? maybeOverlay : undefined"
@@ -182,7 +193,7 @@ function maybeOverlay(sizes: Record<string, number>): {
                         <template #label> Left panel </template>
                         <template #default>
                             <div class="row">
-                                <div class="col col--md-6">
+                                <div class="col col--md-4">
                                     <f-select-field v-model="leftVariant">
                                         <template #label> Variant </template>
                                         <template #default>
@@ -192,7 +203,18 @@ function maybeOverlay(sizes: Record<string, number>): {
                                         </template>
                                     </f-select-field>
                                 </div>
-                                <div class="col col--md-6">
+                                <div class="col col--md-4">
+                                    <f-select-field v-model="leftType">
+                                        <template #label> Style </template>
+                                        <template #default>
+                                            <option value="none">None</option>
+                                            <option value="flat">Flat</option>
+                                            <option value="layer">Layer</option>
+                                            <option value="custom">Custom</option>
+                                        </template>
+                                    </f-select-field>
+                                </div>
+                                <div class="col col--md-4">
                                     <f-select-field v-model="leftFlags">
                                         <template #label> Flags </template>
                                         <template #default>
@@ -211,7 +233,7 @@ function maybeOverlay(sizes: Record<string, number>): {
                         <template #label> Right panel </template>
                         <template #default>
                             <div class="row">
-                                <div class="col col--md-6">
+                                <div class="col col--md-4">
                                     <f-select-field v-model="rightVariant">
                                         <template #label> Variant </template>
                                         <template #default>
@@ -221,7 +243,18 @@ function maybeOverlay(sizes: Record<string, number>): {
                                         </template>
                                     </f-select-field>
                                 </div>
-                                <div class="col col--md-6">
+                                <div class="col col--md-4">
+                                    <f-select-field v-model="rightType">
+                                        <template #label> Style </template>
+                                        <template #default>
+                                            <option value="none">None</option>
+                                            <option value="flat">Flat</option>
+                                            <option value="layer">Layer</option>
+                                            <option value="custom">Custom</option>
+                                        </template>
+                                    </f-select-field>
+                                </div>
+                                <div class="col col--md-4">
                                     <f-select-field v-model="rightFlags">
                                         <template #label> Flags </template>
                                         <template #default>
@@ -237,6 +270,29 @@ function maybeOverlay(sizes: Record<string, number>): {
                     </f-fieldset>
 
                     <pre>{{ { leftOpen, leftVariant, leftFlags, rightOpen, rightVariant, rightFlags } }}</pre>
+                    <template v-if="leftFlags === 'responsive' || rightFlags === 'responsive'">
+                        <div class="size-wrapper-wrapper">
+                        <div class="size-wrapper">
+                            <div class="body-size">{{ lastSizes.body }}px</div>
+                            <div class="body-box"></div>
+                            <div class="area-size">
+                                <div class="left-size">{{ lastSizes.left }}px</div>
+                                <div class="content-size"> = {{ lastSizes.width }}px</div>
+                                <div class="right-size">{{ lastSizes.right }}px</div>
+                            </div>
+                            <div class="area-box">
+                                <div class="left-box">left</div>
+                                <div class="content-box">content</div>
+                                <div class="right-box">right</div>
+                            </div>
+                            <div class="area-silly">
+                                <div class="left-silly"></div>
+                                <div class="content-silly"></div>
+                                <div class="right-silly"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </template>
                 </template>
             </main>
         </template>
@@ -246,50 +302,134 @@ function maybeOverlay(sizes: Record<string, number>): {
 </template>
 
 <style>
-html,
-body {
-    margin: 0;
-    padding: 0;
-}
+    html,
+    body {
+        margin: 0;
+        padding: 0;
+    }
 
-.page-layout--foobar {
-    grid-template:
-        "header header header header" min-content
+    .page-layout--foobar {
+        grid-template:
+            "header header header header" min-content
         "left toolbar toolbar toolbar" min-content
         "left content right2 right"
         "footer footer footer footer" min-content
         / min-content 1fr min-content min-content;
-}
+    }
 
-.area-header,
-.area-content,
-.area-toolbar,
-.area-footer {
-    padding: 1rem;
-}
+    .area-header,
+    .area-content,
+    .area-toolbar,
+    .area-footer {
+        padding: 1rem;
+    }
 
-.area-header {
-    background: lightgreen;
-}
+    .area-header {
+        background: lightgreen;
+        z-index: 10;
+    }
 
-.area-toolbar {
-    background: orange;
-    justify-content: space-between;
-}
+    .area-toolbar {
+        background: orange;
+        justify-content: space-between;
+    }
 
-.area-footer {
-    background: lightblue;
-}
+    .area-footer {
+        background: lightblue;
+        z-index: 10;
+    }
 
-.my-left-panel {
-    background: lawngreen;
-}
+    .my-left-panel {
+        background: lawngreen;
+    }
 
-.my-right-panel {
-    background: hotpink;
-}
+    .my-right-panel {
+        background: hotpink;
+    }
 
-.my-extra-right-panel {
-    background: firebrick;
-}
+    .my-extra-right-panel {
+        background: firebrick;
+    }
+
+    .size-wrapper-wrapper {
+        display: flex;
+        justify-content: center;
+    }
+
+    .size-wrapper {
+        width: 25rem;
+    }
+
+    .body-size, .left-size, .content-size, .right-size {
+        text-align: center;
+    }
+
+    .body-box {
+        height: 1rem;
+        border-top: 1px solid black;
+        border-left: 1px solid black;
+        border-right: 1px solid black;
+        margin-bottom: 0.5rem;
+    }
+
+    .area-box, .area-size, .area-silly {
+        display: flex;
+        gap: 0.5rem;
+    }
+
+    .left-box,
+    .content-box,
+    .right-box {
+        height: 3rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .left-silly,
+    .content-silly,
+    .right-silly {
+        height: 2rem;
+    }
+
+    .left-size,
+    .left-box,
+    .left-silly {
+        width: 25%;
+    }
+
+    .left-box {
+        border-top: 1px solid black;
+        border-left: 1px solid black;
+        border-right: 1px solid black;
+    }
+
+    .left-silly, .content-silly, .right-silly {
+        border-left: 1px dashed black;
+        border-right: 1px dashed black;
+    }
+
+    .content-size,
+    .content-box,
+    .content-silly {
+        flex: 1 0 auto;
+    }
+
+    .content-box {
+        border-top: 1px solid black;
+        border-left: 1px solid black;
+        border-right: 1px solid black;
+    }
+
+    .right-size,
+    .right-box,
+    .right-silly {
+        width: 25%;
+    }
+
+    .right-box {
+        border-top: 1px solid black;
+        border-left: 1px solid black;
+        border-right: 1px solid black;
+    }
 </style>
