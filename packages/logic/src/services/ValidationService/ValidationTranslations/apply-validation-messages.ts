66cd2ed5d2b7c2ffbc9f4ec1ff2642edce606aba
David Sveningsson <ext@sidvind.com>
2025-01-31 09:34:26 +0100

    feat!: remove deprecated `applyValidationMessages` function from `@fkui/logic` (refs SFKUI-6963)
    BREAKING CHANGE: The deprecated and obsolete function `applyValidationMessages(..)` has been removed.

--- packages/logic/src/services/ValidationService/ValidationTranslations/apply-validation-messages.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This function does nothing and should not be used.
- *
- * @public
- * @deprecated This function does nothing.
- */
-export function applyValidationMessages(): void {
-    /* do nothing */
-}
