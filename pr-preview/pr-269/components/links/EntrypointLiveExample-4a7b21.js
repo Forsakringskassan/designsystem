e355cb4fdcd1017a8e5d8dfa3e7829f25c078492
NicklasWei <NicklasWei@users.noreply.github.com>
2025-02-10 13:17:06 +0000

    Deploy preview for PR 269 🛫

--- pr-preview/pr-269/components/links/EntrypointLiveExample-4a7b21.js
+++ pr-preview/pr-269/components/links/EntrypointLiveExample-4a7b21.js
@@ -35,24 +35,16 @@
   var import_vue5 = __require("vue");
   var exampleComponent = (0, import_vue3.defineComponent)({
     name: "EntrypointLiveExample",
-    components: { LiveExample: import_docs_live_example.LiveExample, FCheckboxField: import_vue4.FCheckboxField },
-    data() {
-      return {
-        isFullWidth: false
-      };
-    },
+    components: { LiveExample: import_docs_live_example.LiveExample },
     computed: {
       components() {
         return { FIcon: import_vue4.FIcon };
-      },
-      fullwidth() {
-        return this.isFullWidth ? "entrypoint--full-width" : "";
       },
       template() {
         return (
           /* HTML */
           `
-                <a class="entrypoint ${this.fullwidth}" href="javascript:">
+                <a class="entrypoint" href="javascript:">
                     Ans\xF6k om hundbidrag
                     <span class="sr-only"> Till tj\xE4nsten ans\xF6k om hundbidrag</span>
                     <f-icon name="arrow-right" class="entrypoint__icon"></f-icon>
@@ -63,28 +55,11 @@
     }
   });
   function render(_ctx, _cache, $props, $setup, $data, $options) {
-    const _component_f_checkbox_field = (0, import_vue5.resolveComponent)("f-checkbox-field");
     const _component_live_example = (0, import_vue5.resolveComponent)("live-example");
     return (0, import_vue5.openBlock)(), (0, import_vue5.createBlock)(_component_live_example, {
       components: _ctx.components,
       template: _ctx.template
-    }, {
-      default: (0, import_vue5.withCtx)(() => [
-        (0, import_vue5.createVNode)(_component_f_checkbox_field, {
-          modelValue: _ctx.isFullWidth,
-          "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _ctx.isFullWidth = $event),
-          value: true
-        }, {
-          default: (0, import_vue5.withCtx)(() => _cache[1] || (_cache[1] = [
-            (0, import_vue5.createTextVNode)(" Fullbredd ")
-          ])),
-          _: 1
-          /* STABLE */
-        }, 8, ["modelValue"])
-      ]),
-      _: 1
-      /* STABLE */
-    }, 8, ["components", "template"]);
+    }, null, 8, ["components", "template"]);
   }
   exampleComponent.render = render;
   setup({
