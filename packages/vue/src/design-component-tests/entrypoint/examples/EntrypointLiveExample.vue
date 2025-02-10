9201de795285e10b6732a1a27bf708db1ff06382
Olof Fredriksson <oloflolof@gmail.com>
2025-02-10 10:03:21 +0100

    feat: entrypoint always uses full width (refs SB-4982)
    BREAKING CHANGE: Consumers of `entrypoint` need to define their disered width.

--- packages/vue/src/design-component-tests/entrypoint/examples/EntrypointLiveExample.vue
+++ packages/vue/src/design-component-tests/entrypoint/examples/EntrypointLiveExample.vue
@@ -1,32 +1,23 @@
 <template>
-    <live-example :components="components" :template="template">
-        <f-checkbox-field v-model="isFullWidth" :value="true"> Fullbredd </f-checkbox-field>
-    </live-example>
+    <live-example :components="components" :template="template"> </live-example>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { FCheckboxField, FIcon } from "@fkui/vue";
+import { FIcon } from "@fkui/vue";
 import { LiveExample } from "@forsakringskassan/docs-live-example";
 
 export default defineComponent({
     name: "EntrypointLiveExample",
-    components: { LiveExample, FCheckboxField },
-    data() {
-        return {
-            isFullWidth: false,
-        };
-    },
+    components: { LiveExample },
     computed: {
         components(): object {
             return { FIcon };
         },
-        fullwidth(): string {
-            return this.isFullWidth ? "entrypoint--full-width" : "";
-        },
+
         template(): string {
             return /* HTML */ `
-                <a class="entrypoint ${this.fullwidth}" href="javascript:">
+                <a class="entrypoint" href="javascript:">
                     Ansök om hundbidrag
                     <span class="sr-only"> Till tjänsten ansök om hundbidrag</span>
                     <f-icon name="arrow-right" class="entrypoint__icon"></f-icon>
