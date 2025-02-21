75adc62a32e1bd7f0e361f5c62d90a07a931c97d
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-02-21 12:57:32 +0100

    build: disable reporting for vue components (refs SFKUI-6500)

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,30 +1,3 @@
+export * from "./stuff";
 export * from "./components";
-export {
-    type UseSlotUtils,
-    type WithoutInstance,
-    useSlotUtils,
-} from "./composables";
-export {
-    config,
-    type FKUIConfig,
-    FKUIConfigButtonOrder,
-    type FKUIContext,
-    type MaybeWithFKUIContext,
-    setRunningContext,
-} from "./config";
 export * from "./internal-components";
-export * from "./plugins";
-export {
-    type ComponentUnmountEvent,
-    type ComponentValidityEvent,
-    type ErrorItem,
-    type GroupValidityEvent,
-    type ListArray,
-    type ListItem,
-    type UnknownItem,
-    ErrorData,
-    ErrorViewData,
-    FormErrorList,
-    MenuAction,
-} from "./types";
-export * from "./utils";
