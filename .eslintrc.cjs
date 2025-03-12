4fc3dce241d456a15a83eb62a3324d395d2a132f
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-03-12 12:21:18 +0100

    build: use esm vite configuration (refs SFKUI-6500)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,7 +25,7 @@ module.exports = {
         },
 
         {
-            files: "*.ts",
+            files: ["*.ts", "*.mts"],
             extends: ["@forsakringskassan/typescript"],
         },
 
