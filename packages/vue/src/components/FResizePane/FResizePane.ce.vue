7391aa2842c7bf5872a8f687987a1b8a622d8cdc
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-03-18 13:51:36 +0100

    refactor: handle layout direction in FResizePane (refs SFKUI-6500)

--- packages/vue/src/components/FResizePane/FResizePane.ce.vue
+++ packages/vue/src/components/FResizePane/FResizePane.ce.vue
@@ -58,7 +58,7 @@ const separatorSize = ref(0);
 const layoutSize = ref(0);
 const storageKey = computed(() => (area.value ? `layout/${area.value}/size` : null));
 
-const { attachPanel: attachment, area } = useAreaData(root);
+const { attachPanel: attachment, direction, area } = useAreaData(root);
 
 useKeyboardHandler({
     increase() {
@@ -112,6 +112,10 @@ const orientation = computed((): Orientation => {
     } else {
         return "vertical";
     }
+});
+
+const classes = computed(() => {
+    return [`resize--${attachment.value}`, `resize--${direction.value}`];
 });
 
 const layoutElement = computed(() => {
@@ -177,7 +181,7 @@ function getLayoutSize(): number {
 </script>
 
 <template>
-    <div ref="root" class="resize" :class="`resize--${attachment}`">
+    <div ref="root" class="resize" :class="classes">
         <div ref="content" class="resize__content">
             <!-- @slot Pane content -->
             <slot name="content"></slot>
