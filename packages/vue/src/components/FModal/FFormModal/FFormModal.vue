a85164e4edb3762e70f52ca827ba61e7c32dc043
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2025-03-25 16:02:35 +0100

    refactor: removed unwanted fix in other file (refs SFKUI-7119)

--- packages/vue/src/components/FModal/FFormModal/FFormModal.vue
+++ packages/vue/src/components/FModal/FFormModal/FFormModal.vue
@@ -59,7 +59,7 @@ import { FValidationForm, type FValidationFormCallback } from "../../FValidation
 import { TranslationMixin } from "../../../plugins/translation";
 import { sizes } from "../sizes";
 import { FModalButton, FModalButtonDescriptor, prepareButtonList } from "../modal-button";
-import { config } from "../../../config";
+import { FKUIConfigButtonOrder } from "../../../config";
 
 export default defineComponent({
     name: "FFormModal",
@@ -183,7 +183,7 @@ export default defineComponent({
     },
     computed: {
         preparedButtons(): FModalButton[] {
-            return prepareButtonList(this.buttons, config.buttonOrder);
+            return prepareButtonList(this.buttons, FKUIConfigButtonOrder.RIGHT_TO_LEFT);
         },
     },
     methods: {
