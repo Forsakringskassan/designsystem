9ede2493455444bb96a0c9a632c12b5a8342e6a8
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2025-03-27 16:25:25 +0100

    feat: started documentation of slot (refs SFKUI-7051)

--- packages/vue/src/components/FInteractiveTable/FInteractiveTable.vue
+++ packages/vue/src/components/FInteractiveTable/FInteractiveTable.vue
@@ -515,7 +515,16 @@ function setActiveRow(row: T | undefined): void {
 <template>
     <div :class="wrapperClasses">
         <!-- technical debt / fulhack: this is to make sure the typing understands there is an undocumented slot  -->
+        <!--
+            @slot Slot for screen reader text when row get focus. The row object is available through `v-slot="{ <propertyName> }"`, e.g. `v-slot="{ row }"`.
+
+            The following properties are available:
+
+            * `row: ListItem;` The object to be visualized.
+            @binding {T} row - The object to be screen read.
+        -->
         <slot v-if="false" name="row-description"></slot>
+
         <table class="table" :role="tableRole" :class="tableClasses" v-bind="$attrs">
             <caption v-if="hasCaption">
                 <!-- @slot Slot for table caption. -->
