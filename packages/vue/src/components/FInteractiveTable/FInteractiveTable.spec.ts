47527c855cc5d2713fd64fd1dc6f07a5972b5416
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2025-03-31 16:13:41 +0200

    fix: `FTableColumn` `name` only required with `FSortFilterDataset` (fixes SFKUI-7059)
    fixes #312

--- packages/vue/src/components/FInteractiveTable/FInteractiveTable.spec.ts
+++ packages/vue/src/components/FInteractiveTable/FInteractiveTable.spec.ts
@@ -363,8 +363,8 @@ it("should add table colum headers to <thead> with correct classes", async () =>
     );
 });
 
-it("should not add duplicate table colums", async () => {
-    expect.assertions(2);
+it("should throw error if `FTableColumn.name` is duplicated", async () => {
+    expect.assertions(1);
     const TestComponent = {
         components: { FInteractiveTable, FTableColumn },
         template: /* HTML */ `
@@ -381,11 +381,11 @@ it("should not add duplicate table colums", async () => {
             };
         },
     };
-    const wrapper = createWrapper(TestComponent);
-    await wrapper.vm.$nextTick();
-    const th = wrapper.findAll("thead th");
-    expect(th).toHaveLength(1); /* should only be added once */
-    expect(th[0].text()).toBe("A");
+    expect(() => {
+        mount(TestComponent);
+    }).toThrowErrorMatchingInlineSnapshot(
+        `"Expected FTableColumn to have a unique name but encountered duplicate of "a""`,
+    );
 });
 
 it("should set scope on table columns", async () => {
