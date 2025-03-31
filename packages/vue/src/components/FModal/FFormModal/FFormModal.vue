e339e8c29b0be878722eae68067c900f002c7db6
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2025-03-31 15:30:41 +0200

    fix: adjusted method call to use standard value (refs SFKUI-7069)

--- packages/vue/src/components/FModal/FFormModal/FFormModal.vue
+++ packages/vue/src/components/FModal/FFormModal/FFormModal.vue
@@ -59,7 +59,6 @@ import { FValidationForm, type FValidationFormCallback } from "../../FValidation
 import { TranslationMixin } from "../../../plugins/translation";
 import { sizes } from "../sizes";
 import { FModalButton, FModalButtonDescriptor, prepareButtonList } from "../modal-button";
-import { config } from "../../../config";
 
 export default defineComponent({
     name: "FFormModal",
@@ -191,7 +190,7 @@ export default defineComponent({
     },
     computed: {
         preparedButtons(): FModalButton[] {
-            return prepareButtonList(this.buttons, config.buttonOrder);
+            return prepareButtonList(this.buttons);
         },
     },
     methods: {
