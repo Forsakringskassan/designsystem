a2658e6d55a7fe66f798069f9ee6bb6c1abca5a3
Coulon Fabrice (66135984) <fabrice.coulon@forsakringskassan.se>
2025-03-31 14:28:35 +0200

    fix: f-form-modal to use buttonOrder value correctly (refs SFKUI-7184)

--- packages/vue/src/components/FModal/FFormModal/FFormModal.vue
+++ packages/vue/src/components/FModal/FFormModal/FFormModal.vue
@@ -59,7 +59,6 @@ import { FValidationForm, type FValidationFormCallback } from "../../FValidation
 import { TranslationMixin } from "../../../plugins/translation";
 import { sizes } from "../sizes";
 import { FModalButton, FModalButtonDescriptor, prepareButtonList } from "../modal-button";
-import { FKUIConfigButtonOrder } from "../../../config";
 
 export default defineComponent({
     name: "FFormModal",
@@ -183,7 +182,7 @@ export default defineComponent({
     },
     computed: {
         preparedButtons(): FModalButton[] {
-            return prepareButtonList(this.buttons, FKUIConfigButtonOrder.RIGHT_TO_LEFT);
+            return prepareButtonList(this.buttons);
         },
     },
     methods: {
