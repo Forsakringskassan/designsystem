07925eaf74ccd9ef19a6f721fa655873f2c7107a
Carlsson Martin <martin.carlsson@forsakringskassan.se>
2025-04-04 10:55:40 +0200

    feat: add date directive formatter (refs SFKUI-7093)

--- packages/vue/src/plugins/format/formatters.ts
+++ packages/vue/src/plugins/format/formatters.ts
@@ -24,3 +24,61 @@ export async function formatNumber(
         el.textContent = numberFormater(number.number, number.decimals) ?? "";
     }
 }
+
+export function formatDate(el: HTMLElement, date: string | unknown): void {
+    el.classList.add("formatter--date");
+    el.textContent = "";
+
+    if (typeof date === "string") {
+        el.textContent = parseDate(date) ?? "";
+    } else if (date instanceof FDate) {
+        el.textContent = date.toString();
+    }
+}
+
+export function formatDateFull(el: HTMLElement, date: string | unknown): void {
+    el.classList.add("formatter--date-full");
+    el.textContent = "";
+
+    if (typeof date === "string") {
+        const dateString = parseDate(date) ?? "";
+        el.textContent = FDate.fromIso(dateString).toString(DateFormat.FULL);
+    } else if (date instanceof FDate) {
+        el.textContent = date.toString(DateFormat.FULL);
+    }
+}
+
+export function formatDateLong(el: HTMLElement, date: string | unknown): void {
+    el.classList.add("formatter--date-long");
+    el.textContent = "";
+
+    if (typeof date === "string") {
+        const dateString = parseDate(date) ?? "";
+        el.textContent = FDate.fromIso(dateString).toString(DateFormat.LONG);
+    } else if (date instanceof FDate) {
+        el.textContent = date.toString(DateFormat.LONG);
+    }
+}
+
+export function formatDateRange(
+    el: HTMLElement,
+    range: DateRange | unknown,
+): void {
+    el.classList.add("formatter--date-range");
+    el.textContent = "";
+
+    if (!isDateRange(range)) {
+        return;
+    }
+    const fromDate =
+        range.from instanceof FDate
+            ? range.from.toString()
+            : parseDate(range.from);
+
+    const toDate =
+        range.to instanceof FDate ? range.to.toString() : parseDate(range.to);
+
+    if (fromDate && toDate) {
+        el.textContent = `${FDate.fromIso(fromDate ?? "")} – ${FDate.fromIso(toDate ?? "")}`;
+    }
+}
