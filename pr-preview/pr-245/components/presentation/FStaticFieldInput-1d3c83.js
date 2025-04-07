99b4c92e3ddd0782239b6d1b6bd086f939c61e73
ext <ext@users.noreply.github.com>
2025-04-07 16:28:04 +0000

    Deploy preview for PR 245 🛫

--- pr-preview/pr-245/components/presentation/FStaticFieldInput-1d3c83.js
+++ pr-preview/pr-245/components/presentation/FStaticFieldInput-1d3c83.js
@@ -1,83 +1,83 @@
-"use strict";
-(() => {
-  var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
-    get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
-  }) : x)(function(x) {
-    if (typeof require !== "undefined") return require.apply(this, arguments);
-    throw Error('Dynamic require of "' + x + '" is not supported');
-  });
-
-  // docs/src/setup.ts
-  var import_vue = __require("vue");
-  var import_vue2 = __require("@fkui/vue");
-  function setup(options) {
-    const { rootComponent, selector } = options;
-    const app = (0, import_vue.createApp)({
-      render() {
-        return (0, import_vue.h)(import_vue2.FErrorHandlingApp, { defaultComponent: rootComponent });
-      }
-    });
-    (0, import_vue2.setRunningContext)(app);
-    app.use(import_vue2.ErrorPlugin, {
-      captureWarnings: true,
-      logToConsole: true
-    });
-    app.use(import_vue2.ValidationPlugin);
-    app.use(import_vue2.TestPlugin);
-    app.use(import_vue2.TranslationPlugin);
-    app.mount(selector);
-  }
-
-  // virtual-entry:./packages/vue/src/components/FStaticField/examples/FStaticFieldInput.vue
-  var import_vue3 = __require("vue");
-  var import_vue4 = __require("@fkui/vue");
-  var import_vue5 = __require("vue");
-  var exampleComponent = (0, import_vue3.defineComponent)({
-    name: "FStaticFieldTextField",
-    components: { FStaticField: import_vue4.FStaticField, FTextField: import_vue4.FTextField },
-    data() {
-      return { name: "" };
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
     }
   });
-  var _hoisted_1 = { "data-test": "output-field" };
-  function render(_ctx, _cache, $props, $setup, $data, $options) {
-    const _component_f_text_field = (0, import_vue5.resolveComponent)("f-text-field");
-    const _component_f_static_field = (0, import_vue5.resolveComponent)("f-static-field");
-    const _directive_validation = (0, import_vue5.resolveDirective)("validation");
-    return (0, import_vue5.openBlock)(), (0, import_vue5.createElementBlock)("div", _hoisted_1, [
-      (0, import_vue5.withDirectives)(((0, import_vue5.openBlock)(), (0, import_vue5.createBlock)(_component_f_text_field, {
-        id: "normal",
-        modelValue: _ctx.name,
-        "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _ctx.name = $event)
-      }, {
-        default: (0, import_vue5.withCtx)(() => _cache[1] || (_cache[1] = [
-          (0, import_vue5.createTextVNode)(" Namn ")
-        ])),
-        _: 1
-        /* STABLE */
-      }, 8, ["modelValue"])), [
-        [
-          _directive_validation,
-          { maxLength: { length: 100 } },
-          void 0,
-          { maxLength: true }
-        ]
-      ]),
-      (0, import_vue5.createVNode)(_component_f_static_field, null, {
-        label: (0, import_vue5.withCtx)(() => _cache[2] || (_cache[2] = [
-          (0, import_vue5.createTextVNode)(" Beskrivning ")
-        ])),
-        default: (0, import_vue5.withCtx)(() => [
-          _cache[3] || (_cache[3] = (0, import_vue5.createTextVNode)(" En liten statisk beskrivning. "))
-        ]),
-        _: 1
-        /* STABLE */
-      })
-    ]);
-  }
-  exampleComponent.render = render;
-  setup({
-    rootComponent: exampleComponent,
-    selector: "#FStaticFieldInput"
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
   });
-})();
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:./packages/vue/src/components/FStaticField/examples/FStaticFieldInput.vue
+import { defineComponent } from "vue";
+import { FStaticField, FTextField } from "@fkui/vue";
+import { createTextVNode as _createTextVNode, resolveComponent as _resolveComponent, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives, createVNode as _createVNode, createElementBlock as _createElementBlock } from "vue";
+var exampleComponent = defineComponent({
+  name: "FStaticFieldTextField",
+  components: { FStaticField, FTextField },
+  data() {
+    return { name: "" };
+  }
+});
+var _hoisted_1 = { "data-test": "output-field" };
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_f_text_field = _resolveComponent("f-text-field");
+  const _component_f_static_field = _resolveComponent("f-static-field");
+  const _directive_validation = _resolveDirective("validation");
+  return _openBlock(), _createElementBlock("div", _hoisted_1, [
+    _withDirectives((_openBlock(), _createBlock(_component_f_text_field, {
+      id: "normal",
+      modelValue: _ctx.name,
+      "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _ctx.name = $event)
+    }, {
+      default: _withCtx(() => _cache[1] || (_cache[1] = [
+        _createTextVNode(" Namn ")
+      ])),
+      _: 1
+      /* STABLE */
+    }, 8, ["modelValue"])), [
+      [
+        _directive_validation,
+        { maxLength: { length: 100 } },
+        void 0,
+        { maxLength: true }
+      ]
+    ]),
+    _createVNode(_component_f_static_field, null, {
+      label: _withCtx(() => _cache[2] || (_cache[2] = [
+        _createTextVNode(" Beskrivning ")
+      ])),
+      default: _withCtx(() => [
+        _cache[3] || (_cache[3] = _createTextVNode(" En liten statisk beskrivning. "))
+      ]),
+      _: 1
+      /* STABLE */
+    })
+  ]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#FStaticFieldInput"
+});
+export {
+  render
+};
--- pr-preview/pr-245/functions/plugins/ValidationPluginToggleEnabled-dfcd07.js
+++ pr-preview/pr-245/functions/plugins/ValidationPluginToggleEnabled-dfcd07.js
@@ -1,90 +1,90 @@
-"use strict";
-(() => {
-  var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
-    get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
-  }) : x)(function(x) {
-    if (typeof require !== "undefined") return require.apply(this, arguments);
-    throw Error('Dynamic require of "' + x + '" is not supported');
-  });
-
-  // docs/src/setup.ts
-  var import_vue = __require("vue");
-  var import_vue2 = __require("@fkui/vue");
-  function setup(options) {
-    const { rootComponent, selector } = options;
-    const app = (0, import_vue.createApp)({
-      render() {
-        return (0, import_vue.h)(import_vue2.FErrorHandlingApp, { defaultComponent: rootComponent });
-      }
-    });
-    (0, import_vue2.setRunningContext)(app);
-    app.use(import_vue2.ErrorPlugin, {
-      captureWarnings: true,
-      logToConsole: true
-    });
-    app.use(import_vue2.ValidationPlugin);
-    app.use(import_vue2.TestPlugin);
-    app.use(import_vue2.TranslationPlugin);
-    app.mount(selector);
-  }
-
-  // virtual-entry:./packages/vue/src/plugins/validation/examples/ValidationPluginToggleEnabled.vue
-  var import_vue3 = __require("vue");
-  var import_logic = __require("@fkui/logic");
-  var import_vue4 = __require("@fkui/vue");
-  var import_vue5 = __require("vue");
-  var exampleComponent = (0, import_vue3.defineComponent)({
-    name: "ValidationPluginToggleEnabled",
-    components: { FTextField: import_vue4.FTextField },
-    data() {
-      return { model: "", toggleEnabled: true };
-    },
-    mounted() {
-      import_logic.ValidationService.setSubmitted("validator-enabled");
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
     }
   });
-  function render(_ctx, _cache, $props, $setup, $data, $options) {
-    const _component_f_text_field = (0, import_vue5.resolveComponent)("f-text-field");
-    const _directive_test = (0, import_vue5.resolveDirective)("test");
-    const _directive_validation = (0, import_vue5.resolveDirective)("validation");
-    return (0, import_vue5.openBlock)(), (0, import_vue5.createElementBlock)("div", null, [
-      (0, import_vue5.createElementVNode)(
-        "p",
-        null,
-        "Validering aktiverad: " + (0, import_vue5.toDisplayString)(_ctx.toggleEnabled),
-        1
-        /* TEXT */
-      ),
-      (0, import_vue5.withDirectives)(((0, import_vue5.openBlock)(), (0, import_vue5.createBlock)(_component_f_text_field, {
-        id: "validator-enabled",
-        modelValue: _ctx.model,
-        "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _ctx.model = $event)
-      }, {
-        default: (0, import_vue5.withCtx)(() => _cache[2] || (_cache[2] = [
-          (0, import_vue5.createTextVNode)(" Max tio tecken ")
-        ])),
-        _: 1
-        /* STABLE */
-      }, 8, ["modelValue"])), [
-        [_directive_test, "validator-enabled"],
-        [
-          _directive_validation,
-          { maxLength: { enabled: _ctx.toggleEnabled, length: 10 } },
-          void 0,
-          { maxLength: true }
-        ]
-      ]),
-      (0, import_vue5.createElementVNode)("button", {
-        class: "button button--primary",
-        "data-test": "validator-enabled-button",
-        type: "button",
-        onClick: _cache[1] || (_cache[1] = ($event) => _ctx.toggleEnabled = !_ctx.toggleEnabled)
-      }, " Aktivera/Inaktivera ")
-    ]);
-  }
-  exampleComponent.render = render;
-  setup({
-    rootComponent: exampleComponent,
-    selector: "#ValidationPluginToggleEnabled"
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
   });
-})();
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:./packages/vue/src/plugins/validation/examples/ValidationPluginToggleEnabled.vue
+import { defineComponent } from "vue";
+import { ValidationService } from "@fkui/logic";
+import { FTextField } from "@fkui/vue";
+import { toDisplayString as _toDisplayString, createElementVNode as _createElementVNode, createTextVNode as _createTextVNode, resolveComponent as _resolveComponent, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives, createElementBlock as _createElementBlock } from "vue";
+var exampleComponent = defineComponent({
+  name: "ValidationPluginToggleEnabled",
+  components: { FTextField },
+  data() {
+    return { model: "", toggleEnabled: true };
+  },
+  mounted() {
+    ValidationService.setSubmitted("validator-enabled");
+  }
+});
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_f_text_field = _resolveComponent("f-text-field");
+  const _directive_test = _resolveDirective("test");
+  const _directive_validation = _resolveDirective("validation");
+  return _openBlock(), _createElementBlock("div", null, [
+    _createElementVNode(
+      "p",
+      null,
+      "Validering aktiverad: " + _toDisplayString(_ctx.toggleEnabled),
+      1
+      /* TEXT */
+    ),
+    _withDirectives((_openBlock(), _createBlock(_component_f_text_field, {
+      id: "validator-enabled",
+      modelValue: _ctx.model,
+      "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _ctx.model = $event)
+    }, {
+      default: _withCtx(() => _cache[2] || (_cache[2] = [
+        _createTextVNode(" Max tio tecken ")
+      ])),
+      _: 1
+      /* STABLE */
+    }, 8, ["modelValue"])), [
+      [_directive_test, "validator-enabled"],
+      [
+        _directive_validation,
+        { maxLength: { enabled: _ctx.toggleEnabled, length: 10 } },
+        void 0,
+        { maxLength: true }
+      ]
+    ]),
+    _createElementVNode("button", {
+      class: "button button--primary",
+      "data-test": "validator-enabled-button",
+      type: "button",
+      onClick: _cache[1] || (_cache[1] = ($event) => _ctx.toggleEnabled = !_ctx.toggleEnabled)
+    }, " Aktivera/Inaktivera ")
+  ]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#ValidationPluginToggleEnabled"
+});
+export {
+  render
+};
