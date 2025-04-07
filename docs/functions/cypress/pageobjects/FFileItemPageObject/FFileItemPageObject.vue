e04536855a22c35221960affbe9995d2f03d9838
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-04-07 17:29:48 +0200

    docs: ffileitem documentation added (refs SFKUI-7022)

---
+++ docs/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { FFileItem } from "@fkui/vue";
+</script>
+
+<template>
+    <!-- cut above -->
+    <f-file-item v-test="'file-item'" :file-name="'myFile.pdf'" :mime-type="'application/pdf'"></f-file-item>
+    <!-- cut below -->
+</template>
