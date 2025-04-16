Forsakringskassan/fk-designsystem

docs: fbadge documentation added (refs SFKUI-7023)

docs/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject-is-inverted.cy.ts
@@ -1,0 +1,18 @@
+import { FBadgePageObject } from "@fkui/vue/pageobject";
+import Example from "./FBadgePageObject-is-inverted.vue";
+
+it("should check if the badge is inverted or not ", () => {
+    cy.mount(Example);
+
+    /* --- cut above --- */
+    const myBadgeNotIverted = new FBadgePageObject(
+        "[data-test=my-badge-not-inverted]",
+    );
+    myBadgeNotIverted.isInverted().should("be.equal", false);
+
+    const myBadgeInverted = new FBadgePageObject(
+        "[data-test=my-badge-inverted]",
+    );
+    myBadgeInverted.isInverted().should("be.equal", true);
+    /* --- cut below --- */
+});

docs/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject-status.cy.ts
@@ -1,0 +1,11 @@
+import { FBadgePageObject } from "@fkui/vue/pageobject";
+import Example from "./FBadgePageObject-status.vue";
+
+it("Should match the status warning in the badge", () => {
+    cy.mount(Example);
+
+    /* --- cut above --- */
+    const myBadge = new FBadgePageObject("[data-test=my-badge-warning]");
+    myBadge.status().should("be.equal", "warning");
+    /* --- cut below --- */
+});

docs/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject.cy.ts
@@ -1,0 +1,12 @@
+import { FBadgePageObject } from "@fkui/vue/pageobject";
+import Example from "./FBadgePageObject.vue";
+
+it("Should show the status default and that the badge is not inverted", () => {
+    cy.mount(Example);
+
+    /* --- cut above --- */
+    const myBadge = new FBadgePageObject("[data-test=my-badge]");
+    myBadge.status().should("be.equal", "default");
+    myBadge.isInverted().should("be.equal", false);
+    /* --- cut below --- */
+});

Forsakringskassan/designsystem

docs/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject-is-inverted.cy.ts
@@ -1,0 +1,18 @@
+import { FBadgePageObject } from "@fkui/vue/cypress";
+import Example from "./FBadgePageObject-is-inverted.vue";
+
+it("should check if the badge is inverted or not ", () => {
+    cy.mount(Example);
+
+    /* --- cut above --- */
+    const myBadgeNotIverted = new FBadgePageObject(
+        "[data-test=my-badge-not-inverted]",
+    );
+    myBadgeNotIverted.isInverted().should("be.equal", false);
+
+    const myBadgeInverted = new FBadgePageObject(
+        "[data-test=my-badge-inverted]",
+    );
+    myBadgeInverted.isInverted().should("be.equal", true);
+    /* --- cut below --- */
+});

docs/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject-status.cy.ts
@@ -1,0 +1,11 @@
+import { FBadgePageObject } from "@fkui/vue/cypress";
+import Example from "./FBadgePageObject-status.vue";
+
+it("Should match the status warning in the badge", () => {
+    cy.mount(Example);
+
+    /* --- cut above --- */
+    const myBadge = new FBadgePageObject("[data-test=my-badge-warning]");
+    myBadge.status().should("be.equal", "warning");
+    /* --- cut below --- */
+});

docs/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject.cy.ts
@@ -1,0 +1,12 @@
+import { FBadgePageObject } from "@fkui/vue/cypress";
+import Example from "./FBadgePageObject.vue";
+
+it("Should show the status default and that the badge is not inverted", () => {
+    cy.mount(Example);
+
+    /* --- cut above --- */
+    const myBadge = new FBadgePageObject("[data-test=my-badge]");
+    myBadge.status().should("be.equal", "default");
+    myBadge.isInverted().should("be.equal", false);
+    /* --- cut below --- */
+});