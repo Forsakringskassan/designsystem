0f323c379876076884e3a49efb514fd94869e572
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2025-05-14 14:14:57 +0000

    chore(release): [skip ci] deploy documentation from main branch

---
+++ main/functions/cypress/pageobjects/FProgressbarPageObject/FProgressbarPageObject-el-fe7302.js
@@ -0,0 +1,68 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FProgressbarPageObject/FProgressbarPageObject-el.vue:FProgressbarPageObject-el-fe7302.js
+import { defineComponent as _defineComponent } from "vue";
+import { FProgressbar } from "@fkui/vue";
+import { resolveDirective as _resolveDirective, withDirectives as _withDirectives, openBlock as _openBlock, createBlock as _createBlock } from "vue";
+var exampleComponent = /* @__PURE__ */ _defineComponent({
+  __name: "FProgressbarPageObject-el",
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const __returned__ = { get FProgressbar() {
+      return FProgressbar;
+    } };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _directive_test = _resolveDirective("test");
+  return _withDirectives((_openBlock(), _createBlock(
+    $setup["FProgressbar"],
+    {
+      value: 40,
+      "aria-label": "Progressbar"
+    },
+    null,
+    512
+    /* NEED_PATCH */
+  )), [
+    [_directive_test, "progressbar"]
+  ]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-fe7302"
+});
+export {
+  render
+};
