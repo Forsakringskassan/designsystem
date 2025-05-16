e509f7a7104d9de7c73f77706234de06bbf42017
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2025-05-16 12:19:42 +0000

    chore(release): [skip ci] deploy documentation from main branch

--- main/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject-typeOfFile-c90076.js
+++ main/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject-typeOfFile-c90076.js
@@ -28,7 +28,7 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject-typeOfFile.vue:FFileItemPageObject-typeOfFile-a2916d.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject-typeOfFile.vue:FFileItemPageObject-typeOfFile-c90076.js
 import { defineComponent as _defineComponent } from "vue";
 import { FFileItem } from "@fkui/vue";
 import { resolveDirective as _resolveDirective, createVNode as _createVNode, withDirectives as _withDirectives, Fragment as _Fragment, openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue";
@@ -93,7 +93,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-a2916d"
+  selector: "#example-c90076"
 });
 export {
   render
--- main/functions/cypress/pageobjects/FLabelPageObject/FLabelPageObject-format-description-aa9a8e.js
+++ main/functions/cypress/pageobjects/FLabelPageObject/FLabelPageObject-format-description-aa9a8e.js
@@ -28,16 +28,16 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject-fileName.vue:FFileItemPageObject-fileName-099421.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FLabelPageObject/FLabelPageObject-format-description.vue:FLabelPageObject-format-description-aa9a8e.js
 import { defineComponent as _defineComponent } from "vue";
-import { FFileItem } from "@fkui/vue";
-import { resolveDirective as _resolveDirective, withDirectives as _withDirectives, openBlock as _openBlock, createBlock as _createBlock } from "vue";
+import { FLabel } from "@fkui/vue";
+import { createTextVNode as _createTextVNode, normalizeClass as _normalizeClass, createElementVNode as _createElementVNode, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives } from "vue";
 var exampleComponent = /* @__PURE__ */ _defineComponent({
-  __name: "FFileItemPageObject-fileName",
+  __name: "FLabelPageObject-format-description",
   setup(__props, { expose: __expose }) {
     __expose();
-    const __returned__ = { get FFileItem() {
-      return FFileItem;
+    const __returned__ = { get FLabel() {
+      return FLabel;
     } };
     Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
     return __returned__;
@@ -45,23 +45,31 @@ var exampleComponent = /* @__PURE__ */ _defineComponent({
 });
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   const _directive_test = _resolveDirective("test");
-  return _withDirectives((_openBlock(), _createBlock(
-    $setup["FFileItem"],
-    {
-      "file-name": "myFile.doc",
-      "mime-type": "application/msword"
-    },
-    null,
-    512
-    /* NEED_PATCH */
-  )), [
-    [_directive_test, "awesome-file"]
+  return _withDirectives((_openBlock(), _createBlock($setup["FLabel"], null, {
+    default: _withCtx(() => _cache[0] || (_cache[0] = [
+      _createTextVNode(" Etikett ")
+    ])),
+    description: _withCtx(({ formatDescriptionClass }) => [
+      _createElementVNode(
+        "span",
+        {
+          class: _normalizeClass(formatDescriptionClass)
+        },
+        " Formatbeskrivning ",
+        2
+        /* CLASS */
+      )
+    ]),
+    _: 1
+    /* STABLE */
+  })), [
+    [_directive_test, "awesome-label"]
   ]);
 }
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-099421"
+  selector: "#example-aa9a8e"
 });
 export {
   render
