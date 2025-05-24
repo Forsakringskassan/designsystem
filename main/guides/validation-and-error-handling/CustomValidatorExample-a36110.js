c99336dc1e77bab6fb11055a8fe3e7d978eaf2b1
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2025-05-24 11:05:39 +0000

    chore(release): [skip ci] deploy documentation from main branch

--- main/guides/validation-and-error-handling/CustomValidatorExample-a36110.js
+++ main/guides/validation-and-error-handling/CustomValidatorExample-a36110.js
@@ -28,7 +28,7 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/guides/validation/examples/CustomValidatorExample.vue:CustomValidatorExample-deec2b.js
+// virtual-entry:virtual:docs/guides/validation/examples/CustomValidatorExample.vue:CustomValidatorExample-a36110.js
 import { FTextField } from "@fkui/vue";
 import {
   ValidationErrorMessageBuilder,
@@ -181,7 +181,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-deec2b"
+  selector: "#example-a36110"
 });
 export {
   render,
