0c585c88e890de560a1aaa62f204dbdf31c68ea3
ext <ext@users.noreply.github.com>
2025-05-26 21:46:53 +0000

    Deploy preview for PR 519 🛫

---
+++ pr-preview/pr-519/functions/cypress/pageobjects/FCheckboxFieldPageObject/FCheckboxFieldPageObject-details-2233bb.js
@@ -0,0 +1,44 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FCheckboxFieldPageObject/FCheckboxFieldPageObject-details.vue:FCheckboxFieldPageObject-details-2233bb.js
+import { openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue";
+var exampleComponent = {};
+function render(_ctx, _cache) {
+  return _openBlock(), _createElementBlock("div", null, "lorem ipsum");
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-2233bb"
+});
+export {
+  render
+};
---
+++ pr-preview/pr-519/functions/cypress/pageobjects/FRadioGroupPageObject/FRadioGroupPageObject-numberOfOptions-a4be23.js
@@ -0,0 +1,44 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FRadioGroupPageObject/FRadioGroupPageObject-numberOfOptions.vue:FRadioGroupPageObject-numberOfOptions-a4be23.js
+import { openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue";
+var exampleComponent = {};
+function render(_ctx, _cache) {
+  return _openBlock(), _createElementBlock("div", null, "lorem ipsum");
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-a4be23"
+});
+export {
+  render
+};
