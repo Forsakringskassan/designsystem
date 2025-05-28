ac815243847d9df33f7c8029c2cb8927a652d92b
Carlsson Martin <martin.carlsson@forsakringskassan.se>
2025-05-28 15:44:18 +0200

    WIP

---
+++ internal/vue-sandbox/src/views/MyComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-field">
+        <div class="i-width-sm-12">
+            <label class="label" for="fkui-vue-element-0003"> Etikett 3</label>
+        </div>
+        <div class="text-field__input-wrapper i-width-sm-12">
+            <div class="text-field__icon-wrapper">
+                <input id="fkui-vue-element-0003" type="text" class="text-field__input" />
+            </div>
+        </div>
+    </div>
+</template>
