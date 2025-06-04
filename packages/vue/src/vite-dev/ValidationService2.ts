f146f3d8511bcf58db1af41ef055c0697b10c993
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-06-04 15:08:11 +0200

    element

--- packages/vue/src/vite-dev/ValidationService2.ts
+++ packages/vue/src/vite-dev/ValidationService2.ts
@@ -4,6 +4,16 @@ import {
     type ViewValueValidator,
 } from "./validator-definition";
 
+/** @internal */
+export const sym = Symbol("validation-target");
+
+declare global {
+    interface HTMLElement {
+        [sym]: ValidationTarget<unknown, unknown>;
+    }
+}
+
+/** @internal */
 export interface ValidationTarget<TValue, TModel> {
     getViewValue(): TValue | null | undefined;
     getModelValue(): TModel;
@@ -24,9 +34,23 @@ function isModelValueValidator<K extends string, TValue, TModel>(
     return value.type === "parsed";
 }
 
-export function validateElement<TValue, TModel>(
-    target: ValidationTarget<TValue, TModel>,
-): boolean {
+export function enableValidation(
+    element: HTMLElement,
+    target: ValidationTarget<unknown, unknown>,
+): void {
+    console.log('enable validation on', element);
+    element[sym] = target;
+}
+
+export function validateElement<TValue, TModel>(element: {
+    [sym]?: ValidationTarget<TValue, TModel>;
+}): boolean {
+    const { [sym]: target } = element;
+    if (!target) {
+        console.log('no target in element', element);
+        return true;
+    }
+
     const viewValue = target.getViewValue();
     for (const validator of target.validators.filter(isViewValueValidator)) {
         const result = validator.validate(viewValue);
