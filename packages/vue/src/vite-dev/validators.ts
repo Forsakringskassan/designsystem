e444d5532eb78ed3e47e7db4dc86c32ef479db6d
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-06-04 13:26:05 +0200

    further complications

---
+++ packages/vue/src/vite-dev/validators.ts
@@ -0,0 +1,17 @@
+import { defineValidator } from "./validator-definition";
+
+declare module "./validator-result" {
+    interface ValidationCodeMapping {
+        personnummer: "format" | "checksum";
+    }
+}
+
+export const personnummer = defineValidator("personnummer", {
+    type: "raw",
+    validate(value: string) {
+        if (typeof value === "string" && /^\d+$/.test(value)) {
+            return { name: "personnummer", valid: true };
+        }
+        return { name: "personnummer", valid: false, code: "format" };
+    },
+});
