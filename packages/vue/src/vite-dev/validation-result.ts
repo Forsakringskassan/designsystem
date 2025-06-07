626a9bcbb24652947ea35d4b6e6e7e835bc6e676
David Sveningsson <ext@sidvind.com>
2025-06-07 20:10:22 +0200

    UNFIN

---
+++ packages/vue/src/vite-dev/validation-result.ts
@@ -0,0 +1,15 @@
+
+/**
+ * The result of {@link validateElement}.
+ *
+ * @public
+ */
+export interface ValidationResult {
+    /** True if there is no errors */
+    readonly isValid: boolean;
+    /** List of errors present */
+    readonly errors: Array<{
+        readonly element: HTMLElement;
+        readonly message: string;
+    }>;
+}
