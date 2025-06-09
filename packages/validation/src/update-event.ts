965106b45d58c232790bc31f8d5c3082c5f9d191
David Sveningsson <ext@sidvind.com>
2025-06-09 11:01:05 +0200

    UNFIN

---
+++ packages/validation/src/update-event.ts
@@ -0,0 +1,20 @@
+/**
+ * @public
+ */
+export interface UpdateEventDetails<TValue = unknown, TModel = unknown> {
+    isValid: boolean;
+    message: string;
+    viewValue?: TValue;
+    modelValue?: TModel;
+    formattedValue?: TValue;
+
+    /** true if the form associated with the component has been attempted to be submitted */
+    submitted: boolean;
+}
+
+/**
+ * @public
+ */
+export type UpdateEvent<TValue = unknown, TModel = unknown> = CustomEvent<
+    UpdateEventDetails<TValue, TModel>
+>;
