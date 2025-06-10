aa42b2d0a50c614a0d43e1a73030cbb7be8401df
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2025-06-10 10:51:19 +0200

    feat: deprecate `FTableColumnPageObject` (refs SFKUI-6500)
    `FTableColumnPageObject` has been deprecated. Use methods on
    `FInteractiveTablePageObject` instead.

--- packages/vue/src/cypress/FTableColumn.pageobject.ts
+++ packages/vue/src/cypress/FTableColumn.pageobject.ts
@@ -3,6 +3,7 @@ import { FCheckboxFieldPageObject } from "./FCheckboxField.pageobject";
 
 /**
  * @public
+ * @deprecated Use methods on `FInteractiveTable` instead. Deprecated since %version%.
  */
 export class FTableColumnPageObject implements BasePageObject {
     public selector: string;
@@ -14,14 +15,23 @@ export class FTableColumnPageObject implements BasePageObject {
         this.el = () => cy.get(this.selector);
     }
 
+    /**
+     * @deprecated Use ´FInteractiveTablePageObject.cell()´ instead. Deprecated since %version%.
+     */
     public tableRowBodyContent(position: number): DefaultCypressChainable {
         return cy.get(`${this.selector} td:nth(${position})`);
     }
 
+    /**
+     * @deprecated Use ´FInteractiveTablePageObject.header()´ instead. Deprecated since %version%.
+     */
     public tableRowHeaderContent(): DefaultCypressChainable {
         return cy.get(`${this.selector} th`);
     }
 
+    /**
+     * @deprecated Use ´FInteractiveTablePageObject.checkbox()´ instead. Deprecated since %version%.
+     */
     public checkbox(): FCheckboxFieldPageObject {
         return new FCheckboxFieldPageObject(this.selector);
     }
