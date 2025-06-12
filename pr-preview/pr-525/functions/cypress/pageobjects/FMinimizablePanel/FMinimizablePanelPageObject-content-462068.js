0e9f1d0c5bb141f9f61bf4065dd87680102a9a71
johancarnegard <johancarnegard@users.noreply.github.com>
2025-06-12 07:06:14 +0000

    Deploy preview for PR 525 🛫

--- pr-preview/pr-525/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-content-462068.js
+++ pr-preview/pr-525/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-content-462068.js
@@ -28,12 +28,12 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-footer.vue:FMinimizablePanelPageObject-footer-82b6de.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-content.vue:FMinimizablePanelPageObject-content-462068.js
 import { defineComponent as _defineComponent } from "vue";
 import { FPageLayout, FMinimizablePanel } from "@fkui/vue";
 import { createElementVNode as _createElementVNode, withCtx as _withCtx, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
 var exampleComponent = /* @__PURE__ */ _defineComponent({
-  __name: "FMinimizablePanelPageObject-footer",
+  __name: "FMinimizablePanelPageObject-content",
   setup(__props, { expose: __expose }) {
     __expose();
     const __returned__ = { get FPageLayout() {
@@ -49,15 +49,21 @@ var _hoisted_1 = ["slot"];
 var _hoisted_2 = ["slot"];
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   return _openBlock(), _createBlock($setup["FPageLayout"], { layout: "three-column" }, {
-    default: _withCtx(({ left, content }) => [
-      _createVNode($setup["FMinimizablePanel"], { slot: left }, {
-        default: _withCtx(({ footer }) => [
-          _createElementVNode("div", { slot: footer }, "Lorem ipsum", 8, _hoisted_1)
+    default: _withCtx((layoutScope) => [
+      _createVNode($setup["FMinimizablePanel"], {
+        slot: layoutScope.left
+      }, {
+        default: _withCtx((panelScope) => [
+          _createElementVNode("div", {
+            slot: panelScope.content
+          }, "Lorem ipsum", 8, _hoisted_1)
         ]),
         _: 2
         /* DYNAMIC */
       }, 1032, ["slot"]),
-      _createElementVNode("div", { slot: content }, "Inneh\xE5ll", 8, _hoisted_2)
+      _createElementVNode("div", {
+        slot: layoutScope.content
+      }, "Inneh\xE5ll", 8, _hoisted_2)
     ]),
     _: 1
     /* STABLE */
@@ -66,7 +72,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-82b6de"
+  selector: "#example-462068"
 });
 export {
   render
--- pr-preview/pr-525/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-el-6a4e54.js
+++ pr-preview/pr-525/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-el-6a4e54.js
@@ -28,12 +28,12 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-toggle-button.vue:FMinimizablePanelPageObject-toggle-button-7b9959.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-el.vue:FMinimizablePanelPageObject-el-6a4e54.js
 import { defineComponent as _defineComponent } from "vue";
 import { FPageLayout, FMinimizablePanel } from "@fkui/vue";
 import { createVNode as _createVNode, createElementVNode as _createElementVNode, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock } from "vue";
 var exampleComponent = /* @__PURE__ */ _defineComponent({
-  __name: "FMinimizablePanelPageObject-toggle-button",
+  __name: "FMinimizablePanelPageObject-el",
   setup(__props, { expose: __expose }) {
     __expose();
     const __returned__ = { get FPageLayout() {
@@ -59,7 +59,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-7b9959"
+  selector: "#example-6a4e54"
 });
 export {
   render
