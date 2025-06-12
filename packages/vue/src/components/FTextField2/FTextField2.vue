1327240a0e567c607df1b6627c1c92da88b0bcfc
David Sveningsson <ext@sidvind.com>
2025-06-12 23:45:59 +0200

    UNFIN

--- packages/vue/src/components/FTextField2/FTextField2.vue
+++ packages/vue/src/components/FTextField2/FTextField2.vue
@@ -1,30 +1,38 @@
 <!-- eslint-disable import/no-extraneous-dependencies -->
-<script setup lang="ts">
-import { ElementIdService, formatNumber, parseNumber } from "@fkui/logic";
-import { onMounted, ref, useTemplateRef, watch, watchEffect } from "vue";
+<script setup lang="ts" generic="T">
+import { ElementIdService } from "@fkui/logic";
+import { onMounted, ref, useTemplateRef } from "vue";
 import { FLabel } from "@fkui/vue";
-import { type UpdateEvent, enableValidation, addValidatorsToElement } from "@fkui/validation";
+import { addValidatorsToElement } from "@fkui/validation";
 import { type Validity, useValidation } from "./use-validation";
 
 const id = ElementIdService.generateElementId();
 
-const modelValue = defineModel<string | number>({ default: "" });
-const viewValue = ref<string>(formatNumber(modelValue.value) ?? "");
+type ParserFn<U> = (value: string) => U;
+type FormatterFn<U> = (value: U) => string;
+
+const props = defineProps<{
+    parser?: ParserFn<T>;
+    formatter?: FormatterFn<T>;
+}>();
+
+const modelValue = defineModel<string | T>({ default: "" });
+const viewValue = ref<string>(props.formatter ? props.formatter(modelValue.value as T) : (modelValue.value as string));
 const validity = defineModel<Validity>("validity", {
     required: false,
     default: { isValid: false },
 });
 
 const element = useTemplateRef("input");
-const { attributes, showValidationError, validationMessage } = useValidation(element, {
+const { attributes, showValidationError, validationMessage } = useValidation<string, T>(element, {
     viewValue,
     modelValue,
     validity,
-    parser(value: string): number {
-        return parseNumber(value)!;
+    parser(value) {
+        return props.parser ? props.parser(value) : (value as T);
     },
-    formatter(value: number): string {
-        return formatNumber(value)!;
+    formatter(value) {
+        return props.formatter ? props.formatter(value) : (value as string);
     },
     event: ["blur"],
 });
@@ -56,11 +64,13 @@ onMounted(() => {
     <!-- [html-validate-disable-block fkui/required-max-length -- temp] -->
     <input
         :id
-        :ariaInvalid="attributes.ariaInvalid"
-        :required="attributes.required.value"
         ref="input"
         v-model="viewValue"
+        :ariaInvalid="attributes.ariaInvalid"
+        :required="attributes.required.value"
         type="text"
     />
-    <pre>{{ JSON.stringify({ showValidationError, validationMessage, viewValue, modelValue, validity }, null, 2) }}</pre>
+    <pre>{{
+        JSON.stringify({ showValidationError, validationMessage, viewValue, modelValue, validity }, null, 2)
+    }}</pre>
 </template>
--- packages/vue/src/vite-dev/app.vue
+++ packages/vue/src/vite-dev/app.vue
@@ -7,18 +7,19 @@ const namn = ref("World");
 const validity = ref<{ isValid: boolean }>({ isValid: false });
 const form = useTemplateRef("form");
 
-async function onSubmit(event: Event) {
+async function onSubmit(event: Event): Promise<void> {
     event.preventDefault();
     if (form.value) {
         setFormSubmitted(form.value);
         const result = await validateElement(form.value);
+        /* eslint-disable-next-line no-console -- temp */
         console.log(result);
     }
 }
 </script>
 
 <template>
-    <form @submit="onSubmit" ref="form">
+    <form ref="form" @submit="onSubmit">
         <f-text-field2 v-model="namn" v-model:validity="validity"> Namn </f-text-field2>
         <button type="submit">Submit</button>
         <p v-if="validity.isValid">Denna texten visas bara om värdet är giltigt</p>
