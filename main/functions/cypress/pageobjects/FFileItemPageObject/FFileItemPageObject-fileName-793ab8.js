625b71979187eae836fae4cd1a12562367aeeef6
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2025-06-13 15:08:24 +0000

    chore(release): [skip ci] deploy documentation from main branch

--- main/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject-fileName-793ab8.js
+++ main/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject-fileName-793ab8.js
@@ -28,7 +28,7 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject-fileName.vue:FFileItemPageObject-fileName-c85dac.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FFileItemPageObject/FFileItemPageObject-fileName.vue:FFileItemPageObject-fileName-793ab8.js
 import { defineComponent as _defineComponent } from "vue";
 import { FFileItem } from "@fkui/vue";
 import { resolveDirective as _resolveDirective, withDirectives as _withDirectives, openBlock as _openBlock, createBlock as _createBlock } from "vue";
@@ -61,7 +61,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-c85dac"
+  selector: "#example-793ab8"
 });
 export {
   render
