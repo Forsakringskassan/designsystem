b35e266fc50a20861577ab7379701019dee43a55
jaken22 <jaken22@users.noreply.github.com>
2025-06-17 08:07:33 +0000

    Deploy preview for PR 524 🛫

--- pr-preview/pr-524/guides/formatting-and-parsing/WithFormatterExample-916efb.js
+++ pr-preview/pr-524/guides/formatting-and-parsing/WithFormatterExample-916efb.js
@@ -28,20 +28,20 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/guides/validation/examples/WithParserExample.vue:WithParserExample-deded1.js
+// virtual-entry:virtual:docs/guides/validation/examples/WithFormatterExample.vue:WithFormatterExample-916efb.js
 import { defineComponent } from "vue";
 import { FTextField, FOutputField } from "@fkui/vue";
 import { createTextVNode as _createTextVNode, resolveComponent as _resolveComponent, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives, createElementVNode as _createElementVNode, toDisplayString as _toDisplayString, createVNode as _createVNode, createElementBlock as _createElementBlock } from "vue";
-function myParser(value) {
-  return value.toUpperCase();
+function myFormatter(value) {
+  return value.toLowerCase();
 }
 var exampleComponent = defineComponent({
-  name: "WithParserExample",
+  name: "WithFormatterExample",
   components: { FTextField, FOutputField },
   data() {
     return {
       modelValue: "",
-      myParser
+      myFormatter
     };
   }
 });
@@ -56,17 +56,17 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
   return _openBlock(), _createElementBlock("div", _hoisted_1, [
     _createElementVNode("div", _hoisted_2, [
       _withDirectives((_openBlock(), _createBlock(_component_f_text_field, {
-        id: "with-parser",
+        id: "with-formatter",
         modelValue: _ctx.modelValue,
         "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _ctx.modelValue = $event),
-        parser: _ctx.myParser
+        formatter: _ctx.myFormatter
       }, {
         default: _withCtx(() => _cache[1] || (_cache[1] = [
           _createTextVNode(" Inmatningsf\xE4lt ")
         ])),
         _: 1
         /* STABLE */
-      }, 8, ["modelValue", "parser"])), [
+      }, 8, ["modelValue", "formatter"])), [
         [
           _directive_validation,
           { maxLength: { length: 100 } },
@@ -76,7 +76,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
       ])
     ]),
     _createElementVNode("div", _hoisted_3, [
-      _createVNode(_component_f_output_field, { for: "with-parser" }, {
+      _createVNode(_component_f_output_field, { for: "with-formatter" }, {
         label: _withCtx(() => _cache[2] || (_cache[2] = [
           _createTextVNode(" modelValue ")
         ])),
@@ -98,7 +98,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-deded1"
+  selector: "#example-916efb"
 });
 export {
   render
