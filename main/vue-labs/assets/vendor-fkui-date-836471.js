75ada1a5bdecba029c4a4ab31e12674321667ec9
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2025-06-18 08:20:59 +0000

    chore(release): [skip ci] deploy documentation from main branch

--- main/vue-labs/assets/vendor-fkui-date-836471.js
+++ main/vue-labs/assets/vendor-fkui-date-836471.js
@@ -751,6 +751,15 @@ var FDate = class _FDate {
   addYears(value) {
     return new _FDate(this.value.add(value, "year"));
   }
+  /**
+   * Returns a new {@link FDate} object with the date before This one.
+   *
+   * @public
+   * @since %version%
+   */
+  previous() {
+    return this.addDays(-1);
+  }
   next() {
     return this.addDays(1);
   }
@@ -869,6 +878,248 @@ var FDate = class _FDate {
     return this.toString(DateFormat.ISO8601);
   }
 };
+var FYear = class _FYear {
+  _value;
+  constructor(value) {
+    if (Math.round(value) === value) {
+      this._value = value;
+    } else {
+      this._value = NaN;
+    }
+  }
+  /**
+   * Create {@link FYear} from current date.
+   *
+   * @example
+   *
+   * ```ts
+   * FYear.now();
+   * ```
+   *
+   * @public
+   */
+  static now() {
+    const value = (/* @__PURE__ */ new Date()).getFullYear();
+    return new _FYear(value);
+  }
+  /**
+   * Create {@link FYear} from a year (stored as number or string with four
+   * digits).
+   *
+   * @example
+   *
+   * ```ts
+   * FYear.fromYear(1999);
+   * FYear.fromYear("1999");
+   * ```
+   *
+   * @public
+   * @param value - The year to set the `FYear` object to.
+   */
+  static fromYear(value) {
+    const parsed = typeof value === "string" ? parseInt(value, 10) : value;
+    return new _FYear(parsed);
+  }
+  /**
+   * Create {@link FYear} from an `FDate` or `Date` object.
+   *
+   * @example
+   *
+   * ```ts
+   * FYear.fromDate(FDate.now());
+   * FYear.fromDate(new Date());
+   * ```
+   *
+   * @public
+   * @param value - The date to get year from.
+   */
+  static fromDate(value) {
+    if (value instanceof Date) {
+      return new _FYear(value.getFullYear());
+    } else {
+      return new _FYear(value.year);
+    }
+  }
+  /**
+   * Get the year as number (four digits)
+   *
+   * ```
+   * FYear.now().value; // => 1999
+   * ```
+   *
+   * @public
+   */
+  get value() {
+    return this._value;
+  }
+  /**
+   * This returns a `boolean` indicating whether the FYear object contains a
+   * valid year or not.
+   *
+   * ```
+   * FYear().isValid(); // => boolean
+   * ```
+   *
+   * @public
+   */
+  isValid() {
+    return !isNaN(this._value);
+  }
+  /**
+   * Returns a new {@link FYear} object with a specified amount of years
+   * added. Specify a negative amount in order to subtract years.
+   *
+   * ```
+   * FYear().addYears(7); // => FYear
+   * ```
+   *
+   * @public
+   * @param amount - The amount of years to add to this one.
+   */
+  addYears(amount) {
+    return new _FYear(this._value + amount);
+  }
+  /**
+   * Returns a new {@link FYear} object with the year before this one.
+   *
+   * ```
+   * FYear(2025).previous(); // => FYear { 2024 }
+   * ```
+   *
+   * @public
+   */
+  previous() {
+    return this.addYears(-1);
+  }
+  /**
+   * Returns a new {@link FYear} object with the year after this one.
+   *
+   * ```
+   * FYear(2025).next(); // => FYear { 2026 }
+   * ```
+   *
+   * @public
+   */
+  next() {
+    return this.addYears(1);
+  }
+  /**
+   * Compares two {@link FYear} objects and returns `true` if they represent
+   * the same year.
+   *
+   * Invalid dates always returns `false`.
+   *
+   * @public
+   * @param rhs - The year to compare with.
+   * @returns `true` if the years are the same.
+   */
+  equals(rhs) {
+    return this._value === _FYear.toValue(rhs);
+  }
+  /**
+   * Returns true if this year is before given year.
+   *
+   * If the years are the same this function returns false.
+   *
+   * @public
+   * @param rhs - The year to compare with.
+   * @returns `true` if this year is before given year.
+   */
+  isBefore(rhs) {
+    return this._value < _FYear.toValue(rhs);
+  }
+  /**
+   * Returns true if this year is after given year.
+   *
+   * If the years are the same this function returns false.
+   *
+   * @public
+   * @param rhs - The year to compare with.
+   * @returns `true` if this year is after given year.
+   */
+  isAfter(rhs) {
+    return this._value > _FYear.toValue(rhs);
+  }
+  /**
+   * Compares two {@link FYear} objects. Returns and integer indicating
+   * whenever `a` comes before or after or is equal to `b`.
+   *
+   * - `-1` if `a` beomes before `b`.
+   * - `0` if `a` and `b` are the same year.
+   * - `1` if `a` beomes after `b`.
+   *
+   * If either or both years is invalid the result is undefined behaviour and
+   * should not be relied on. Use {@link FYear.isValid} to ensure validity
+   * first, e.g. `myArray.filter(it => it.isValid())` before sorting.
+   *
+   * @example
+   *
+   * ```ts
+   * yearArray.sort(FYear.compare);
+   * ```
+   *
+   * @public
+   * @param a - First year object to compare.
+   * @param b - Second year object to compare.
+   * @returns `-1`, `0` or `1`
+   */
+  static compare(a, b) {
+    const ax = _FYear.toFYear(a);
+    const bx = _FYear.toFYear(b);
+    const aInvalid = !ax.isValid();
+    const bInvalid = !bx.isValid();
+    if (aInvalid || bInvalid) {
+      if (aInvalid && bInvalid) {
+        return 0;
+      } else if (aInvalid) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+    return Math.max(Math.min(ax._value - bx._value, 1), -1);
+  }
+  /**
+   * Returns a string representation of the year.
+   *
+   * @public
+   */
+  toString() {
+    if (this.isValid()) {
+      return this._value.toString();
+    } else {
+      return "";
+    }
+  }
+  /**
+   * Serializes to a JSON value.
+   *
+   * @public
+   */
+  toJSON() {
+    if (this.isValid()) {
+      return this._value;
+    } else {
+      return null;
+    }
+  }
+  /**
+   * @internal
+   */
+  static toFYear(value) {
+    if (value instanceof _FYear) {
+      return value;
+    } else {
+      return _FYear.fromYear(value);
+    }
+  }
+  /**
+   * @internal
+   */
+  static toValue(value) {
+    return this.toFYear(value)._value;
+  }
+};
 function* range(begin, end) {
   if (end.isBefore(begin)) {
     const message = `Begin (${begin.toString()}) must be earlier or equal to end (${end.toString()})`;
@@ -894,6 +1145,7 @@ function groupByWeek(begin, end) {
 export {
   DateFormat,
   FDate,
+  FYear,
   Locale,
   Weekday,
   getLocale,
