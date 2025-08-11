69f61ef7e30bceb64e7d3f8b66cb080a423a94b1
udenius <udenius@users.noreply.github.com>
2025-08-11 11:12:41 +0000

    Deploy preview for PR 574 🛫

---
+++ pr-preview/pr-574/components/table-and-list/FDefinitionListLiveExample-421211.js
@@ -0,0 +1,108 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:packages/vue/src/components/FDefinitionList/examples/FDefinitionListLiveExample.vue:FDefinitionListLiveExample-421211.js
+import { defineComponent } from "vue";
+import { FCheckboxField, FDefinitionList } from "@fkui/vue";
+import { LiveExample } from "@forsakringskassan/docs-live-example";
+import { createTextVNode as _createTextVNode, resolveComponent as _resolveComponent, withCtx as _withCtx, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
+var exampleComponent = defineComponent({
+  name: "FDefinitionListLiveExample",
+  components: { FCheckboxField, LiveExample },
+  data() {
+    return {
+      useJustifiedLayout: false
+    };
+  },
+  computed: {
+    livedata() {
+      return {
+        exampleDefinitions: [
+          { term: "Skulle ha jobbat", description: "8 timmar" },
+          { term: "Vabbade", description: "8 timmar" },
+          { term: "Omfattning", description: "100 procent" }
+        ]
+      };
+    },
+    components() {
+      return {
+        FDefinitionList
+      };
+    },
+    justified() {
+      return this.useJustifiedLayout ? "justified" : "";
+    },
+    template() {
+      return (
+        /* HTML */
+        `
+                <f-definition-list :definitions="exampleDefinitions" ${this.justified} />
+            `
+      );
+    }
+  }
+});
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_f_checkbox_field = _resolveComponent("f-checkbox-field");
+  const _component_live_example = _resolveComponent("live-example");
+  return _openBlock(), _createBlock(_component_live_example, {
+    components: _ctx.components,
+    template: _ctx.template,
+    livedata: _ctx.livedata
+  }, {
+    default: _withCtx(() => [
+      _createVNode(_component_f_checkbox_field, {
+        modelValue: _ctx.useJustifiedLayout,
+        "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _ctx.useJustifiedLayout = $event),
+        value: true
+      }, {
+        default: _withCtx(() => _cache[1] || (_cache[1] = [
+          _createTextVNode(
+            " Anv\xE4nd marginaljusterad layout ",
+            -1
+            /* CACHED */
+          )
+        ])),
+        _: 1,
+        __: [1]
+      }, 8, ["modelValue"])
+    ]),
+    _: 1
+    /* STABLE */
+  }, 8, ["components", "template", "livedata"]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-421211"
+});
+export {
+  render
+};
