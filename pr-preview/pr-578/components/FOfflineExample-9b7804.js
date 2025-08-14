7af2eecfde727cc531a66a6e7cad9e3f0de8ddb2
MCFK <MCFK@users.noreply.github.com>
2025-08-14 11:00:12 +0000

    Deploy preview for PR 578 🛫

--- pr-preview/pr-578/components/FOfflineExample-9b7804.js
+++ pr-preview/pr-578/components/FOfflineExample-9b7804.js
@@ -3047,7 +3047,6 @@ var FValidationGroup_default = defineComponent9({
      *   `componentsWithError`: a list of components with errors sorted in DOM order
      *
      *   `componentCount`: number of registered components
-     * @model
      */
     modelValue: {
       type: Object,
