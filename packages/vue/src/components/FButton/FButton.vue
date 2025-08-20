8a26e5eb75a570064d232ec223b4dbb58fd9c572
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-08-20 15:00:20 +0200

    refactor: fix `defineProps` is a compiler macro (refs SFKUI-6500)

--- packages/vue/src/components/FButton/FButton.vue
+++ packages/vue/src/components/FButton/FButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, defineProps, type PropType } from "vue";
+import { computed, type PropType } from "vue";
 import { FIcon } from "../FIcon";
 
 const props = defineProps({
