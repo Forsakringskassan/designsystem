bc1f3217c420771403a7984e52789bcc0291bc27
Johan Carnegård <johan.carnegard@forsakringskassan.se>
2025-08-21 07:08:00 +0200

    feat: add semantic tokens to anchor (refs SFKUI-7312)

--- packages/vue/src/components/FMessageBox/examples/FMessageBoxIconLink.vue
+++ packages/vue/src/components/FMessageBox/examples/FMessageBoxIconLink.vue
@@ -11,14 +11,13 @@ export default defineComponent({
 <template>
     <f-message-box type="info">
         <template #default="{ headingSlotClass }">
-            <h2 :class="headingSlotClass">Meddelanderuta med länk och ikon i meddelande</h2>
-            <p>
-                <a class="anchor" href="javascript:" target="_blank">
-                    Länk
-                    <span class="sr-only"> öppnas i nytt fönster </span>
-                </a>
+            <h2 :class="headingSlotClass">Meddelanderuta med länk</h2>
+            <p>Glöm inte att klicka på länken.</p>
+            <a class="anchor anchor--block" href="javascript:" target="_blank">
+                Länk
+                <span class="sr-only"> öppnas i nytt fönster </span>
                 <f-icon name="new-window"></f-icon>
-            </p>
+            </a>
         </template>
     </f-message-box>
 </template>
