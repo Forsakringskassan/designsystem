23132d35ddf7de866fdb6f074ec7b349b219e4b4
MCFK <MCFK@users.noreply.github.com>
2025-09-12 11:27:33 +0000

    Deploy preview for PR 668 🛫

---
+++ pr-preview/pr-668/functions/plugins/FormatPluginNumber-a872a8.js
@@ -0,0 +1,93 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:packages/vue/src/plugins/format/examples/FormatPluginNumber.vue:FormatPluginNumber-a872a8.js
+import { resolveDirective as _resolveDirective, createElementVNode as _createElementVNode, withDirectives as _withDirectives, Fragment as _Fragment, openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue";
+var number = 1234567891234e-4;
+var stringNumber = "123456789.1234";
+var exampleComponent = {
+  __name: "FormatPluginNumber",
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const precisionNumber = {
+      number: 1234567891234e-4,
+      decimals: 2
+    };
+    const __returned__ = { number, stringNumber, precisionNumber };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+};
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _directive_format = _resolveDirective("format");
+  return _openBlock(), _createElementBlock(
+    _Fragment,
+    null,
+    [
+      _withDirectives(_createElementVNode(
+        "p",
+        null,
+        null,
+        512
+        /* NEED_PATCH */
+      ), [
+        [_directive_format, $setup.number, "number"]
+      ]),
+      _withDirectives(_createElementVNode(
+        "p",
+        null,
+        null,
+        512
+        /* NEED_PATCH */
+      ), [
+        [_directive_format, $setup.stringNumber, "number"]
+      ]),
+      _withDirectives(_createElementVNode(
+        "p",
+        null,
+        null,
+        512
+        /* NEED_PATCH */
+      ), [
+        [_directive_format, $setup.precisionNumber, "number"]
+      ])
+    ],
+    64
+    /* STABLE_FRAGMENT */
+  );
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-a872a8"
+});
+export {
+  render
+};
