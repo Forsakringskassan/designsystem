4c05960ec82a9e8c66ebad7b8ca2d1830f026b81
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-09-24 23:18:53 +0200

    fix: add missing `iconLibrary` prop to `FButton` (refs SFKUI-6500)

--- packages/vue/src/components/FButton/FButton.spec.ts
+++ packages/vue/src/components/FButton/FButton.spec.ts
@@ -1,4 +1,5 @@
 import { shallowMount } from "@vue/test-utils";
+import { FIcon } from "../FIcon";
 import FButton from "./FButton.vue";
 
 describe("props", () => {
@@ -11,5 +12,17 @@ describe("props", () => {
         });
         const button = wrapper.get("button");
         expect(button.attributes("type")).toBe("submit");
+    });
+
+    it("iconLibrary should be passed to FIcon", () => {
+        const wrapper = shallowMount(FButton, {
+            props: {
+                iconLeft: "foo",
+                iconLibrary: "bar",
+            },
+        });
+        const icon = wrapper.getComponent(FIcon);
+        expect(icon.attributes("name")).toBe("foo");
+        expect(icon.attributes("library")).toBe("bar");
     });
 });
