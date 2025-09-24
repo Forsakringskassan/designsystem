b594b5869114d32ea1344614d19d86cc6ebfa964
ext <ext@users.noreply.github.com>
2025-09-24 12:37:02 +0000

    Deploy preview for PR 692 🛫

---
+++ pr-preview/pr-692/functions/functions/dom-functions/get-html-element-from-vue-ref-91586a.js
@@ -0,0 +1,53 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:docs/functions/functions/dom-functions/get-html-element-from-vue-ref.md:get-html-element-from-vue-ref-91586a.js
+import { createElementVNode as _createElementVNode, openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue";
+var exampleComponent = {};
+var _hoisted_1 = { ref: "myParagraph" };
+function render(_ctx, _cache) {
+  return _openBlock(), _createElementBlock("div", null, [
+    _createElementVNode(
+      "p",
+      _hoisted_1,
+      "lorem ipsum",
+      512
+      /* NEED_PATCH */
+    )
+  ]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-91586a"
+});
+export {
+  render
+};
