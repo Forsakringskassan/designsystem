a32c412e0185954b0b0981f7620ce7c0dbd30d47
NicklasWei <NicklasWei@users.noreply.github.com>
2025-09-29 21:23:46 +0000

    Deploy preview for PR 684 🛫

--- pr-preview/pr-684/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-header-44f0a4.js
+++ pr-preview/pr-684/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-header-44f0a4.js
@@ -28,12 +28,12 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-checkbox.vue:FInteractiveTablePageObject-checkbox-174a1b.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-header.vue:FInteractiveTablePageObject-header-44f0a4.js
 import { defineComponent as _defineComponent } from "vue";
 import { FInteractiveTable, FTableColumn } from "@fkui/vue";
 import { createTextVNode as _createTextVNode, toDisplayString as _toDisplayString, withCtx as _withCtx, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
 var exampleComponent = /* @__PURE__ */ _defineComponent({
-  __name: "FInteractiveTablePageObject-checkbox",
+  __name: "FInteractiveTablePageObject-header",
   setup(__props, { expose: __expose }) {
     __expose();
     const rows = [
@@ -51,10 +51,7 @@ var exampleComponent = /* @__PURE__ */ _defineComponent({
   }
 });
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return _openBlock(), _createBlock($setup["FInteractiveTable"], {
-    rows: $setup.rows,
-    selectable: ""
-  }, {
+  return _openBlock(), _createBlock($setup["FInteractiveTable"], { rows: $setup.rows }, {
     caption: _withCtx(() => [..._cache[0] || (_cache[0] = [
       _createTextVNode(
         " Tabell ",
@@ -62,13 +59,6 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
         /* CACHED */
       )
     ])]),
-    "checkbox-description": _withCtx(({ row }) => [
-      _createTextVNode(
-        " Select row " + _toDisplayString(row.a),
-        1
-        /* TEXT */
-      )
-    ]),
     default: _withCtx(({ row }) => [
       _createVNode(
         $setup["FTableColumn"],
@@ -129,7 +119,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-174a1b"
+  selector: "#example-44f0a4"
 });
 export {
   render
