ba7c8d89772fd9454f93b3a4278b362178a67464
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2025-10-03 04:31:10 +0000

    chore(release): [skip ci] deploy documentation from main branch

--- main/components/expandable/FExpandableParagraphExample-100d51.js
+++ main/components/expandable/FExpandableParagraphExample-100d51.js
@@ -28,13 +28,13 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:packages/vue/src/components/FExpandablePanel/examples/FExpandablePanelExample.vue:FExpandablePanelExample-4ee106.js
+// virtual-entry:virtual:packages/vue/src/components/FExpandableParagraph/examples/FExpandableParagraphExample.vue:FExpandableParagraphExample-100d51.js
 import { defineComponent } from "vue";
-import { FExpandablePanel } from "@fkui/vue";
+import { FExpandableParagraph } from "@fkui/vue";
 import { createTextVNode as _createTextVNode, createElementVNode as _createElementVNode, resolveComponent as _resolveComponent, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock } from "vue";
 var exampleComponent = defineComponent({
-  name: "FExpandablePanelExample",
-  components: { FExpandablePanel },
+  name: "FExpandableParagraphExample",
+  components: { FExpandableParagraph },
   data() {
     return {
       expanded: false,
@@ -48,20 +48,23 @@ var exampleComponent = defineComponent({
   }
 });
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  const _component_f_expandable_panel = _resolveComponent("f-expandable-panel");
-  return _openBlock(), _createBlock(_component_f_expandable_panel, {
+  const _component_f_expandable_paragraph = _resolveComponent("f-expandable-paragraph");
+  return _openBlock(), _createBlock(_component_f_expandable_paragraph, {
     expanded: _ctx.expanded,
+    "header-tag": "span",
     onToggle: _ctx.onToggle
   }, {
     title: _withCtx(() => [..._cache[0] || (_cache[0] = [
       _createTextVNode(
-        " Titel ",
+        " Titel (span) ",
         -1
         /* CACHED */
       )
     ])]),
     default: _withCtx(() => [..._cache[1] || (_cache[1] = [
-      _createTextVNode(
+      _createElementVNode(
+        "span",
+        null,
         " Inneh\xE5ll ",
         -1
         /* CACHED */
@@ -87,7 +90,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-4ee106"
+  selector: "#example-100d51"
 });
 export {
   render
