3c84b6d2267c621d277238cf86d127bebf3d23e9
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2025-10-06 13:30:32 +0200

    refactor: add component tests to `FTable` (refs SFKUI-7340)

---
+++ packages/vue-labs/src/components/FTable/FTable.cy.ts
@@ -0,0 +1,132 @@
+import { type DefineComponent, defineComponent } from "vue";
+import { FTablePageObject } from "../../cypress/FTable.pageobject";
+import { FTable, defineTableColumns } from ".";
+
+const table = new FTablePageObject();
+
+interface Row {
+    id: string;
+    name: string;
+    input: string;
+    check: boolean;
+}
+
+const columns = defineTableColumns<Row>([
+    {
+        type: "rowheader",
+        header: "Name",
+        key: "name",
+    },
+    {
+        type: "text",
+        header: "Input",
+        key: "input",
+        editable: true,
+        value(row) {
+            return row.input;
+        },
+    },
+    {
+        type: "checkbox",
+        header: "Kryssruta",
+        key: "check",
+        editable: true,
+    },
+    {
+        type: "button",
+        header: "Button",
+        icon: "trashcan",
+        value(row) {
+            return `Ta bort ${row.id}`;
+        },
+    },
+    {
+        header: "Anchor",
+        type: "anchor",
+        href: "#",
+        value() {
+            return "Link";
+        },
+    },
+    {
+        type: "text",
+        header: "ID",
+        value(row) {
+            return row.id;
+        },
+    },
+]);
+
+function createComponent(
+    expandable?: "custom" | "rows",
+    selectable?: "multi" | "single",
+): DefineComponent {
+    const expandableSlot =
+        expandable === "custom"
+            ? "<template #expandable='{ row }'> {{ row.name }} </template>"
+            : "";
+    return defineComponent({
+        template: /* HTML */ `
+            <f-table
+                :rows
+                :columns
+                selectable="${selectable}"
+                expandable-attribute="${expandable ? "nested" : undefined}"
+            >
+                ${expandableSlot}
+            </f-table>
+        `,
+        components: {
+            FTable,
+        },
+        data() {
+            return {
+                rows: [
+                    {
+                        id: "1",
+                        name: "a",
+                        input: "input-a",
+                        check: false,
+                        nested: [
+                            {
+                                id: "1-1",
+                                name: "a-a",
+                                input: "input-a-a",
+                                check: false,
+                            },
+                        ],
+                    },
+                    {
+                        id: "2",
+                        name: "b",
+                        input: "input-b",
+                        check: false,
+                        nested: [
+                            {
+                                id: "2-1",
+                                name: "b-a",
+                                input: "input-b-a",
+                                check: false,
+                            },
+                        ],
+                    },
+                    {
+                        id: "3",
+                        name: "c",
+                        input: "input-c",
+                        check: false,
+                        nested: [
+                            {
+                                id: "3-1",
+                                name: "c-a",
+                                input: "input-c-a",
+                                check: false,
+                            },
+                        ],
+                    },
+                ],
+                columns,
+            };
+        },
+    });
+}
