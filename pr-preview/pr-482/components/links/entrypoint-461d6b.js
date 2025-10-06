3b9a0aaacd8d1efdb92869a30999ea684e662d3f
johancarnegard <johancarnegard@users.noreply.github.com>
2025-10-06 05:46:17 +0000

    Deploy preview for PR 482 🛫

--- pr-preview/pr-482/components/links/entrypoint-461d6b.js
+++ pr-preview/pr-482/components/links/entrypoint-461d6b.js
@@ -28,7 +28,7 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/components/links/entrypoint.md:entrypoint-c2fadf.js
+// virtual-entry:virtual:docs/components/links/entrypoint.md:entrypoint-461d6b.js
 import { FIcon } from "@fkui/vue";
 import { createElementVNode as _createElementVNode, createVNode as _createVNode, createTextVNode as _createTextVNode, openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue";
 var exampleComponent = {
@@ -69,7 +69,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-c2fadf"
+  selector: "#example-461d6b"
 });
 export {
   render
