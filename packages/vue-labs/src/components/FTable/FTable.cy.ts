9d4097df8a70b714e840072af5abf95e8604c078
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2025-10-13 15:43:20 +0200

    refactor: updated test (refs SFKUI-6500)

--- packages/vue-labs/src/components/FTable/FTable.cy.ts
+++ packages/vue-labs/src/components/FTable/FTable.cy.ts
@@ -1,22 +1,27 @@
 import { type DefineComponent, defineComponent } from "vue";
 import { FTablePageObject } from "../../cypress/FTable.pageobject";
 import { FTable, defineTableColumns } from ".";
-import { should } from "chai";
 
 const table = new FTablePageObject();
 
 interface Row {
-    id: string;
-    name: string;
+    rowheader: string;
+    text: string;
     input: string;
-    check: boolean;
+    button: string;
+    anchor: string;
 }
 
 const columns = defineTableColumns<Row>([
     {
         type: "rowheader",
-        header: "Name",
-        key: "name",
+        header: "Rowheader",
+        key: "rowheader",
+    },
+    {
+        type: "text",
+        header: "Text",
+        key: "text",
     },
     {
         type: "text",
@@ -28,53 +33,49 @@ const columns = defineTableColumns<Row>([
         },
     },
     {
-        type: "checkbox",
-        header: "Kryssruta",
-        key: "check",
-        editable: true,
-    },
-    {
         type: "button",
         header: "Button",
+        key: "button",
         icon: "trashcan",
         value(row) {
-            return `Ta bort ${row.id}`;
+            return row.button;
         },
     },
     {
         header: "Anchor",
         type: "anchor",
+        key: "anchor",
         href: "#",
-        value() {
-            return "Link";
-        },
-    },
-    {
-        type: "text",
-        header: "ID",
         value(row) {
-            return row.id;
+            return row.anchor;
         },
     },
 ]);
 
-function createComponent(
-    expandable?: "custom" | "rows",
-    selectable?: "multi" | "single",
-    empty?: string
-): DefineComponent {
+interface TableOptions {
+    expandable?: "custom" | "rows";
+    selectable?: "multi" | "single";
+    striped?: boolean;
+    empty?: boolean;
+}
+
+function createComponent(options: TableOptions = {}): DefineComponent {
+    const { selectable, expandable, striped, empty } = options;
+    const stripedAttr = striped ? "striped" : "";
+    const expandableAttr = expandable ? 'expandable-attribute="nested"' : "";
     const expandableSlot =
         expandable === "custom"
             ? "<template #expandable='{ row }'> {{ row.name }} </template>"
             : "";
+
     return defineComponent({
         template: /* HTML */ `
             <f-table
-                :rows="${empty==="true" ? []:"rows"}"
+                :rows="${empty ? "[]" : "rows"}"
                 :columns
                 selectable="${selectable}"
-                expandable-attribute="${expandable ? "nested" : undefined}"
-                striped
+                ${expandableAttr}
+                ${stripedAttr}
             >
                 ${expandableSlot}
             </f-table>
@@ -86,46 +87,34 @@ function createComponent(
             return {
                 rows: [
                     {
-                        id: "1",
-                        name: "a",
-                        input: "input-a",
-                        check: false,
+                        rowheader: "A1",
+                        text: "A2",
+                        input: "A3",
+                        button: "A4",
+                        anchor: "A5",
                         nested: [
                             {
-                                id: "1-1",
-                                name: "a-a",
-                                input: "input-a-a",
-                                check: false,
+                                rowheader: "A-A1",
+                                text: "A-A2",
+                                input: "A-A3",
+                                button: "A-A4",
+                                anchor: "A-A5",
+                            },
+                            {
+                                rowheader: "A-B1",
+                                text: "A-B2",
+                                input: "A-B3",
+                                button: "A-B4",
+                                anchor: "A-B5",
                             },
                         ],
                     },
                     {
-                        id: "2",
-                        name: "b",
-                        input: "input-b",
-                        check: false,
-                        nested: [
-                            {
-                                id: "2-1",
-                                name: "b-a",
-                                input: "input-b-a",
-                                check: false,
-                            },
-                        ],
-                    },
-                    {
-                        id: "3",
-                        name: "c",
-                        input: "input-c",
-                        check: false,
-                        nested: [
-                            {
-                                id: "3-1",
-                                name: "c-a",
-                                input: "input-c-a",
-                                check: false,
-                            },
-                        ],
+                        rowheader: "B1",
+                        text: "B2",
+                        input: "B3",
+                        button: "B4",
+                        anchor: "B5",
                     },
                 ],
                 columns,
@@ -134,72 +123,74 @@ function createComponent(
     });
 }
 
-beforeEach(()=>{
+beforeEach(() => {
     cy.viewport(1024, 768);
-})
+});
 
 it("Should set correct headertext 1.3", () => {
     cy.mount(createComponent());
-        table.header(1).should("contain.text", "Name");
-        table.header(2).should("contain.text", "Input");
-        table.header(3).should("contain.text", "Kryssruta");
-        table.header(4).should("contain.text", "Button");
-        table.header(5).should("contain.text", "Anchor");
-        table.header(6).should("contain.text", "ID");
+    table.header(1).should("contain.text", "Rowheader");
+    table.header(2).should("contain.text", "Text");
+    table.header(3).should("contain.text", "Input");
+    table.header(4).should("contain.text", "Button");
+    table.header(5).should("contain.text", "Anchor");
 });
 
-it.skip("should set rowheader 1.4", () => {
-    cy.mount(createComponent());
-    table.cell({row:1, col:1}).should("have.prop", "tagName", "TH");
-    table.cell({row:2, col:1}).should("have.prop", "tagName", "TH");
-    table.cell({row:3, col:1}).should("have.prop", "tagName", "TH");
+describe("1.4", () => {
+    it("should set rowheader 1.4", () => {
+        cy.mount(createComponent());
+        table.cell({ row: 1, col: 1 }).should("have.prop", "tagName", "TH");
+        table.cell({ row: 2, col: 1 }).should("have.prop", "tagName", "TH");
+    });
+
+    it("should set rowheader on expandable rows 1.4", () => {
+        cy.mount(createComponent({ expandable: "rows" }));
+        table.expandButton(1).click();
+        table.cell({ row: 1, col: 2 }).should("have.prop", "tagName", "TH");
+        table.cell({ row: 2, col: 2 }).should("have.prop", "tagName", "TH");
+        table.cell({ row: 3, col: 2 }).should("have.prop", "tagName", "TH");
+        table.cell({ row: 4, col: 2 }).should("have.prop", "tagName", "TH");
+    });
 });
 
-it.skip("should set rowheader on expandable rows 1.4", () => {
-    cy.mount(createComponent("rows"));
-    table.expandButton(1).click();
-    table.cell({row:2, col:1}).should("have.prop", "tagName", "TH");
+describe("1.5", () => {
+    it("should have correct class for striped 1.5", () => {
+        cy.mount(createComponent({ striped: true }));
+        table.el().should("have.class", "table-ng--striped");
+    });
+
+    it.skip("should have correct striped appearence 1.5", () => {
+        cy.mount(createComponent({ striped: true }));
+        cy.toMatchScreenshot();
+    });
+
+    it.skip("should have correct seperator when not striped 1.5", () => {
+        cy.mount(createComponent());
+        cy.toMatchScreenshot();
+    });
+
+    it.skip("should have correct class for expandable 1.5", () => {
+        cy.mount(createComponent({ expandable: "rows", striped: true }));
+        table.expandButton(1).click();
+        cy.toMatchScreenshot();
+    });
 });
 
-it.skip("should have correct class for striped 1.5", () => {
-    cy.mount(createComponent("rows", "multi"));
-    table.el().should("have.class", "table-ng--striped");
-});
-
-it.skip("should have correct strip 1.5", () => {
-    cy.mount(createComponent("rows", "multi"));    
-    cy.toMatchScreenshot();
-});
-it.skip("should have correct seperator when not striped 1.5", () => {
-    cy.mount(createComponent("rows", "multi"));    
-    cy.toMatchScreenshot();
-});
-
-it.skip("should have correct class for expandable 1.5", () => {
-    cy.mount(createComponent("rows", "multi"));
-    table.expandButton(1).click();
-    cy.toMatchScreenshot();
-});
-
-it.skip("should have empty row 1.8", () => {
-    cy.mount(createComponent("rows", "multi", "true"));
-    table.cell({row:1, col:1}).should("have.attr", "colspan", "8");
+it.only("should have empty row 1.8", () => {
+    cy.mount(createComponent({ empty: true }));
+    table.cell({ row: 1, col: 1 }).should("have.attr", "colspan", "6");
     table.rows().should("have.length", 1);
-     table.cell({row:1, col:1}).should("contain.text", "Tabellen är tom");
-   
+    table.cell({ row: 1, col: 1 }).should("contain.text", "Tabellen är tom");
 });
+
 it("should have correct focus 1.10", () => {
-    cy.mount(createComponent("rows", "multi"));
+    cy.mount(createComponent());
     table.tabbableElement().focus();
-    cy.focused().trigger("keydown", {code:"ArrowRight"});
-    table.selectInput(1).should("have.focus");
+    cy.focused().trigger("keydown", { code: "ArrowRight" });
+    table.cell({ row: 1, col: 2 }).should("have.focus");
 });
 
 it("should have correct checkbox labels 7.6", () => {
-    cy.mount(createComponent("rows", "multi"));
+    cy.mount(createComponent({ selectable: "multi" }));
     table.selectInput(1).should("have.attr", "aria-label", "selectable");
 });
-
-
-
-
