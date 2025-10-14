e203386eeed74ec0145e970aa933941f5c648c26
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-10-14 16:59:17 +0200

    fixup! refactor: tests for normalizeColumn (refs SFKUI-6500)

--- packages/vue-labs/src/components/FTable/table-column.spec.ts
+++ packages/vue-labs/src/components/FTable/table-column.spec.ts
@@ -1,3 +1,4 @@
+import { ref } from "vue";
 import { defineTableColumn, normalizeTableColumn } from "./table-column";
 
 interface Item {
@@ -13,35 +14,70 @@ const bar: Item = {
 };
 
 describe("text", () => {
-    it("should default to not editable", () => {
-        expect.assertions(1);
-        const column = defineTableColumn<Item>({
-            type: "text",
+    describe("header property", () => {
+        it("should default to empty text", () => {
+            expect.assertions(1);
+            const column = defineTableColumn<Item>({
+                type: "text",
+            });
+            const result = normalizeTableColumn(column);
+            expect(result.header.value).toBe("");
         });
-        const result = normalizeTableColumn(column);
-        expect(result.editable(foo)).toBeFalsy();
+
+        it("should accept string", () => {
+            expect.assertions(1);
+            const column = defineTableColumn<Item>({
+                type: "text",
+                header: "lorem ipsum",
+            });
+            const result = normalizeTableColumn(column);
+            expect(result.header.value).toBe("lorem ipsum");
+        });
+
+        it("should accept ref", () => {
+            expect.assertions(1);
+            const header = ref("");
+            const column = defineTableColumn<Item>({
+                type: "text",
+                header,
+            });
+            header.value = "lorem ipsum";
+            const result = normalizeTableColumn(column);
+            expect(result.header.value).toBe("lorem ipsum");
+        });
     });
 
-    it("should accept editable boolean property", () => {
-        expect.assertions(1);
-        const column = defineTableColumn<Item>({
-            type: "text",
-            editable: true,
+    describe("editable property", () => {
+        it("should default to false", () => {
+            expect.assertions(1);
+            const column = defineTableColumn<Item>({
+                type: "text",
+            });
+            const result = normalizeTableColumn(column);
+            expect(result.editable(foo)).toBeFalsy();
         });
-        const result = normalizeTableColumn(column);
-        expect(result.editable(foo)).toBeTruthy();
-    });
 
-    it("should accept editable callback property", () => {
-        expect.assertions(2);
-        const column = defineTableColumn<Item>({
-            type: "text",
-            editable(item) {
-                return item.value === "bar";
-            },
+        it("should accept boolean", () => {
+            expect.assertions(1);
+            const column = defineTableColumn<Item>({
+                type: "text",
+                editable: true,
+            });
+            const result = normalizeTableColumn(column);
+            expect(result.editable(foo)).toBeTruthy();
         });
-        const result = normalizeTableColumn(column);
-        expect(result.editable(foo)).toBeFalsy();
-        expect(result.editable(bar)).toBeTruthy();
+
+        it("should accept callback", () => {
+            expect.assertions(2);
+            const column = defineTableColumn<Item>({
+                type: "text",
+                editable(item) {
+                    return item.value === "bar";
+                },
+            });
+            const result = normalizeTableColumn(column);
+            expect(result.editable(foo)).toBeFalsy();
+            expect(result.editable(bar)).toBeTruthy();
+        });
     });
 });
