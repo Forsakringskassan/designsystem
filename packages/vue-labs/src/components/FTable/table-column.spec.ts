4534f31f31076ddbd8e86dec6d7843c00fc502d4
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-10-15 15:18:32 +0200

    fixup! fixup! refactor: tests for normalizeColumn (refs SFKUI-6500)

--- packages/vue-labs/src/components/FTable/table-column.spec.ts
+++ packages/vue-labs/src/components/FTable/table-column.spec.ts
@@ -47,6 +47,39 @@ describe("text", () => {
         });
     });
 
+    describe("description property", () => {
+        it("should default to empty text", () => {
+            expect.assertions(1);
+            const column = defineTableColumn<Item>({
+                type: "text",
+            });
+            const result = normalizeTableColumn(column);
+            expect(result.description.value).toBe("");
+        });
+
+        it("should accept string", () => {
+            expect.assertions(1);
+            const column = defineTableColumn<Item>({
+                type: "text",
+                description: "lorem ipsum",
+            });
+            const result = normalizeTableColumn(column);
+            expect(result.description.value).toBe("lorem ipsum");
+        });
+
+        it("should accept ref", () => {
+            expect.assertions(1);
+            const description = ref("");
+            const column = defineTableColumn<Item>({
+                type: "text",
+                description,
+            });
+            description.value = "lorem ipsum";
+            const result = normalizeTableColumn(column);
+            expect(result.description.value).toBe("lorem ipsum");
+        });
+    });
+
     describe("editable property", () => {
         it("should default to false", () => {
             expect.assertions(1);
@@ -78,6 +111,41 @@ describe("text", () => {
             const result = normalizeTableColumn(column);
             expect(result.editable(foo)).toBeFalsy();
             expect(result.editable(bar)).toBeTruthy();
+        });
+    });
+
+    describe("key", () => {
+        it("should generate value function", () => {
+            expect.assertions(2);
+            const column = defineTableColumn<Item>({
+                type: "text",
+                key: "value",
+            });
+            const result = normalizeTableColumn(column);
+            expect(result.value(foo)).toBe("foo");
+            expect(result.value(bar)).toBe("bar");
+        });
+
+        it("should generate update function", () => {
+            expect.assertions(2);
+            const column = defineTableColumn<Item>({
+                type: "text",
+                key: "value",
+            });
+            const result = normalizeTableColumn(column);
+            result.update(foo, "updated", foo.value);
+            expect(result.value(foo)).toBe("updated");
+            expect(result.value(bar)).toBe("bar");
+        });
+
+        it("should define sorting", () => {
+            expect.assertions(1);
+            const column = defineTableColumn<Item>({
+                type: "text",
+                key: "value",
+            });
+            const result = normalizeTableColumn(column);
+            expect(result.sortable).toBe("value");
         });
     });
 });
