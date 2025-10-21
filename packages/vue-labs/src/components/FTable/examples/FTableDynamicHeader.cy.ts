d5d2f090c5a5c055dc8473607a29eae19a0778fb
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2025-10-21 11:08:49 +0200

    refactor: update tests to use `FTablePageObject` (refs SFKUI-7357)

--- packages/vue-labs/src/components/FTable/examples/FTableDynamicHeader.cy.ts
+++ packages/vue-labs/src/components/FTable/examples/FTableDynamicHeader.cy.ts
@@ -1,15 +1,18 @@
+import { FTablePageObject } from "../../../cypress";
 import Example from "./FTableDynamicHeader.vue";
+
+const table = new FTablePageObject();
 
 it("should update column headers", () => {
     cy.mount(Example);
-    cy.get("thead th:nth-child(1)").should("contain.text", "Column 1");
-    cy.get("thead th:nth-child(2)").should("contain.text", "Column 2");
-    cy.get("thead th:nth-child(3)").should("contain.text", "Column 3");
+    table.headerTitle(1).should("contain.text", "Column 1");
+    table.headerTitle(2).should("contain.text", "Column 2");
+    table.headerTitle(3).should("contain.text", "Column 3");
     cy.get("[data-test=input-1] input").type("{selectall}{del}foo");
     cy.get("[data-test=input-2] input").type("{selectall}{del}bar");
     cy.get("[data-test=input-3] input").type("{selectall}{del}baz");
-    cy.get("table [tabindex=0]").focus();
-    cy.get("thead th:nth-child(1)").should("contain.text", "foo");
-    cy.get("thead th:nth-child(2)").should("contain.text", "bar");
-    cy.get("thead th:nth-child(3)").should("contain.text", "baz");
+    table.tabbableElement().focus();
+    table.headerTitle(1).should("contain.text", "foo");
+    table.headerTitle(2).should("contain.text", "bar");
+    table.headerTitle(3).should("contain.text", "baz");
 });
