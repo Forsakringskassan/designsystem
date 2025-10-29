7b1b563d3f507168f2b1e30222e8d0058b77e3ce
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-10-29 13:36:07 +0100

    refactor: more readable code and output (refs SFKUI-6500)

--- packages/theme-default/extractVariables.js
+++ packages/theme-default/extractVariables.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
-const { moduleReturnVariables } = require("get-css-variables");
+const fs = require("node:fs");
+const path = require("node:path");
+const { getVariables } = require("get-css-variables");
 const { globSync } = require("glob");
 
 globSync("dist/*.css").forEach((file) => {
-    const content = moduleReturnVariables(fs.readFileSync(file, "utf-8"));
-    const fileName = path.basename(file, path.extname(file));
-    const dts = [
+    const variables = getVariables(fs.readFileSync(file, "utf-8"));
+    const value = `const value = ${JSON.stringify(variables, null, 2)};`;
+    const cjs = [value, `module.exports = value;`].join("\n");
+    const cts = [
         `declare const value: Record<string, string>;`,
         `export = value;`,
     ].join("\n");
-    fs.writeFileSync(`dist/${fileName}.js`, content);
-    fs.writeFileSync(`dist/${fileName}.d.ts`, dts);
+    const { name } = path.parse(file);
+    fs.writeFileSync(`dist/${name}.js`, cjs);
+    fs.writeFileSync(`dist/${name}.d.ts`, cts);
 });
 
 fs.mkdirSync("dist", { recursive: true });
