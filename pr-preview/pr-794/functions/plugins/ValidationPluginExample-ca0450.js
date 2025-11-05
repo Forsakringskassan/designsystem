fd64e110168875713854dc55f981fb880652a71b
dvitamin <dvitamin@users.noreply.github.com>
2025-11-05 16:53:29 +0000

    Deploy preview for PR 794 🛫

---
+++ pr-preview/pr-794/functions/plugins/ValidationPluginExample-ca0450.js
@@ -0,0 +1,75 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:packages/vue/src/plugins/validation/examples/ValidationPluginExample.vue:ValidationPluginExample-ca0450.js
+import { defineComponent } from "vue";
+import { FTextField } from "@fkui/vue";
+import { createTextVNode as _createTextVNode, resolveComponent as _resolveComponent, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives } from "vue";
+var exampleComponent = defineComponent({
+  name: "ValidationPluginExample",
+  components: { FTextField }
+});
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_f_text_field = _resolveComponent("f-text-field");
+  const _directive_validation = _resolveDirective("validation");
+  return _withDirectives((_openBlock(), _createBlock(_component_f_text_field, null, {
+    default: _withCtx(() => [..._cache[0] || (_cache[0] = [
+      _createTextVNode(
+        " Namn p\xE5 barn ",
+        -1
+        /* CACHED */
+      )
+    ])]),
+    _: 1
+    /* STABLE */
+  })), [
+    [
+      _directive_validation,
+      {
+        maxLength: { length: 20 },
+        minLength: { length: 2 }
+      },
+      void 0,
+      {
+        required: true,
+        maxLength: true,
+        minLength: true
+      }
+    ]
+  ]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-ca0450"
+});
+export {
+  render
+};
