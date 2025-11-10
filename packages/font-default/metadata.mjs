fb0eff5d2f60c67eefdcb88fdcc02358d28e9b40
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-11-11 00:23:20 +0100

    refactor: migrate metadata to js instead of json (refs SFKUI-6500)

---
+++ packages/font-default/metadata.mjs
@@ -0,0 +1,15 @@
+/**
+ * @typedef {Object} Asset {
+ * @property {string} filename
+ */
+
+/** @type {Asset[]} */
+const assets = [
+    { filename: "fonts/Inter-Bold.woff2" },
+    { filename: "fonts/Inter-Light.woff2" },
+    { filename: "fonts/Inter-Medium.woff2" },
+    { filename: "fonts/Inter-Regular.woff2" },
+    { filename: "fonts/Inter-SemiBold.woff2" },
+];
+
+export default assets;
