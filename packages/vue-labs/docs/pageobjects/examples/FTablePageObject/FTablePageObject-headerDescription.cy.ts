f94b732cf88710ddab31a1719e06b1c90b6040d0
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2025-11-13 22:53:20 +0100

    docs: `FTablePageObject` docs use `data-test` selector (refs SFKUI-6500)

--- packages/vue-labs/docs/pageobjects/examples/FTablePageObject/FTablePageObject-headerDescription.cy.ts
+++ packages/vue-labs/docs/pageobjects/examples/FTablePageObject/FTablePageObject-headerDescription.cy.ts
@@ -5,7 +5,7 @@ it("headerDescription() should get correct header description text.", () => {
     cy.mount(Example);
 
     /* --- cut above --- */
-    const table = new FTablePageObject();
+    const table = new FTablePageObject("[data-test=table]");
     table.headerDescription(1).should("contain.text", "Description 1");
     table.headerDescription(2).should("contain.text", "Description 2");
     /* --- cut below --- */
--- packages/vue-labs/docs/pageobjects/examples/FTablePageObject/FTablePageObject-rows.cy.ts
+++ packages/vue-labs/docs/pageobjects/examples/FTablePageObject/FTablePageObject-rows.cy.ts
@@ -5,7 +5,7 @@ it("rows() should get correct number of rows.", () => {
     cy.mount(Example);
 
     /* --- cut above --- */
-    const table = new FTablePageObject();
+    const table = new FTablePageObject("[data-test=table]");
     table.rows().should("have.length", 3);
     /* --- cut below --- */
 });
