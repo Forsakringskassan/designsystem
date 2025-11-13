defaeefb99fbcdcead13be6d3b849f0aac9ccdcb
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2025-11-13 22:53:20 +0100

    docs: `FTablePageObject` docs use `data-test` selector (refs SFKUI-6500)

--- packages/vue-labs/docs/pageobjects/examples/FTablePageObject/FTablePageObject-headerTitle.cy.ts
+++ packages/vue-labs/docs/pageobjects/examples/FTablePageObject/FTablePageObject-headerTitle.cy.ts
@@ -5,7 +5,7 @@ it("headerTitle() should get correct header title text.", () => {
     cy.mount(Example);
 
     /* --- cut above --- */
-    const table = new FTablePageObject();
+    const table = new FTablePageObject("[data-test=table]");
     table.headerTitle(1).should("contain.text", "ID");
     table.headerTitle(2).should("contain.text", "Namn");
     /* --- cut below --- */
--- packages/vue-labs/docs/pageobjects/examples/FTablePageObject/FTablePageObject-selectHeaderInput.cy.ts
+++ packages/vue-labs/docs/pageobjects/examples/FTablePageObject/FTablePageObject-selectHeaderInput.cy.ts
@@ -5,7 +5,7 @@ it("selectHeaderInput() should select all rows.", () => {
     cy.mount(Example);
 
     /* --- cut above --- */
-    const table = new FTablePageObject();
+    const table = new FTablePageObject("[data-test=table]");
     table.selectHeaderInput().click();
     table.selectInput(1).should("be.checked");
     table.selectInput(2).should("be.checked");
