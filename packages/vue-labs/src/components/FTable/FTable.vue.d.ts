c59dcd1046df39f4cab15ffb7f89825a7a6ad458
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-11-18 11:32:33 +0100

    build: add hack to get FTable.vue working in jest tests (refs SFKUI-6500)

---
+++ packages/vue-labs/src/components/FTable/FTable.vue.d.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type -- generated file */
+
+import { type PublicProps, type VNode } from "vue";
+
+declare function FTable<
+    T,
+    KeyAttribute extends keyof T = keyof T,
+    ExpandableAttribute extends keyof T = keyof T,
+>(
+    props: PublicProps,
+    ctx?: Pick<NonNullable<Awaited<typeof setup>>, "attrs" | "emit" | "slots">,
+    expose?: NonNullable<Awaited<typeof setup>>["expose"],
+    setup?: Promise<{
+        props: PublicProps;
+        expose: (exposed: {}) => void;
+        attrs: any;
+        slots: {};
+        emit: {};
+    }>,
+): VNode & {
+    __ctx?: Awaited<typeof setup>;
+};
+
+declare const value: typeof FTable;
+export default value;
