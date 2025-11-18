1d1db991f181bf0bda4ee05c89cb39456a300c7f
NicklasWei <NicklasWei@users.noreply.github.com>
2025-11-18 12:24:07 +0000

    Deploy preview for PR 814 🛫

--- pr-preview/pr-814/functions/cypress/pageobjects/FDetailsPanelPageObject/FDetailsPanelPageObject-0b33d2.js
+++ pr-preview/pr-814/functions/cypress/pageobjects/FDetailsPanelPageObject/FDetailsPanelPageObject-0b33d2.js
@@ -28,41 +28,45 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject-status.vue:FBadgePageObject-status-5ed784.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FDetailsPanelPageObject/FDetailsPanelPageObject.vue:FDetailsPanelPageObject-0b33d2.js
 import { defineComponent as _defineComponent } from "vue";
-import { FBadge } from "@fkui/vue";
-import { createTextVNode as _createTextVNode, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives } from "vue";
+import { onMounted } from "vue";
+import { FDetailsPanel, FPageLayout, useDetailsPanel } from "@fkui/vue";
+import { createVNode as _createVNode, createElementVNode as _createElementVNode, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock } from "vue";
 var exampleComponent = /* @__PURE__ */ _defineComponent({
-  __name: "FBadgePageObject-status",
+  __name: "FDetailsPanelPageObject",
   setup(__props, { expose: __expose }) {
     __expose();
-    const __returned__ = { get FBadge() {
-      return FBadge;
+    onMounted(() => {
+      useDetailsPanel("awesome-panel").open("foo");
+    });
+    const __returned__ = { get FDetailsPanel() {
+      return FDetailsPanel;
+    }, get FPageLayout() {
+      return FPageLayout;
     } };
     Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
     return __returned__;
   }
 });
+var _hoisted_1 = ["slot"];
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  const _directive_test = _resolveDirective("test");
-  return _withDirectives((_openBlock(), _createBlock($setup["FBadge"], { status: "warning" }, {
-    default: _withCtx(() => [..._cache[0] || (_cache[0] = [
-      _createTextVNode(
-        " MyBadge ",
-        -1
-        /* CACHED */
-      )
-    ])]),
+  return _openBlock(), _createBlock($setup["FPageLayout"], { layout: "three-column" }, {
+    default: _withCtx(({ left, content }) => [
+      _createVNode($setup["FDetailsPanel"], {
+        slot: left,
+        name: "awesome-panel"
+      }, null, 8, ["slot"]),
+      _createElementVNode("div", { slot: content }, "Inneh\xE5ll", 8, _hoisted_1)
+    ]),
     _: 1
     /* STABLE */
-  })), [
-    [_directive_test, "my-badge-warning"]
-  ]);
+  });
 }
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-5ed784"
+  selector: "#example-0b33d2"
 });
 export {
   render
