535c9b54b897662bd12e4ce134c70877e8d310f0
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2025-11-20 13:37:27 +0100

    refactor: new `PaginationPageButtonsExample` example (refs SFKUI-6500)

--- docs/components/pagination.md
+++ docs/components/pagination.md
@@ -14,6 +14,11 @@ Innehållet kan till exempel vara sökresultat eller tabeller.
 PaginationExample.vue
 ```
 
+## Sidknappar eller sidräknare
+
+Sidknappar visas som standard.
+Sidräknare ersätter sidknapparna om paginatorn är för smal för att kunna visa sidknappar.
+
 ## Dynamisk hämtning av data
 
 Det går att använda dynamisk hämtning av data. Det innebär att komponenten hämtar enbart det data som visas på den aktuella sidan istället för att göra ett urval av alla objekt.
---
+++ packages/vue/src/design-component-tests/Pagination/examples/PaginationPageButtonsExample.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { FPaginateDataset, FPaginator } from "@fkui/vue";
+import { LiveExample } from "@forsakringskassan/docs-live-example";
+import { persons } from "./pagination-data";
+
+export default defineComponent({
+    name: "PaginationExample",
+    components: {
+        LiveExample,
+    },
+    data() {
+        return {};
+    },
+    computed: {
+        components(): object {
+            return {
+                FPaginateDataset,
+                FPaginator,
+            };
+        },
+        livedata(): object {
+            return {
+                rows: persons,
+            };
+        },
+        template(): string {
+            return /* HTML */ `
+                <f-paginate-dataset :items="rows">
+                    <template #default="{ items: currentPageItems }">
+                        <f-paginator />
+                    </template>
+                </f-paginate-dataset>
+            `;
+        },
+    },
+});
+</script>
+
+<template>
+    <live-example :components :template :livedata></live-example>
+</template>
