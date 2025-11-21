aae381d932cbbb461f511f5ab6bf0951f93d8bd7
udenius <udenius@users.noreply.github.com>
2025-11-21 10:16:25 +0000

    Deploy preview for PR 677 🛫

--- pr-preview/pr-677/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject-status-1ea00c.js
+++ pr-preview/pr-677/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject-status-1ea00c.js
@@ -28,7 +28,7 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject-status.vue:FBadgePageObject-status-d110ab.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FBadgePageObject/FBadgePageObject-status.vue:FBadgePageObject-status-1ea00c.js
 import { defineComponent as _defineComponent } from "vue";
 import { FBadge } from "@fkui/vue";
 import { createTextVNode as _createTextVNode, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives } from "vue";
@@ -62,7 +62,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-d110ab"
+  selector: "#example-1ea00c"
 });
 export {
   render
