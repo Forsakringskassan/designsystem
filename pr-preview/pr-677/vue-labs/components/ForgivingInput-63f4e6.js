abaf3ddc4f1ccacdcbb364384f8875466af18a1f
udenius <udenius@users.noreply.github.com>
2025-11-24 11:13:00 +0000

    Deploy preview for PR 677 🛫

--- pr-preview/pr-677/vue-labs/components/ForgivingInput-63f4e6.js
+++ pr-preview/pr-677/vue-labs/components/ForgivingInput-63f4e6.js
@@ -446,10 +446,10 @@ function requireSharedStore() {
   var SHARED = "__core-js_shared__";
   var store = sharedStore.exports = globalThis2[SHARED] || defineGlobalProperty2(SHARED, {});
   (store.versions || (store.versions = [])).push({
-    version: "3.46.0",
+    version: "3.47.0",
     mode: IS_PURE ? "pure" : "global",
     copyright: "\xA9 2014-2025 Denis Pushkarev (zloirock.ru), 2025 CoreJS Company (core-js.io)",
-    license: "https://github.com/zloirock/core-js/blob/v3.46.0/LICENSE",
+    license: "https://github.com/zloirock/core-js/blob/v3.47.0/LICENSE",
     source: "https://github.com/zloirock/core-js"
   });
   return sharedStore.exports;
