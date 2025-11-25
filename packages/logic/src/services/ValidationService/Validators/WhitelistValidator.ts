25481215420d3db8711c615581a5c5f3490dfc34
Eklund James (66131523) <james.eklund@forsakringskassan.se>
2025-11-25 13:27:48 +0100

    fix: use untrimmed value for whitelist validation (fixes SFKUI-7421)

--- packages/logic/src/services/ValidationService/Validators/WhitelistValidator.ts
+++ packages/logic/src/services/ValidationService/Validators/WhitelistValidator.ts
@@ -6,7 +6,8 @@ const WHITELIST_REGEXP = /^[a-zA-Z0-9 .,\-()\r\n?+=!:@*\xC0-\xFF]*$/;
 export const whitelistValidator: Validator = {
     name: "whitelist",
     instant: true,
-    validation(value) {
+    validation(_value, element) {
+        const value = "value" in element ? element.value : "";
         return isEmpty(value) || WHITELIST_REGEXP.test(value);
     },
 };
