a475bd413516b9320a189aeb327860b496523859
ext <ext@users.noreply.github.com>
2025-11-27 08:39:09 +0000

    Deploy preview for PR 838 🛫

--- pr-preview/pr-838/functions/cypress/pageobjects/FLabelPageObject/FLabelPageObject-error-icon-c6b34a.js
+++ pr-preview/pr-838/functions/cypress/pageobjects/FLabelPageObject/FLabelPageObject-error-icon-c6b34a.js
@@ -28,12 +28,12 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FLabelPageObject/FLabelPageObject-error-message.vue:FLabelPageObject-error-message-7185d6.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FLabelPageObject/FLabelPageObject-error-icon.vue:FLabelPageObject-error-icon-c6b34a.js
 import { defineComponent as _defineComponent } from "vue";
 import { FTextField, FValidationForm } from "@fkui/vue";
 import { createTextVNode as _createTextVNode, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives, createElementVNode as _createElementVNode } from "vue";
 var exampleComponent = /* @__PURE__ */ _defineComponent({
-  __name: "FLabelPageObject-error-message",
+  __name: "FLabelPageObject-error-icon",
   setup(__props, { expose: __expose }) {
     __expose();
     const __returned__ = { get FTextField() {
@@ -87,7 +87,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-7185d6"
+  selector: "#example-c6b34a"
 });
 export {
   render
