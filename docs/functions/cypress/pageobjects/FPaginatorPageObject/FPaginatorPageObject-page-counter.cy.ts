1d5080786b456e7f4a903b1de38938f25cdb8a2d
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2025-11-28 15:46:41 +0100

    fix: `pageCounter` selector in `FPaginator.pageobject` includes `sr-only` element (refs SFKUI-6500)

--- docs/functions/cypress/pageobjects/FPaginatorPageObject/FPaginatorPageObject-page-counter.cy.ts
+++ docs/functions/cypress/pageobjects/FPaginatorPageObject/FPaginatorPageObject-page-counter.cy.ts
@@ -1,11 +1,11 @@
 import { FPaginatorPageObject } from "@fkui/vue/cypress";
 import Example from "./FPaginatorPageObject.vue";
 
-it("pageCounter() should contain correct text", () => {
+it("pageCounter() should return correct text", () => {
     cy.mount(Example);
 
     /* --- cut above --- */
     const paginator = new FPaginatorPageObject("[data-test='myPaginator']");
-    paginator.pageCounter().should("contain.text", "1 av 20");
+    paginator.pageCounter().should("have.text", "1 av 20");
     /* --- cut below --- */
 });
