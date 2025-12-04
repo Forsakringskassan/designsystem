db57ddfbcb028e25f98434faef6269ca9e68180b
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2025-12-04 15:29:41 +0100

    test: new `FPaginator` screenshots (refs SFKUI-7352)

---
+++ packages/vue/src/components/FPaginator/FPaginator.cy.ts
@@ -0,0 +1,22 @@
+import { FPaginator } from ".";
+
+describe("visual regression", () => {
+    beforeEach(() => {
+        cy.mount(FPaginator, {
+            props: {
+                numberOfPages: 15,
+                currentPage: 7,
+            },
+        });
+    });
+
+    it("should have approved design for desktop", () => {
+        cy.viewport(800, 80);
+        cy.toMatchScreenshot();
+    });
+
+    it("should have approved design for mobile", () => {
+        cy.viewport(400, 80);
+        cy.toMatchScreenshot();
+    });
+});
