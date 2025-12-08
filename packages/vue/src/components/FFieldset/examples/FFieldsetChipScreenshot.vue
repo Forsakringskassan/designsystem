9289b3969e5c18443792e70f811a5cbd0cc5d039
Carlsson Martin <martin.carlsson@forsakringskassan.se>
2025-12-08 15:36:25 +0100

    test: add `FFieldset` screenshots (refs SFKUI-7411)

---
+++ packages/vue/src/components/FFieldset/examples/FFieldsetChipScreenshot.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+    FButton,
+    FCheckboxField,
+    FFieldset,
+    FRadioField,
+    FTooltip,
+    FValidationForm,
+} from "@fkui/vue";
+
+const choices1 = ref();
+const choices2 = ref([]);
+</script>
+
+<template>
+    <!-- [html-validate-disable-next wcag/h32 -- false positive, FButton is a submit button ] -->
+    <f-validation-form id="screenshot" :use-error-list="false">
+        <template #default>
+            <f-fieldset v-validation.required name="chips" chip horizontal>
+                <template #label> Fieldset-etikett </template>
+
+                <template #description="{ descriptionClass }">
+                    <span :class="descriptionClass">
+                        Exemplet visar ett fieldset med fyra horizontella chip, utan tooltip och med
+                        valideringsfel. Där första chippet är i fokus.
+                    </span>
+                </template>
+
+                <f-radio-field v-model="choices1" value="Ett alternativ">
+                    Ett alternativ
+                </f-radio-field>
+
+                <f-radio-field v-model="choices1" value="Alternativ 2">
+                    Alternativ 2
+                </f-radio-field>
+
+                <f-radio-field v-model="choices1" value="Ytterligare alternativ">
+                    Ytterligare alternativ
+                </f-radio-field>
+
+                <f-radio-field v-model="choices1" value="Sista" disabled> Sista </f-radio-field>
+            </f-fieldset>
+
+            <f-fieldset v-validation.required name="chips" chip>
+                <template #label> Fieldset-etikett </template>
+
+                <template #tooltip>
+                    <f-tooltip screen-reader-text="Läs mer om alternativen" header-tag="h2">
+                        <template #header> Header </template>
+                        <template #body> Body </template>
+                    </f-tooltip>
+                </template>
+
+                <template #description="{ descriptionClass }">
+                    <span :class="descriptionClass">
+                        Exemplet visar ett fieldset med fyra vertikala chip, tooltip och med
+                        valideringsfel.
+                    </span>
+                </template>
+
+                <f-checkbox-field v-model="choices2" value="Ett alternativ">
+                    Ett alternativ
+                </f-checkbox-field>
+
+                <f-checkbox-field v-model="choices2" value="Alternativ 2">
+                    Alternativ 2
+                </f-checkbox-field>
+
+                <f-checkbox-field v-model="choices2" value="Ytterligare alternativ">
+                    Ytterligare alternativ
+                </f-checkbox-field>
+
+                <f-checkbox-field v-model="choices2" value="Sista" disabled>
+                    Sista
+                </f-checkbox-field>
+            </f-fieldset>
+
+            <f-button id="button" type="submit"> Validate </f-button>
+        </template>
+    </f-validation-form>
+</template>
+
+<style>
+#screenshot {
+    padding: 12px;
+}
+</style>
---
+++ packages/vue/src/components/FFieldset/examples/FFieldsetRadiobuttonScreenshot.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { FButton, FFieldset, FRadioField, FTooltip, FValidationForm } from "@fkui/vue";
+
+const modelValue1 = ref();
+const modelValue2 = ref();
+</script>
+
+<template>
+    <!-- [html-validate-disable-next wcag/h32 -- false positive, FButton is a submit button ] -->
+    <f-validation-form id="screenshot" :use-error-list="false">
+        <template #default>
+            <f-fieldset v-validation.required name="radio1">
+                <template #label> Fieldset-etikett </template>
+
+                <template #description="{ descriptionClass }">
+                    <span :class="descriptionClass">
+                        Exemplet visar ett fieldset med två radioknappar, utan tooltip och med
+                        valideringsfel. Där första radioknappen är i fokus.
+                    </span>
+                </template>
+
+                <template #default>
+                    <f-radio-field v-model="modelValue1" :value="true"> Ja </f-radio-field>
+                    <f-radio-field v-model="modelValue1" :value="false"> Nej </f-radio-field>
+                </template>
+            </f-fieldset>
+
+            <f-fieldset v-validation.required name="radio2" border>
+                <template #label> Fieldset-etikett </template>
+
+                <template #tooltip>
+                    <f-tooltip screen-reader-text="Läs mer om radioknappar" header-tag="h2">
+                        <template #header> Radioknappar </template>
+                        <template #body> Lite information. </template>
+                    </f-tooltip>
+                </template>
+
+                <template #description="{ descriptionClass }">
+                    <span :class="descriptionClass">
+                        Exemplet visar ett fieldset med två radioknappar med ram, tooltip och
+                        valideringsfel.
+                    </span>
+                </template>
+
+                <template #default>
+                    <f-radio-field v-model="modelValue2" :value="true"> Ja </f-radio-field>
+                    <f-radio-field v-model="modelValue2" :value="false"> Nej </f-radio-field>
+                </template>
+            </f-fieldset>
+
+            <f-button id="button" type="submit"> Validate </f-button>
+        </template>
+    </f-validation-form>
+</template>
+
+<style>
+#screenshot {
+    padding: 12px;
+}
+</style>
