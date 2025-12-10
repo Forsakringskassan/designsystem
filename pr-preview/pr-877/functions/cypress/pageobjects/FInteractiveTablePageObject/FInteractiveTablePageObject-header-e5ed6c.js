ed236be18850d5c965bde6d11a0f95ccb0a5988d
ext <ext@users.noreply.github.com>
2025-12-10 17:24:20 +0000

    Deploy preview for PR 877 🛫

--- pr-preview/pr-877/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-header-e5ed6c.js
+++ pr-preview/pr-877/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-header-e5ed6c.js
@@ -28,7 +28,7 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-header.vue:FInteractiveTablePageObject-header-9191dd.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-header.vue:FInteractiveTablePageObject-header-e5ed6c.js
 import { defineComponent as _defineComponent } from "vue";
 import { FInteractiveTable, FTableColumn } from "@fkui/vue";
 import { createTextVNode as _createTextVNode, toDisplayString as _toDisplayString, withCtx as _withCtx, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
@@ -119,7 +119,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-9191dd"
+  selector: "#example-e5ed6c"
 });
 export {
   render
