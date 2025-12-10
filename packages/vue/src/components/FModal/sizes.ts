1d3ad0f11071bdad1357be088a37fcb66b3d4105
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-12-10 18:19:16 +0100

    fix: export `FModalSize` type for consumers creating custom modals (refs SFKUI-6500)

--- packages/vue/src/components/FModal/sizes.ts
+++ packages/vue/src/components/FModal/sizes.ts
@@ -1,12 +1,38 @@
-export const sizes = [
-    "",
+/**
+ * The sizes allowed by the `FModal` `size` prop.
+ *
+ * @public
+ * @since %version%
+ */
+export const modalSizes = [
     "small",
     "medium",
     "large",
-    "fullscreen",
     "fullwidth",
+
+    /** @deprecated deprecated default value */
+    "",
+
+    /** @deprecated deprecated alias for fullwidth */
+    "fullscreen",
 ] as const;
 
+/**
+ * The sizes allowed by the `FModal` `size` prop.
+ *
+ * @public
+ * @since %version%
+ */
+export type FModalSize = Exclude<
+    (typeof modalSizes)[number],
+    /* exclude deprecated alias from the type, if the deprecated types are
+     * needed the consumer should add those manually.  */
+    "" | "fullscreen"
+>;
+
+/**
+ * @internal
+ */
 export function sizeClass(size: string): string[] {
     if (!size) {
         return [];
