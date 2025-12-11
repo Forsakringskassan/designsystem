f81a321c8c243d1430c6da40768306cfc5871b4a
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-12-11 08:21:07 +0100

    feat: new `@fkui/vue/selectors` library with test framework-agnostic selectors (refs SFKUI-6500)

--- generate-docs.mjs
+++ generate-docs.mjs
@@ -148,6 +148,11 @@ const docs = new Generator(import.meta.url, {
             expose: "named",
             alias: "vue/dist/vue.esm-bundler.js",
         },
+        {
+            package: "@fkui/vue/selectors",
+            expose: "named",
+            alias: "./packages/vue/dist/esm/selectors.esm.js",
+        },
         "@fkui/logic",
         "@fkui/date",
         "@fkui/vue",
