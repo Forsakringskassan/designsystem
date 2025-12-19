ffdb35a8170eca5a1b2c0e23f96acb8aaa7c99b8
udenius <udenius@users.noreply.github.com>
2025-12-19 09:56:54 +0000

    Deploy preview for PR 883 🛫

---
+++ pr-preview/pr-883/vue-labs/pageobjects/FTablePageObject-headerTitle-a67e30.js
@@ -0,0 +1,88 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  app.use(ErrorPlugin);
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.mount(selector);
+  setRunningContext(app);
+}
+
+// virtual-entry:virtual:docs/pageobjects/examples/FTablePageObject/FTablePageObject-headerTitle.vue:FTablePageObject-headerTitle-a67e30.js
+import { defineComponent as _defineComponent } from "vue";
+import { ref } from "vue";
+import { FTable, defineTableColumns } from "@fkui/vue-labs";
+import { resolveDirective as _resolveDirective, withDirectives as _withDirectives, openBlock as _openBlock, createBlock as _createBlock } from "vue";
+var exampleComponent = /* @__PURE__ */ _defineComponent({
+  __name: "FTablePageObject-headerTitle",
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const columns = defineTableColumns([
+      {
+        type: "text",
+        header: "ID",
+        value(row) {
+          return row.id;
+        }
+      },
+      {
+        type: "text",
+        header: "Namn",
+        value(row) {
+          return row.name;
+        }
+      }
+    ]);
+    const rows = ref([
+      {
+        id: "1",
+        name: "Foo"
+      },
+      {
+        id: "2",
+        name: "Bar"
+      },
+      {
+        id: "3",
+        name: "Baz"
+      }
+    ]);
+    const __returned__ = { columns, rows, get FTable() {
+      return FTable;
+    } };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _directive_test = _resolveDirective("test");
+  return _withDirectives((_openBlock(), _createBlock($setup["FTable"], {
+    columns: $setup.columns,
+    rows: $setup.rows
+  }, null, 8, ["columns", "rows"])), [
+    [_directive_test, "table"]
+  ]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-a67e30"
+});
+export {
+  render
+};
