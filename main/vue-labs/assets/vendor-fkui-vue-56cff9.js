5d0e49e059b9b037100d631b2a158c158f3e383f
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2026-01-13 11:30:53 +0000

    chore(release): [skip ci] deploy documentation from main branch

--- main/vue-labs/assets/vendor-fkui-vue-56cff9.js
+++ main/vue-labs/assets/vendor-fkui-vue-56cff9.js
@@ -6884,48 +6884,41 @@ function setInternalKey(item, value) {
   Object.defineProperty(item, internalKey, {
     value: value !== null && value !== void 0 ? value : String(internalIndex++),
     enumerable: false,
-    writable: true
+    writable: false
   });
 }
-function setInternalKeys(items, key, nestedKey, seenValues = /* @__PURE__ */ new Set()) {
-  if (key === void 0) {
-    return items.map((item) => {
-      setInternalKey(item);
-      if (nestedKey !== void 0) {
-        const nestedItem = item[nestedKey];
-        if (Array.isArray(nestedItem)) {
-          setInternalKeys(nestedItem);
-        }
-      }
-      return item;
-    });
-  }
+function setInternalKeys(items, keyAttribute, expandableAttribute, seenValues = /* @__PURE__ */ new Set()) {
   return items.map((item, index) => {
-    const value = item[key];
-    const keyString = String(key);
-    const invalidValue = (
-      /* eslint-disable-next-line @typescript-eslint/no-base-to-string -- ok since we only test if the string is empty */
-      value === void 0 || value === null || String(value).length === 0
-    );
-    if (invalidValue) {
-      throw new Error(`Key [${keyString}] is missing or has invalid value in item index ${String(index)}`);
+    const value = keyAttribute ? item[keyAttribute] : void 0;
+    if (keyAttribute) {
+      ensureUniqueKey(keyAttribute, value, index, seenValues);
     }
-    if (seenValues.has(value)) {
-      throw new Error(
-        /* eslint-disable-next-line @typescript-eslint/no-base-to-string -- technical debt */
-        `Expected each item to have key [${keyString}] with unique value but encountered duplicate of "${String(value)}" in item index ${String(index)}.`
-      );
-    }
-    setInternalKey(item, String(value));
-    seenValues.add(value);
-    if (nestedKey !== void 0) {
-      const nestedItem = item[nestedKey];
+    setInternalKey(item, value);
+    if (expandableAttribute !== void 0) {
+      const nestedItem = item[expandableAttribute];
       if (Array.isArray(nestedItem)) {
-        setInternalKeys(nestedItem, key, void 0, seenValues);
+        setInternalKeys(nestedItem, keyAttribute, expandableAttribute, seenValues);
       }
     }
     return item;
   });
+}
+function ensureUniqueKey(keyAttribute, value, index, seenValues) {
+  const keyString = String(keyAttribute);
+  const invalidValue = (
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string -- technical debt
+    value === void 0 || value === null || String(value).length === 0
+  );
+  if (invalidValue) {
+    throw new Error(`Key [${keyString}] is missing or has invalid value in item index ${String(index)}`);
+  }
+  if (seenValues.has(value)) {
+    throw new Error(
+      /* eslint-disable-next-line @typescript-eslint/no-base-to-string -- technical debt */
+      `Expected each item to have key [${keyString}] with unique value but encountered duplicate of "${String(value)}" in item index ${String(index)}.`
+    );
+  }
+  seenValues.add(value);
 }
 function getValidatableElement(element) {
   if (isValidatableHTMLElement(element)) {
