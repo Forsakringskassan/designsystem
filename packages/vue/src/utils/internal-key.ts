3995a79f1741919f05c395d79bc6a82a6907356b
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-01-13 11:30:36 +0100

    refactor: improve `setInternalKey` and `setInternalKeys` functions (refs SFKUI-6500)

--- packages/vue/src/utils/internal-key.ts
+++ packages/vue/src/utils/internal-key.ts
@@ -10,7 +10,7 @@ export function getInternalKey<T>(): keyof T {
 
 /** @internal */
 /* eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters -- technical debt, this is a weird one */
-export function setInternalKey<T>(item: T, value?: string): void {
+export function setInternalKey<T>(item: T, value: unknown | undefined): void {
     if (item[internalKey as keyof T]) {
         return;
     }
@@ -18,7 +18,7 @@ export function setInternalKey<T>(item: T, value?: string): void {
     Object.defineProperty(item, internalKey, {
         value: value ?? String(internalIndex++),
         enumerable: false,
-        writable: true,
+        writable: false,
     });
 }
 
@@ -40,43 +40,14 @@ export function setInternalKeys<T>(
     expandableAttribute?: keyof T,
     seenValues = new Set<unknown>(),
 ): T[] {
-    if (keyAttribute === undefined) {
-        return items.map((item) => {
-            setInternalKey(item);
-
-            if (expandableAttribute !== undefined) {
-                const nestedItem = item[expandableAttribute];
-                if (Array.isArray(nestedItem)) {
-                    setInternalKeys(nestedItem);
-                }
-            }
-
-            return item;
-        });
-    }
-
     return items.map((item, index) => {
-        const value = item[keyAttribute] as unknown;
-        const keyString = String(keyAttribute);
-        const invalidValue =
-            /* eslint-disable-next-line @typescript-eslint/no-base-to-string -- ok since we only test if the string is empty */
-            value === undefined || value === null || String(value).length === 0;
+        const value = keyAttribute ? item[keyAttribute] : undefined;
 
-        if (invalidValue) {
-            throw new Error(
-                `Key [${keyString}] is missing or has invalid value in item index ${String(index)}`,
-            );
-        }
-        if (seenValues.has(value)) {
-            throw new Error(
-                /* eslint-disable-next-line @typescript-eslint/no-base-to-string -- technical debt */
-                `Expected each item to have key [${keyString}] with unique value but encountered duplicate of "${String(value)}" in item index ${String(index)}.`,
-            );
+        if (keyAttribute) {
+            ensureUniqueKey(keyAttribute, value, index, seenValues);
         }
 
-        /* eslint-disable-next-line @typescript-eslint/no-base-to-string -- technical debt */
-        setInternalKey(item, String(value));
-        seenValues.add(value);
+        setInternalKey(item, value);
 
         if (expandableAttribute !== undefined) {
             const nestedItem = item[expandableAttribute];
@@ -84,7 +55,7 @@ export function setInternalKeys<T>(
                 setInternalKeys(
                     nestedItem,
                     keyAttribute,
-                    undefined,
+                    expandableAttribute,
                     seenValues,
                 );
             }
@@ -92,4 +63,29 @@ export function setInternalKeys<T>(
 
         return item;
     });
+}
+
+function ensureUniqueKey(
+    keyAttribute: string | number | symbol,
+    value: unknown,
+    index: number,
+    seenValues: Set<unknown>,
+): void {
+    const keyString = String(keyAttribute);
+    const invalidValue =
+        // eslint-disable-next-line @typescript-eslint/no-base-to-string -- technical debt
+        value === undefined || value === null || String(value).length === 0;
+
+    if (invalidValue) {
+        throw new Error(
+            `Key [${keyString}] is missing or has invalid value in item index ${String(index)}`,
+        );
+    }
+    if (seenValues.has(value)) {
+        throw new Error(
+            /* eslint-disable-next-line @typescript-eslint/no-base-to-string -- technical debt */
+            `Expected each item to have key [${keyString}] with unique value but encountered duplicate of "${String(value)}" in item index ${String(index)}.`,
+        );
+    }
+    seenValues.add(value);
 }
