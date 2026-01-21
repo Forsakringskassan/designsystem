7769a9c480ab1d404ff05cc4ce459bde25fbf612
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-01-21 09:40:25 +0100

    refactor: move `getValueFn` to helper in own file (refs SFKUI-6500)

--- packages/vue-labs/src/components/FTable/get-value-fn.spec.ts
+++ packages/vue-labs/src/components/FTable/get-value-fn.spec.ts
@@ -1,82 +1,9 @@
-import { getFormattedValueFn, getValueFn } from "./get-value-fn";
+import { getFormattedValueFn } from "./get-value-fn";
 
 interface TableRow {
     id: string;
     text: string;
 }
-
-describe("Without formatter", () => {
-    it("should return and call provided value-function", () => {
-        const valueFnMock = jest.fn((_row: TableRow): string => {
-            return "myValue";
-        });
-
-        const rows: TableRow[] = [
-            {
-                id: "1",
-                text: "aaa",
-            },
-            {
-                id: "2",
-                text: "bbb",
-            },
-        ];
-        const myValueFn = getValueFn<TableRow, string>(
-            valueFnMock,
-            undefined,
-            String,
-            "",
-        );
-        const value = myValueFn(rows[0]);
-        expect(value).toBe("myValue");
-        expect(valueFnMock).toHaveBeenCalledWith({
-            id: "1",
-            text: "aaa",
-        });
-    });
-
-    it("should create and call created value-function", () => {
-        const rows: TableRow[] = [
-            {
-                id: "1",
-                text: "aaa",
-            },
-            {
-                id: "2",
-                text: "bbb",
-            },
-        ];
-        const myValueFn = getValueFn<TableRow, string>(
-            undefined,
-            "text",
-            String,
-            "",
-        );
-        const value = myValueFn(rows[0]);
-        expect(value).toBe("aaa");
-    });
-
-    it("should return a function that returns the default value, when fn and key is undefined", () => {
-        const rows: TableRow[] = [
-            {
-                id: "1",
-                text: "aaa",
-            },
-            {
-                id: "2",
-                text: "bbb",
-            },
-        ];
-        const myValueFn = getValueFn<TableRow, string>(
-            undefined,
-            undefined,
-            String,
-            "my default value",
-        );
-        const value = myValueFn(rows[0]);
-        expect(value).toBe("my default value");
-    });
-});
 
 describe("With formatter", () => {
     it("should return and call provided value-function with formatted value", () => {
