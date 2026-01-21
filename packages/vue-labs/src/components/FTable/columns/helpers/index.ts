d0ab7ec09a54533b2d5d2b9a812c3cabe75343d3
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-01-21 10:15:10 +0100

    refactor: move `getFormattedNumberValueFn` to helper in own file (refs SFKUI-6500)

--- packages/vue-labs/src/components/FTable/columns/helpers/index.ts
+++ packages/vue-labs/src/components/FTable/columns/helpers/index.ts
@@ -1,3 +1,4 @@
+export { getFormattedNumberValueFn } from "./get-formatted-number-value-fn";
 export { getFormattedValueFn } from "./get-formatted-value-fn";
 export { getLabelFn } from "./get-label-fn";
 export { getValueFn } from "./get-value-fn";
