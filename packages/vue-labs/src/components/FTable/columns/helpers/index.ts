d6dd2501c0f7035f99815ee40402ff6f2513af33
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-01-23 14:09:01 +0100

    refactor: move `getParsedNumberUpdateFn` to helper in own file (refs SFKUI-6500)

--- packages/vue-labs/src/components/FTable/columns/helpers/index.ts
+++ packages/vue-labs/src/components/FTable/columns/helpers/index.ts
@@ -1,6 +1,7 @@
 export { getFormattedNumberValueFn } from "./get-formatted-number-value-fn";
 export { getFormattedValueFn } from "./get-formatted-value-fn";
 export { getLabelFn } from "./get-label-fn";
+export { getParsedNumberUpdateFn } from "./get-parsed-number-update-fn";
 export { getParsedUpdateFn } from "./get-parsed-update-fn";
 export { getUpdateFn } from "./get-update-fn";
 export { getValueFn } from "./get-value-fn";
