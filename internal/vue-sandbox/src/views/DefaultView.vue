aa321c6c5d02ad1d1602f54c53c7be5732800490
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2026-02-03 15:46:00 +0100

    chore: recreate playground example from issue (refs SFKUI-6500)

--- internal/vue-sandbox/src/views/DefaultView.vue
+++ internal/vue-sandbox/src/views/DefaultView.vue
@@ -1,52 +1,25 @@
-<script lang="ts">
-import { defineComponent } from "vue";
-import { FTextField } from "@fkui/vue";
+<script setup lang="ts">
+import { FLayoutApplicationTemplate } from "@fkui/vue";
 
-export default defineComponent({
-    components: { FTextField },
-    data() {
-        return {
-            awesomeModel: "",
-        };
-    },
-});
+const count = Array(250)
+    .fill(0)
+    .map((_, i) => i);
 </script>
 
 <template>
-    <div class="sandbox-root">
-        <h1>FKUI Sandbox</h1>
-        <p>
-            Ett internt paket som innehåller en avskalad Vue-applikation. Applikationen är konsument av övriga
-            FKUI-paket och innehåller enbart ett tomt exempel.
-        </p>
-        <p>
-            <strong>Ändra och labba gärna här men glöm inte återställa innan merge!</strong>
-        </p>
-        <hr />
-        <f-text-field
-            id="awesome-field"
-            v-model="awesomeModel"
-            v-validation.required.maxLength="{ maxLength: { length: 10 } }"
-        >
-            <template #default> Inmatningsfält. </template>
-            <template #description="{ descriptionClass }">
-                <span :class="descriptionClass"> Lorem ipsum dolor sit amet. </span>
-            </template>
-        </f-text-field>
-    </div>
+    <f-layout-application-template>
+        <template #header>
+            <div class="sandbox-header">En header</div>
+        </template>
+        <template #default>
+            <div v-for="value in count" :key="value">{{ value }}</div>
+        </template>
+    </f-layout-application-template>
 </template>
 
-<style>
-.sandbox-root {
-    width: min(100% - 2rem, 80ch);
-    margin: auto;
-}
-
-h1 {
-    margin-top: 2rem;
-}
-
-hr {
-    margin-bottom: 2rem;
+<style scoped lang="scss">
+.sandbox-header {
+    border: 1px solid black;
+    padding: 45px;
 }
 </style>
