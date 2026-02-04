75dd34f55cb5bc0417f43b46cce4c429aef434db
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2026-02-04 11:24:53 +0100

    chore: revert "chore: recreate playground example from issue (refs SFKUI-6500)" (refs SFKUI-6500) This reverts commit 0d3b1df3187d322b1d7fbc8bbc884b8803eb002a.

--- internal/vue-sandbox/src/views/DefaultView.vue
+++ internal/vue-sandbox/src/views/DefaultView.vue
@@ -1,25 +1,52 @@
-<script setup lang="ts">
-import { FLayoutApplicationTemplate } from "@fkui/vue";
+<script lang="ts">
+import { defineComponent } from "vue";
+import { FTextField } from "@fkui/vue";
 
-const count = Array(250)
-    .fill(0)
-    .map((_, i) => i);
+export default defineComponent({
+    components: { FTextField },
+    data() {
+        return {
+            awesomeModel: "",
+        };
+    },
+});
 </script>
 
 <template>
-    <f-layout-application-template>
-        <template #header>
-            <div class="sandbox-header">En header</div>
-        </template>
-        <template #default>
-            <div v-for="value in count" :key="value">{{ value }}</div>
-        </template>
-    </f-layout-application-template>
+    <div class="sandbox-root">
+        <h1>FKUI Sandbox</h1>
+        <p>
+            Ett internt paket som innehåller en avskalad Vue-applikation. Applikationen är konsument av övriga
+            FKUI-paket och innehåller enbart ett tomt exempel.
+        </p>
+        <p>
+            <strong>Ändra och labba gärna här men glöm inte återställa innan merge!</strong>
+        </p>
+        <hr />
+        <f-text-field
+            id="awesome-field"
+            v-model="awesomeModel"
+            v-validation.required.maxLength="{ maxLength: { length: 10 } }"
+        >
+            <template #default> Inmatningsfält. </template>
+            <template #description="{ descriptionClass }">
+                <span :class="descriptionClass"> Lorem ipsum dolor sit amet. </span>
+            </template>
+        </f-text-field>
+    </div>
 </template>
 
-<style scoped lang="scss">
-.sandbox-header {
-    border: 1px solid black;
-    padding: 45px;
+<style>
+.sandbox-root {
+    width: min(100% - 2rem, 80ch);
+    margin: auto;
+}
+
+h1 {
+    margin-top: 2rem;
+}
+
+hr {
+    margin-bottom: 2rem;
 }
 </style>
