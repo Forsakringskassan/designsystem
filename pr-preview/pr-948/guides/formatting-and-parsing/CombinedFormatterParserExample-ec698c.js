10625277b0c555beec20d772a74cc10fc8944f85
udenius <udenius@users.noreply.github.com>
2026-02-05 10:17:52 +0000

    Deploy preview for PR 948 🛫

--- pr-preview/pr-948/guides/formatting-and-parsing/CombinedFormatterParserExample-ec698c.js
+++ pr-preview/pr-948/guides/formatting-and-parsing/CombinedFormatterParserExample-ec698c.js
@@ -28,20 +28,19 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/guides/validation/examples/WithFormatterExample.vue:WithFormatterExample-35b3cc.js
+// virtual-entry:virtual:docs/guides/validation/examples/CombinedFormatterParserExample.vue:CombinedFormatterParserExample-ec698c.js
 import { defineComponent } from "vue";
+import { formatNumber, parseNumber } from "@fkui/logic";
 import { FOutputField, FTextField } from "@fkui/vue";
-import { createTextVNode as _createTextVNode, resolveComponent as _resolveComponent, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives, createElementVNode as _createElementVNode, toDisplayString as _toDisplayString, createVNode as _createVNode, createElementBlock as _createElementBlock } from "vue";
-function myFormatter(value) {
-  return value.toLowerCase();
-}
+import { createTextVNode as _createTextVNode, normalizeClass as _normalizeClass, createElementVNode as _createElementVNode, resolveComponent as _resolveComponent, resolveDirective as _resolveDirective, withCtx as _withCtx, openBlock as _openBlock, createBlock as _createBlock, withDirectives as _withDirectives, toDisplayString as _toDisplayString, createVNode as _createVNode, createElementBlock as _createElementBlock } from "vue";
 var exampleComponent = defineComponent({
-  name: "WithFormatterExample",
+  name: "FormatterExample",
   components: { FTextField, FOutputField },
   data() {
     return {
       modelValue: "",
-      myFormatter
+      formatNumber,
+      parseNumber
     };
   }
 });
@@ -56,10 +55,11 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
   return _openBlock(), _createElementBlock("div", _hoisted_1, [
     _createElementVNode("div", _hoisted_2, [
       _withDirectives((_openBlock(), _createBlock(_component_f_text_field, {
-        id: "with-formatter",
+        id: "combined-formatter-parser",
         modelValue: _ctx.modelValue,
         "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _ctx.modelValue = $event),
-        formatter: _ctx.myFormatter
+        formatter: _ctx.formatNumber,
+        parser: _ctx.parseNumber
       }, {
         default: _withCtx(() => [..._cache[1] || (_cache[1] = [
           _createTextVNode(
@@ -68,19 +68,30 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
             /* CACHED */
           )
         ])]),
+        description: _withCtx(({ descriptionClass }) => [
+          _createElementVNode(
+            "span",
+            {
+              class: _normalizeClass(descriptionClass)
+            },
+            " Fyll i ett heltal ",
+            2
+            /* CLASS */
+          )
+        ]),
         _: 1
         /* STABLE */
-      }, 8, ["modelValue", "formatter"])), [
+      }, 8, ["modelValue", "formatter", "parser"])), [
         [
           _directive_validation,
-          { maxLength: { length: 100 } },
+          { maxLength: { length: 20 } },
           void 0,
           { maxLength: true }
         ]
       ])
     ]),
     _createElementVNode("div", _hoisted_3, [
-      _createVNode(_component_f_output_field, { for: "with-formatter" }, {
+      _createVNode(_component_f_output_field, { for: "combined-formatter-example" }, {
         label: _withCtx(() => [..._cache[2] || (_cache[2] = [
           _createTextVNode(
             " modelValue ",
@@ -106,7 +117,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-35b3cc"
+  selector: "#example-ec698c"
 });
 export {
   render
