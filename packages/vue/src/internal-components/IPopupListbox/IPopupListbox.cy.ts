265ad6bc158b40b78824c0c3beba7fdfb1f5df90
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2026-02-06 10:51:02 +0100

    fix: combobox dropdown should not follow horizontal scroll (fixes SFKUI-7595)

--- packages/vue/src/internal-components/IPopupListbox/IPopupListbox.cy.ts
+++ packages/vue/src/internal-components/IPopupListbox/IPopupListbox.cy.ts
@@ -287,3 +287,57 @@ describe("IPopupListbox above anchor", () => {
         cy.toMatchScreenshot();
     });
 });
+
+describe("IPopupListbox scrolling", () => {
+    beforeEach(() => {
+        setViewport({ height: 300, width: 480 });
+    });
+
+    it("should have correct position when opened after scrolling horizontally", () => {
+        const component = defineComponent({
+            template: /* HTML */ `
+                <div style="width: 600px">
+                    <label for="input">Items</label>
+                    <input
+                        id="input"
+                        ref="input"
+                        type="text"
+                        style="height: 50px; margin-top: 200px"
+                    />
+                    <i-popup-listbox
+                        :is-open
+                        :anchor="$refs.input"
+                        :num-of-items="3"
+                    >
+                        <ul
+                            style="list-style-type: none; padding: 0; margin: 0"
+                        >
+                            <li v-for="index in 3" :key="index">
+                                Item {{ index }}
+                            </li>
+                        </ul>
+                    </i-popup-listbox>
+                    <button type="button" @click="isOpen = !isOpen">
+                        Open listbox
+                    </button>
+                </div>
+            `,
+            components: {
+                IPopupListbox,
+            },
+            data() {
+                return {
+                    isOpen: false,
+                };
+            },
+        });
+        cy.mount(component);
+        cy.scrollTo(100);
+        cy.get("ul").should("not.exist");
+        cy.get("button").click();
+        cy.get("ul").should("be.visible");
+        cy.scrollTo(0);
+
+        cy.toMatchScreenshot();
+    });
+});
