42f902c5164bb656b7d0f825836c2d28d521d361
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-02-09 10:51:22 +0100

    feat: expose `fontDir` and `filePath` (full path to font file) (refs SFKUI-6500)

--- docs/functions/font-default.md
+++ docs/functions/font-default.md
@@ -43,13 +43,22 @@ Om du inte använder Vite behöver du:
 
 ## Metadata
 
-Paketet levererar metadata i `@fkui/font-default/metadata.json` med följande format:
+Paketet levererar metadata i `@fkui/font-default/metadata` med följande format:
 
 ```ts
 export interface Asset {
+    /** Name of the font file */
+    name: string;
+    /** Relative path to the font file */
     filename: string;
+    /** Full path to the font file */
+    filePath: string;
 }
 
+/** Absolute path to the folder containing the font files */
+export declare const fontDir: string;
+
+/** List of available font assets */
 export declare const assets: Asset[];
 ```
 
--- packages/font-default/metadata.mjs
+++ packages/font-default/metadata.mjs
@@ -1,15 +1,33 @@
+import path from "node:path";
+
 /**
  * @typedef {Object} Asset {
+ * @property {string} name
  * @property {string} filename
+ * @property {string} filePath
  */
 
+export const fontDir = path.join(import.meta.dirname, "fonts");
+
+/**
+ * @param {string} name
+ * @returns {Asset}
+ */
+function defineAsset(name) {
+    return {
+        name,
+        filename: `fonts/${name}`,
+        filePath: path.join(fontDir, name),
+    };
+}
+
 /** @type {Asset[]} */
-const assets = [
-    { filename: "fonts/Inter-Bold.woff2" },
-    { filename: "fonts/Inter-Light.woff2" },
-    { filename: "fonts/Inter-Medium.woff2" },
-    { filename: "fonts/Inter-Regular.woff2" },
-    { filename: "fonts/Inter-SemiBold.woff2" },
+export const assets = [
+    defineAsset("Inter-Bold.woff2"),
+    defineAsset("Inter-Light.woff2"),
+    defineAsset("Inter-Medium.woff2"),
+    defineAsset("Inter-Regular.woff2"),
+    defineAsset("Inter-SemiBold.woff2"),
 ];
 
 export default assets;
