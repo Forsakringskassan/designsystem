413e808b7d4415e7e39f02ef8ddc39d05345ea52
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2026-02-13 10:27:53 +0100

    fixup! docs: create temporary sandbox demo (refs SFKUI-7638)

--- internal/vue-sandbox/src/views/DefaultView.vue
+++ internal/vue-sandbox/src/views/DefaultView.vue
@@ -1,14 +1,37 @@
 <template>
     <h1>Rubriknivå 1 (h1)</h1>
     Text under rubrik
+
     <h2>Rubriknivå 2 (h2)</h2>
     Text under rubrik
+
     <h3>Rubriknivå 3 (h3)</h3>
     Text under rubrik
+
     <h4>Rubriknivå 4 (h4)</h4>
     Text under rubrik
+
     <h5>Rubriknivå 5 (h5)</h5>
     Text under rubrik
+
     <h6>Rubriknivå 6 (h6)</h6>
     Text under rubrik
+
+    <h1>Rubriknivå 1 (h1)</h1>
+    <span><code>span</code> under rubrik</span>
+
+    <h2>Rubriknivå 2 (h2)</h2>
+    <span><code>span</code> under rubrik</span>
+
+    <h3>Rubriknivå 3 (h3)</h3>
+    <span><code>span</code> under rubrik</span>
+
+    <h4>Rubriknivå 4 (h4)</h4>
+    <span><code>span</code> under rubrik</span>
+
+    <h5>Rubriknivå 5 (h5)</h5>
+    <span><code>span</code> under rubrik</span>
+
+    <h6>Rubriknivå 6 (h6)</h6>
+    <span><code>span</code> under rubrik</span>
 </template>
