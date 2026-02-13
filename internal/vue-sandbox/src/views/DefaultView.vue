5ea2203a35cad53dc5422f337c5431cb748d04e4
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2026-02-13 08:23:15 +0100

    docs: create temporary sandbox demo (refs SFKUI-7638)

--- internal/vue-sandbox/src/views/DefaultView.vue
+++ internal/vue-sandbox/src/views/DefaultView.vue
@@ -1,52 +1,14 @@
-<script lang="ts">
-import { defineComponent } from "vue";
-import { FTextField } from "@fkui/vue";
-
-export default defineComponent({
-    components: { FTextField },
-    data() {
-        return {
-            awesomeModel: "",
-        };
-    },
-});
-</script>
-
 <template>
-    <div class="sandbox-root">
-        <h1>FKUI Sandbox</h1>
-        <p>
-            Ett internt paket som innehåller en avskalad Vue-applikation. Applikationen är konsument av övriga
-            FKUI-paket och innehåller enbart ett tomt exempel.
-        </p>
-        <p>
-            <strong>Ändra och labba gärna här men glöm inte återställa innan merge!</strong>
-        </p>
-        <hr />
-        <f-text-field
-            id="awesome-field"
-            v-model="awesomeModel"
-            v-validation.required.maxLength="{ maxLength: { length: 10 } }"
-        >
-            <template #default> Inmatningsfält. </template>
-            <template #description="{ descriptionClass }">
-                <span :class="descriptionClass"> Lorem ipsum dolor sit amet. </span>
-            </template>
-        </f-text-field>
-    </div>
+    <h1>Rubriknivå 1 (h1)</h1>
+    Text under rubrik
+    <h2>Rubriknivå 2 (h2)</h2>
+    Text under rubrik
+    <h3>Rubriknivå 3 (h3)</h3>
+    Text under rubrik
+    <h4>Rubriknivå 4 (h4)</h4>
+    Text under rubrik
+    <h5>Rubriknivå 5 (h5)</h5>
+    Text under rubrik
+    <h6>Rubriknivå 6 (h6)</h6>
+    Text under rubrik
 </template>
-
-<style>
-.sandbox-root {
-    width: min(100% - 2rem, 80ch);
-    margin: auto;
-}
-
-h1 {
-    margin-top: 2rem;
-}
-
-hr {
-    margin-bottom: 2rem;
-}
-</style>
