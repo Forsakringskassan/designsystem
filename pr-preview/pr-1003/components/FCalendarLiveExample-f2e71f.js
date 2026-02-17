4944ad40854a503d8f5365834169dbe03a4a6c94
NicklasWei <NicklasWei@users.noreply.github.com>
2026-02-17 10:15:52 +0000

    Deploy preview for PR 1003 🛫

--- pr-preview/pr-1003/components/FCalendarLiveExample-f2e71f.js
+++ pr-preview/pr-1003/components/FCalendarLiveExample-f2e71f.js
@@ -6480,6 +6480,9 @@ IComboboxDropdown_default.__file = "packages/vue/src/internal-components/combobo
 import { defineComponent as _defineComponent3 } from "vue";
 var IComboboxToggleButton_default = /* @__PURE__ */ _defineComponent3({
   __name: "IComboboxToggleButton",
+  props: {
+    isOpen: { type: Boolean, required: true }
+  },
   emits: ["toggle"],
   setup(__props, { expose: __expose, emit: __emit }) {
     __expose();
@@ -6507,8 +6510,9 @@ function render22(_ctx, _cache, $props, $setup, $data, $options) {
   }, [
     _createVNode8($setup["FIcon"], {
       name: "arrow-down",
+      rotate: $props.isOpen ? "180" : void 0,
       class: "text-field__icon"
-    })
+    }, null, 8, ["rotate"])
   ], 8, _hoisted_118);
 }
 
