e6288cbaddf498816d7692b2213264c645d71fbb
udenius <udenius@users.noreply.github.com>
2026-02-19 12:15:01 +0000

    Deploy preview for PR 1023 🛫

--- pr-preview/pr-1023/functions/cypress/pageobjects/FProgressbarPageObject/FProgressbarPageObject-progress-status-ca5d7e.js
+++ pr-preview/pr-1023/functions/cypress/pageobjects/FProgressbarPageObject/FProgressbarPageObject-progress-status-ca5d7e.js
@@ -28,7 +28,7 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FProgressbarPageObject/FProgressbarPageObject-progress-status.vue:FProgressbarPageObject-progress-status-eb4b68.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FProgressbarPageObject/FProgressbarPageObject-progress-status.vue:FProgressbarPageObject-progress-status-ca5d7e.js
 import { defineComponent as _defineComponent } from "vue";
 import { FProgressbar } from "@fkui/vue";
 import { resolveDirective as _resolveDirective, withDirectives as _withDirectives, openBlock as _openBlock, createBlock as _createBlock } from "vue";
@@ -61,7 +61,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-eb4b68"
+  selector: "#example-ca5d7e"
 });
 export {
   render
