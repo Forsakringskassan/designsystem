9cfa1ec4d3e6697f93e59a7ac6b656662a2d0fc4
johancarnegard <johancarnegard@users.noreply.github.com>
2026-02-25 08:35:28 +0000

    Deploy preview for PR 1020 🛫

--- pr-preview/pr-1020/assets/processors/selectable-version/select-version-092f7d.js
+++ pr-preview/pr-1020/assets/processors/selectable-version/select-version-092f7d.js
@@ -1903,7 +1903,7 @@ function memoize(callback) {
     return cache.get(key);
   };
 }
-var current = "v6.36.0";
+var current = "v6.36.1";
 var message = "Det finns en nyare version";
 function isOutside(rect, point) {
   if (point.y < rect.top || point.y > rect.top + rect.height) {
