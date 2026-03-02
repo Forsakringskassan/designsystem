f1e968ca6c06885c939af0a668b81a042b755eef
Wilson Mats (66136604) <mats.wilson@forsakringskassan.se>
2026-03-02 16:29:34 +0100

    test(example): creating a table example app (refs SFKUI-6500)

---
+++ examples/table/src/App.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { useRoute } from "vue-router";
+import TopNavigation from "./components/TopNavigation.vue";
+
+const route = useRoute();
+</script>
+
+<template>
+    <div id="app">
+        <TopNavigation />
+
+        <main class="content">
+            <RouterView :key="route.fullPath" />
+        </main>
+    </div>
+</template>
+
+<style scoped>
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+
+.content {
+    flex: 1;
+    padding: 1rem;
+}
+</style>
