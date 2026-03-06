b22ef01f5b70f0bcfdc133c895e5ae0527a398bc
Carlsson Martin <martin.carlsson@forsakringskassan.se>
2026-03-06 11:28:25 +0100

    refactor: make radio button cell clickable (refs SFKUI-7650)

--- packages/vue-labs/src/components/FTable/ITableRadio.vue
+++ packages/vue-labs/src/components/FTable/ITableRadio.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts" generic="T, K extends keyof T">
 import { computed, useTemplateRef } from "vue";
 import { assertRef } from "@fkui/logic";
+import { activateCell } from "./FTable.logic";
 import { type FTableCellApi } from "./f-table-api";
 import { type NormalizedTableColumnRadio } from "./table-column";
 
@@ -15,9 +16,22 @@ const ariaLabel = computed(() => {
     return value.length > 0 ? value : undefined;
 });
 
-function onChange(_e: Event): void {
+const checked = computed(() => {
+    return Boolean(column.checked(row));
+});
+
+function onClickInput(e: Event): void {
+    e.stopPropagation();
     assertRef(inputElement);
-    column.update(row, inputElement.value.checked, !inputElement.value.checked);
+    activateCell(inputElement.value, { focus: true });
+    column.update(row, checked.value, !checked.value);
+}
+
+function onClickTd(e: Event): void {
+    e.stopPropagation();
+    assertRef(inputElement);
+    activateCell(inputElement.value, { focus: true });
+    column.update(row, !checked.value, checked.value);
 }
 
 const expose: FTableCellApi = { tabstopEl: inputElement };
@@ -25,14 +39,7 @@ defineExpose(expose);
 </script>
 
 <template>
-    <td class="table-ng__cell table-ng__cell--radio">
-        <input
-            ref="input"
-            type="radio"
-            :checked="Boolean(column.checked(row))"
-            :aria-label
-            tabindex="-1"
-            @change="onChange"
-        />
+    <td class="table-ng__cell table-ng__cell--radio" @click="onClickTd">
+        <input ref="input" type="radio" :checked :aria-label tabindex="-1" @click="onClickInput" />
     </td>
 </template>
