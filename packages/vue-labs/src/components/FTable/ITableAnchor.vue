be930e60f2fff96951e100bfdcefdf139fafa6ad
Carlsson Martin <martin.carlsson@forsakringskassan.se>
2026-03-06 13:36:30 +0100

    refactor: make anchor cell clickable (refs SFKUI-7650)

--- packages/vue-labs/src/components/FTable/ITableAnchor.vue
+++ packages/vue-labs/src/components/FTable/ITableAnchor.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts" generic="T, K extends keyof T">
 import { computed, useTemplateRef } from "vue";
+import { assertRef } from "@fkui/logic";
 import { type FTableCellApi } from "./f-table-api";
 import { type NormalizedTableColumnAnchor } from "./table-column";
 
@@ -16,10 +17,18 @@ const renderAnchor = computed(() => {
 
 const expose: FTableCellApi = { tabstopEl: targetElement };
 defineExpose(expose);
+
+function onClickTd(e: Event): void {
+    assertRef(targetElement);
+    if (e.target instanceof Node && targetElement.value.contains(e.target)) {
+        return;
+    }
+    targetElement.value.click();
+}
 </script>
 
 <template>
-    <td v-if="renderAnchor" class="table-ng__cell table-ng__cell--anchor">
+    <td v-if="renderAnchor" class="table-ng__cell table-ng__cell--anchor" @click="onClickTd">
         <a ref="target" class="anchor anchor--block" target="_blank" :href="column.href" tabindex="-1">
             {{ column.text(row) }}
         </a>
