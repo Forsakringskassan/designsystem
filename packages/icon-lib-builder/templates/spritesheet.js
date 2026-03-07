47fed31c8d11f24f6efb8b70acf8819fb843145d
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-03-07 13:10:59 +0100

    refactor(eslint): fix `unicorn/prefer-dom-node-dataset` (refs SFKUI-6500)

--- packages/icon-lib-builder/templates/spritesheet.js
+++ packages/icon-lib-builder/templates/spritesheet.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef -- IMPORT_SPRITESHEET, PACKAGE, LIBRARY */
 /* eslint-disable no-unused-vars -- injectSpritesheet function */
+/* eslint-disable unicorn/prefer-dom-node-dataset -- we explicitly want the data- attributes to be present in DOM for easier debugging */
+
 function injectSpritesheet() {
     IMPORT_SPRITESHEET(); // icons is inserted here by the build script
     const element = document.createElement("div");
