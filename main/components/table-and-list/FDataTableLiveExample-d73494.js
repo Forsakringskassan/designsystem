b995b453ae7193ad652f95310f1293a12c221bbe
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2026-03-10 16:19:08 +0000

    chore(release): [skip ci] deploy documentation from main branch

---
+++ main/components/table-and-list/FDataTableLiveExample-d73494.js
@@ -0,0 +1,333 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:packages/vue/src/components/FDataTable/examples/FDataTableLiveExample.vue:FDataTableLiveExample-d73494.js
+import { defineComponent } from "vue";
+import { FCheckboxField, FDataTable, FFieldset, FRadioField, FTableColumn } from "@fkui/vue";
+import { LiveExample } from "@forsakringskassan/docs-live-example";
+import { createTextVNode as _createTextVNode, resolveComponent as _resolveComponent, withCtx as _withCtx, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock, createCommentVNode as _createCommentVNode } from "vue";
+var exampleComponent = defineComponent({
+  name: "FDataTableLiveExample",
+  components: { LiveExample, FCheckboxField, FRadioField, FFieldset },
+  data() {
+    return {
+      isEmpty: false,
+      isStriped: false,
+      hasRowHeader: false,
+      hasRowDescription: false,
+      hasCustomEmptyText: false,
+      hasHiddenCaption: false,
+      emptyItems: [],
+      showHorizontalScroll: false
+    };
+  },
+  computed: {
+    livedata() {
+      return {
+        selectedItems: [],
+        items: [
+          {
+            id: "1",
+            start: "2022-04-10",
+            end: "2022-04-25",
+            level: "Sjukpenning",
+            antal: "15",
+            anteckning: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          },
+          {
+            id: "2",
+            start: "2022-05-06",
+            end: "2022-05-10",
+            level: "L\xE4gstaniv\xE5",
+            antal: "4",
+            anteckning: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          },
+          {
+            id: "3",
+            start: "2022-05-20",
+            end: "2022-05-31",
+            level: "Sjukpenning",
+            antal: "11",
+            anteckning: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          }
+        ]
+      };
+    },
+    components() {
+      return { FDataTable, FTableColumn };
+    },
+    items() {
+      return this.isEmpty ? `:rows="[]"` : `:rows="items"`;
+    },
+    striped() {
+      return this.isStriped ? "striped" : "";
+    },
+    rowHeader() {
+      return this.hasRowHeader ? `row-header` : "";
+    },
+    rowDescription() {
+      return this.hasRowDescription ? `description="(\xE5\xE5\xE5\xE5-mm-dd)"` : "";
+    },
+    scroll() {
+      return this.showHorizontalScroll ? `scroll="horizontal"` : "";
+    },
+    caption() {
+      return this.hasHiddenCaption ? `<span class="sr-only">F\xF6r\xE4ldrapenning</span>` : "F\xF6r\xE4ldrapenning";
+    },
+    empty() {
+      const template = (
+        /* HTML */
+        `<template #empty>
+                Det finns inga aktuella utbetalningar.
+            </template>`
+      );
+      return this.isEmpty && this.hasCustomEmptyText ? template : "";
+    },
+    template() {
+      return (
+        /* HTML */
+        `
+                <f-data-table ${this.items} ${this.striped} ${this.scroll} key-attribute="id">
+                    <template #caption> ${this.caption} </template>
+                    <template #default="{ row }">
+                        <f-table-column title="Niv\xE5" ${this.rowHeader} type="text">
+                            {{ row.level }}
+                        </f-table-column>
+                        <f-table-column
+                            title="Fr\xE5n och med"
+                            ${this.rowDescription}
+                            type="text"
+                            expand
+                        >
+                            <span v-format:date="row.start"></span>
+                        </f-table-column>
+                        <f-table-column title="Till och med" ${this.rowDescription} type="text">
+                            <span v-format:date="row.end"></span>
+                        </f-table-column>
+                        <f-table-column title="Antal dagar" type="numeric">
+                            {{ row.antal }}
+                        </f-table-column>
+                        <f-table-column title="Anteckning" type="text">
+                            {{ row.anteckning }}
+                        </f-table-column>
+                    </template>
+                    ${this.empty}
+                </f-data-table>
+            `
+      );
+    }
+  }
+});
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_f_checkbox_field = _resolveComponent("f-checkbox-field");
+  const _component_f_fieldset = _resolveComponent("f-fieldset");
+  const _component_f_radio_field = _resolveComponent("f-radio-field");
+  const _component_live_example = _resolveComponent("live-example");
+  return _openBlock(), _createBlock(_component_live_example, {
+    components: _ctx.components,
+    template: _ctx.template,
+    livedata: _ctx.livedata
+  }, {
+    default: _withCtx(() => [
+      _createVNode(_component_f_fieldset, { name: "styling" }, {
+        label: _withCtx(() => [..._cache[8] || (_cache[8] = [
+          _createTextVNode(
+            " Styling ",
+            -1
+            /* CACHED */
+          )
+        ])]),
+        default: _withCtx(() => [
+          _createVNode(_component_f_checkbox_field, {
+            modelValue: _ctx.isStriped,
+            "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _ctx.isStriped = $event),
+            value: true
+          }, {
+            default: _withCtx(() => [..._cache[9] || (_cache[9] = [
+              _createTextVNode(
+                " Zebrarandig ",
+                -1
+                /* CACHED */
+              )
+            ])]),
+            _: 1
+            /* STABLE */
+          }, 8, ["modelValue"]),
+          _createVNode(_component_f_checkbox_field, {
+            modelValue: _ctx.hasRowHeader,
+            "onUpdate:modelValue": _cache[1] || (_cache[1] = ($event) => _ctx.hasRowHeader = $event),
+            value: true
+          }, {
+            default: _withCtx(() => [..._cache[10] || (_cache[10] = [
+              _createTextVNode(
+                " Radrubriker ",
+                -1
+                /* CACHED */
+              )
+            ])]),
+            _: 1
+            /* STABLE */
+          }, 8, ["modelValue"]),
+          _createVNode(_component_f_checkbox_field, {
+            modelValue: _ctx.hasRowDescription,
+            "onUpdate:modelValue": _cache[2] || (_cache[2] = ($event) => _ctx.hasRowDescription = $event),
+            value: true
+          }, {
+            default: _withCtx(() => [..._cache[11] || (_cache[11] = [
+              _createTextVNode(
+                " Kolumnbeskrivnig ",
+                -1
+                /* CACHED */
+              )
+            ])]),
+            _: 1
+            /* STABLE */
+          }, 8, ["modelValue"]),
+          _createVNode(_component_f_checkbox_field, {
+            modelValue: _ctx.hasHiddenCaption,
+            "onUpdate:modelValue": _cache[3] || (_cache[3] = ($event) => _ctx.hasHiddenCaption = $event),
+            value: true
+          }, {
+            default: _withCtx(() => [..._cache[12] || (_cache[12] = [
+              _createTextVNode(
+                " Dold caption ",
+                -1
+                /* CACHED */
+              )
+            ])]),
+            _: 1
+            /* STABLE */
+          }, 8, ["modelValue"]),
+          _createVNode(_component_f_checkbox_field, {
+            modelValue: _ctx.showHorizontalScroll,
+            "onUpdate:modelValue": _cache[4] || (_cache[4] = ($event) => _ctx.showHorizontalScroll = $event),
+            value: true
+          }, {
+            default: _withCtx(() => [..._cache[13] || (_cache[13] = [
+              _createTextVNode(
+                " Horisontal skroll ",
+                -1
+                /* CACHED */
+              )
+            ])]),
+            _: 1
+            /* STABLE */
+          }, 8, ["modelValue"])
+        ]),
+        _: 1
+        /* STABLE */
+      }),
+      _createVNode(_component_f_fieldset, { name: "interaktion" }, {
+        label: _withCtx(() => [..._cache[14] || (_cache[14] = [
+          _createTextVNode(
+            " Interaktion ",
+            -1
+            /* CACHED */
+          )
+        ])]),
+        default: _withCtx(() => [
+          _createVNode(_component_f_checkbox_field, {
+            modelValue: _ctx.isEmpty,
+            "onUpdate:modelValue": _cache[5] || (_cache[5] = ($event) => _ctx.isEmpty = $event),
+            value: true
+          }, {
+            default: _withCtx(() => [..._cache[15] || (_cache[15] = [
+              _createTextVNode(
+                " Tom tabell ",
+                -1
+                /* CACHED */
+              )
+            ])]),
+            _: 1
+            /* STABLE */
+          }, 8, ["modelValue"]),
+          _ctx.isEmpty ? (_openBlock(), _createBlock(_component_f_fieldset, {
+            key: 0,
+            name: "radio-empty-text"
+          }, {
+            label: _withCtx(() => [..._cache[16] || (_cache[16] = [
+              _createTextVNode(
+                " Meddelande f\xF6r tom tabell ",
+                -1
+                /* CACHED */
+              )
+            ])]),
+            default: _withCtx(() => [
+              _createVNode(_component_f_radio_field, {
+                modelValue: _ctx.hasCustomEmptyText,
+                "onUpdate:modelValue": _cache[6] || (_cache[6] = ($event) => _ctx.hasCustomEmptyText = $event),
+                value: false
+              }, {
+                default: _withCtx(() => [..._cache[17] || (_cache[17] = [
+                  _createTextVNode(
+                    " Standardmeddelande ",
+                    -1
+                    /* CACHED */
+                  )
+                ])]),
+                _: 1
+                /* STABLE */
+              }, 8, ["modelValue"]),
+              _createVNode(_component_f_radio_field, {
+                modelValue: _ctx.hasCustomEmptyText,
+                "onUpdate:modelValue": _cache[7] || (_cache[7] = ($event) => _ctx.hasCustomEmptyText = $event),
+                value: true
+              }, {
+                default: _withCtx(() => [..._cache[18] || (_cache[18] = [
+                  _createTextVNode(
+                    " Eget meddelande ",
+                    -1
+                    /* CACHED */
+                  )
+                ])]),
+                _: 1
+                /* STABLE */
+              }, 8, ["modelValue"])
+            ]),
+            _: 1
+            /* STABLE */
+          })) : _createCommentVNode("v-if", true)
+        ]),
+        _: 1
+        /* STABLE */
+      })
+    ]),
+    _: 1
+    /* STABLE */
+  }, 8, ["components", "template", "livedata"]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-d73494"
+});
+export {
+  render
+};
---
+++ main/components/table-and-list/FSortFilterDatasetListExample-b6c3d2.js
@@ -0,0 +1,171 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:packages/vue/src/components/FSortFilterDataset/examples/FSortFilterDatasetListExample.vue:FSortFilterDatasetListExample-b6c3d2.js
+import { defineComponent } from "vue";
+import { FList, FSortFilterDataset } from "@fkui/vue";
+
+// packages/vue/src/components/FSortFilterDataset/examples/fruit-data.ts
+var fruits = [
+  {
+    id: "1",
+    name: "\xC4pple",
+    origin: "Sverige",
+    description: "Rund, ofta r\xF6d eller gr\xF6n frukt med s\xF6t eller syrlig smak."
+  },
+  {
+    id: "2",
+    name: "Banan",
+    origin: "Colombia",
+    description: "L\xE5ng, gul frukt med mjukt och s\xF6tt fruktk\xF6tt."
+  },
+  {
+    id: "3",
+    name: "Vattenmelon",
+    origin: "Spanien",
+    description: "Stor, rund frukt med gr\xF6nt skal och saftigt, r\xF6tt fruktk\xF6tt."
+  },
+  {
+    id: "4",
+    name: "Grapefrukt",
+    origin: "Turkiet",
+    description: "Stor, rund citrusfrukt med tjockt skal och saftig, syrlig smak."
+  }
+];
+
+// virtual-entry:virtual:packages/vue/src/components/FSortFilterDataset/examples/FSortFilterDatasetListExample.vue:FSortFilterDatasetListExample-b6c3d2.js
+import { normalizeClass as _normalizeClass, createElementVNode as _createElementVNode, toDisplayString as _toDisplayString, createTextVNode as _createTextVNode, resolveComponent as _resolveComponent, withCtx as _withCtx, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
+var exampleComponent = defineComponent({
+  components: { FSortFilterDataset, FList },
+  data() {
+    return {
+      sortableAttributes: {
+        name: "Namn",
+        origin: "Land"
+      },
+      fruits
+    };
+  }
+});
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_f_list = _resolveComponent("f-list");
+  const _component_f_sort_filter_dataset = _resolveComponent("f-sort-filter-dataset");
+  return _openBlock(), _createBlock(_component_f_sort_filter_dataset, {
+    data: _ctx.fruits,
+    "sortable-attributes": _ctx.sortableAttributes
+  }, {
+    header: _withCtx(({ slotClass }) => [
+      _createElementVNode(
+        "h3",
+        {
+          class: _normalizeClass(slotClass)
+        },
+        "Frukter",
+        2
+        /* CLASS */
+      )
+    ]),
+    default: _withCtx(({ sortFilterResult }) => [
+      _createVNode(_component_f_list, {
+        "key-attribute": "id",
+        items: sortFilterResult
+      }, {
+        default: _withCtx(({ item }) => [
+          _createElementVNode(
+            "h3",
+            null,
+            _toDisplayString(item.name),
+            1
+            /* TEXT */
+          ),
+          _createElementVNode("p", null, [
+            _cache[0] || (_cache[0] = _createTextVNode(
+              " Land: ",
+              -1
+              /* CACHED */
+            )),
+            _createElementVNode(
+              "em",
+              null,
+              _toDisplayString(item.origin),
+              1
+              /* TEXT */
+            ),
+            _cache[1] || (_cache[1] = _createElementVNode(
+              "br",
+              null,
+              null,
+              -1
+              /* CACHED */
+            )),
+            _cache[2] || (_cache[2] = _createTextVNode(
+              " Beskrivning: ",
+              -1
+              /* CACHED */
+            )),
+            _createElementVNode(
+              "em",
+              null,
+              _toDisplayString(item.description),
+              1
+              /* TEXT */
+            ),
+            _cache[3] || (_cache[3] = _createElementVNode(
+              "br",
+              null,
+              null,
+              -1
+              /* CACHED */
+            ))
+          ])
+        ]),
+        screenreader: _withCtx(({ item }) => [
+          _createTextVNode(
+            _toDisplayString(item.name),
+            1
+            /* TEXT */
+          )
+        ]),
+        _: 1
+        /* STABLE */
+      }, 8, ["items"])
+    ]),
+    _: 1
+    /* STABLE */
+  }, 8, ["data", "sortable-attributes"]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-b6c3d2"
+});
+export {
+  render
+};
