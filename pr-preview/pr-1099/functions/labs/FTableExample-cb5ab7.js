f838f2d16d4ec532c3029d9c5bdd58d74d2dc20b
MCFK <MCFK@users.noreply.github.com>
2026-03-16 13:41:21 +0000

    Deploy preview for PR 1099 🛫

--- pr-preview/pr-1099/functions/labs/FTableExample-cb5ab7.js
+++ pr-preview/pr-1099/functions/labs/FTableExample-cb5ab7.js
@@ -5135,14 +5135,9 @@ function usePopupError() {
       anchor,
       arrowAnchor,
       hasFocus,
-      hasHover,
-      inEdit
+      hasHover
     } = popupError;
     if (!anchor || !arrowAnchor) {
-      return;
-    }
-    if (inEdit) {
-      onClosePopupError(popupError);
       return;
     }
     if (hasFocus || hasHover) {
--- pr-preview/pr-1099/functions/labs/FTablePageObject-headerTitle-811060.js
+++ pr-preview/pr-1099/functions/labs/FTablePageObject-headerTitle-811060.js
@@ -5133,14 +5133,9 @@ function usePopupError() {
       anchor,
       arrowAnchor,
       hasFocus,
-      hasHover,
-      inEdit
+      hasHover
     } = popupError;
     if (!anchor || !arrowAnchor) {
-      return;
-    }
-    if (inEdit) {
-      onClosePopupError(popupError);
       return;
     }
     if (hasFocus || hasHover) {
