8c44e4d63e6283de9423ad1ad0265041be0c6427
NicklasWei <NicklasWei@users.noreply.github.com>
2026-03-18 14:07:47 +0000

    Deploy preview for PR 1015 🛫

--- pr-preview/pr-1015/functions/labs/FTablePageObject-headerTitle-37b6d2.js
+++ pr-preview/pr-1015/functions/labs/FTablePageObject-headerTitle-37b6d2.js
@@ -4958,14 +4958,6 @@ function normalizeNumberColumn(column) {
     parser
   };
 }
-function normalizeRadioColumn(column) {
-  return {
-    type: "radio",
-    label: getLabelFn(column.label),
-    checked: getValueFn(column.checked, column.key, Boolean, false),
-    update: getUpdateFn(column.update, column.key)
-  };
-}
 function normalizeRenderColumn(column) {
   return {
     type: void 0,
@@ -5046,12 +5038,6 @@ function normalizeTableColumn(column) {
         ...normalizeCheckboxColumn(column),
         ...base,
         component: _sfc_main$c
-      };
-    case "radio":
-      return {
-        ...normalizeRadioColumn(column),
-        ...base,
-        component: _sfc_main$b
       };
     case "text:currency":
     case "text:number":
