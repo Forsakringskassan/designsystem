51b869a548bfb362f044d9016d40bc76a642f999
Stefan-ferm <Stefan-ferm@users.noreply.github.com>
2026-03-19 14:45:27 +0000

    Deploy preview for PR 1119 🛫

--- pr-preview/pr-1119/components/FCalendarLiveExample-f2e71f.js
+++ pr-preview/pr-1119/components/FCalendarLiveExample-f2e71f.js
@@ -4218,21 +4218,25 @@ var IPopup_default = defineComponent12({
             if (this.isOpen) {
               document.addEventListener("click", this.onDocumentClickHandler);
               window.addEventListener("resize", this.onWindowResizeDebounced);
+              window.addEventListener("scroll", this.onScrollDebounced, { capture: true });
             }
           }, 0);
         } else {
           document.removeEventListener("click", this.onDocumentClickHandler);
           window.removeEventListener("resize", this.onWindowResizeDebounced);
+          window.removeEventListener("scroll", this.onScrollDebounced, { capture: true });
         }
       }
     }
   },
   created() {
     this.onWindowResizeDebounced = debounce(this.onWindowResize, 100).bind(this);
+    this.onScrollDebounced = debounce(this.onScroll, 100).bind(this);
   },
   unmounted() {
     document.removeEventListener("click", this.onDocumentClickHandler);
     window.removeEventListener("resize", this.onWindowResizeDebounced);
+    window.removeEventListener("scroll", this.onScrollDebounced, { capture: true });
   },
   methods: {
     async toggleIsOpen(isOpen) {
@@ -4249,7 +4253,7 @@ var IPopup_default = defineComponent12({
       this.applyFocus();
       this.$emit("open");
     },
-    async calculatePlacement() {
+    async calculatePlacement(options) {
       const popup = getHTMLElementFromVueRef(this.$refs.popup);
       const wrapper = getHTMLElementFromVueRef(this.$refs.wrapper);
       const anchor = getElement(this.anchor);
@@ -4272,6 +4276,9 @@ var IPopup_default = defineComponent12({
         const useOverlay = this.forceOverlay || result.placement !== "Fallback" /* Fallback */;
         if (useOverlay) {
           wrapper.style.left = `${String(result.x)}px`;
+          if (options?.horizontalOnly) {
+            return;
+          }
           wrapper.style.top = `${String(result.y)}px`;
           return;
         }
@@ -4314,7 +4321,23 @@ var IPopup_default = defineComponent12({
     },
     onWindowResizeDebounced() {
     },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars -- Need to match actual `onScroll` method.
+    onScrollDebounced(event) {
+    },
     async onWindowResize() {
+      await this.recalculatePlacement();
+    },
+    async onScroll(event) {
+      if (this.isInline) {
+        return;
+      }
+      const isPopupTarget = event.target instanceof HTMLElement && Boolean(event.target.closest(".popup"));
+      if (isPopupTarget) {
+        return;
+      }
+      await this.recalculatePlacement({ horizontalOnly: true });
+    },
+    async recalculatePlacement(options) {
       if (!this.isOpen) {
         return;
       }
@@ -4329,7 +4352,7 @@ var IPopup_default = defineComponent12({
         this.teleportDisabled = false;
         await this.$nextTick();
       }
-      await this.calculatePlacement();
+      await this.calculatePlacement(options);
       const { placement, forceInline, forceOverlay } = this;
       this.teleportDisabled = isTeleportDisabled({ window, placement, forceInline, forceOverlay });
     },
