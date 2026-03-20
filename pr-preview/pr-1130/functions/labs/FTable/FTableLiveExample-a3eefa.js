28012895068036d8940bbc5e02b2c415230b0914
MatsWilson <MatsWilson@users.noreply.github.com>
2026-03-20 07:05:42 +0000

    Deploy preview for PR 1130 🛫

--- pr-preview/pr-1130/functions/labs/FTable/FTableLiveExample-a3eefa.js
+++ pr-preview/pr-1130/functions/labs/FTable/FTableLiveExample-a3eefa.js
@@ -762,10 +762,10 @@ function requireSharedStore() {
   var SHARED = "__core-js_shared__";
   var store = sharedStore.exports = globalThis2[SHARED] || defineGlobalProperty2(SHARED, {});
   (store.versions || (store.versions = [])).push({
-    version: "3.48.0",
+    version: "3.49.0",
     mode: IS_PURE ? "pure" : "global",
     copyright: "\xA9 2013\u20132025 Denis Pushkarev (zloirock.ru), 2025\u20132026 CoreJS Company (core-js.io). All rights reserved.",
-    license: "https://github.com/zloirock/core-js/blob/v3.48.0/LICENSE",
+    license: "https://github.com/zloirock/core-js/blob/v3.49.0/LICENSE",
     source: "https://github.com/zloirock/core-js"
   });
   return sharedStore.exports;
@@ -1708,15 +1708,17 @@ function requireIterate() {
     var fn = bind(unboundFunction, that);
     var iterator, iterFn, index, length, result, next, step;
     var stop = function(condition) {
-      if (iterator) iteratorClose2(iterator, "normal");
+      var $iterator = iterator;
+      iterator = void 0;
+      if ($iterator) iteratorClose2($iterator, "normal");
       return new Result(true, condition);
     };
-    var callFn = function(value) {
+    var callFn = function(value2) {
       if (AS_ENTRIES) {
-        anObject2(value);
-        return INTERRUPTED ? fn(value[0], value[1], stop) : fn(value[0], value[1]);
+        anObject2(value2);
+        return INTERRUPTED ? fn(value2[0], value2[1], stop) : fn(value2[0], value2[1]);
       }
-      return INTERRUPTED ? fn(value, stop) : fn(value);
+      return INTERRUPTED ? fn(value2, stop) : fn(value2);
     };
     if (IS_RECORD) {
       iterator = iterable.iterator;
@@ -1736,10 +1738,12 @@ function requireIterate() {
     }
     next = IS_RECORD ? iterable.next : iterator.next;
     while (!(step = call(next, iterator)).done) {
+      var value = step.value;
       try {
-        result = callFn(step.value);
+        result = callFn(value);
       } catch (error) {
-        iteratorClose2(iterator, "throw", error);
+        if (iterator) iteratorClose2(iterator, "throw", error);
+        else throw error;
       }
       if (typeof result == "object" && result && isPrototypeOf(ResultPrototype, result)) return result;
     }
@@ -2213,11 +2217,13 @@ function requireIteratorCreateProxy() {
       "return": function() {
         var state = getInternalState(this);
         var iterator = state.iterator;
+        var done = state.done;
         state.done = true;
         if (IS_ITERATOR) {
           var returnMethod = getMethod2(iterator, "return");
           return returnMethod ? call(returnMethod, iterator) : createIterResultObject2(void 0, true);
         }
+        if (done) return createIterResultObject2(void 0, true);
         if (state.inner) try {
           iteratorClose2(state.inner.iterator, NORMAL);
         } catch (error) {
@@ -2226,7 +2232,8 @@ function requireIteratorCreateProxy() {
         if (state.openIters) try {
           iteratorCloseAll2(state.openIters, NORMAL);
         } catch (error) {
-          return iteratorClose2(iterator, THROW, error);
+          if (iterator) return iteratorClose2(iterator, THROW, error);
+          throw error;
         }
         if (iterator) iteratorClose2(iterator, NORMAL);
         return createIterResultObject2(void 0, true);
@@ -2510,7 +2517,7 @@ function requireSetDifference() {
     var O = aSet2(this);
     var otherRec = getSetRecord2(other);
     var result = clone(O);
-    if (size(O) <= otherRec.size) iterateSet(O, function(e) {
+    if (size(result) <= otherRec.size) iterateSet(result, function(e) {
       if (otherRec.includes(e)) remove(result, e);
     });
     else iterateSimple2(otherRec.getIterator(), function(e) {
@@ -2699,7 +2706,7 @@ function requireSetIsDisjointFrom() {
     }, true) !== false;
     var iterator = otherRec.getIterator();
     return iterateSimple2(iterator, function(e) {
-      if (has(O, e)) return iteratorClose2(iterator, "normal", false);
+      if (has(O, e)) return iteratorClose2(iterator.iterator, "normal", false);
     }) !== false;
   };
   return setIsDisjointFrom;
@@ -2784,7 +2791,7 @@ function requireSetIsSupersetOf() {
     if (size(O) < otherRec.size) return false;
     var iterator = otherRec.getIterator();
     return iterateSimple2(iterator, function(e) {
-      if (!has(O, e)) return iteratorClose2(iterator, "normal", false);
+      if (!has(O, e)) return iteratorClose2(iterator.iterator, "normal", false);
     }) !== false;
   };
   return setIsSupersetOf;
@@ -3062,6 +3069,9 @@ function getCell(element) {
 }
 async function setDefaultCellTarget(table) {
   await nextTick3();
+  if (!table.tHead) {
+    return null;
+  }
   const target = getCellTarget(table, 1, 0);
   activateCell(target, {
     focus: false
@@ -3911,7 +3921,7 @@ function requireDoesNotExceedSafeInteger() {
   var $TypeError = TypeError;
   var MAX_SAFE_INTEGER = 9007199254740991;
   doesNotExceedSafeInteger = function(it) {
-    if (it > MAX_SAFE_INTEGER) throw $TypeError("Maximum allowed index exceeded");
+    if (it > MAX_SAFE_INTEGER) throw new $TypeError("Maximum allowed index exceeded");
     return it;
   };
   return doesNotExceedSafeInteger;
@@ -5037,26 +5047,26 @@ function normalizeTextColumn(column) {
 function normalizeTableColumn(column) {
   const base = normalizeBaseColumn(column);
   if ("render" in column) {
-    return {
+    return Object.freeze({
       ...normalizeRenderColumn(column),
       ...base
-    };
+    });
   }
   switch (column.type) {
     case "checkbox":
-      return {
+      return Object.freeze({
         ...normalizeCheckboxColumn(column),
         ...base,
         component: _sfc_main$c
-      };
+      });
     case "text:currency":
     case "text:number":
     case "text:percent":
-      return {
+      return Object.freeze({
         ...normalizeNumberColumn(column),
         ...base,
         component: _sfc_main$4
-      };
+      });
     case "text":
     case "text:bankAccountNumber":
     case "text:bankgiro":
@@ -5068,47 +5078,47 @@ function normalizeTableColumn(column) {
     case "text:phoneNumber":
     case "text:plusgiro":
     case "text:postalCode":
-      return {
+      return Object.freeze({
         ...normalizeTextColumn(column),
         ...base,
         component: _sfc_main$4
-      };
+      });
     case "rowheader":
-      return {
+      return Object.freeze({
         ...normalizeRowHeaderColumn(column),
         ...base,
         component: _sfc_main$6
-      };
+      });
     case "anchor":
-      return {
+      return Object.freeze({
         ...normalizeAnchorColumn(column),
         ...base,
         component: _sfc_main$9
-      };
+      });
     case "button":
-      return {
+      return Object.freeze({
         ...normalizeButtonColumn(column),
         ...base,
         component: _sfc_main$8
-      };
+      });
     case "select":
-      return {
+      return Object.freeze({
         ...normalizeSelectColumn(column),
         ...base,
         component: _sfc_main$5
-      };
+      });
     case "menu":
-      return {
+      return Object.freeze({
         ...normalizeMenuColumn(column),
         ...base,
         component: _sfc_main$7
-      };
+      });
     case void 0:
-      return {
+      return Object.freeze({
         ...normalizeSimpleColumn(column),
         ...base,
         component: _sfc_main$4
-      };
+      });
   }
 }
 function defineTableColumns(columns) {
@@ -5507,26 +5517,32 @@ var _hoisted_2$1 = {
   "data-test": "caption"
 };
 var _hoisted_3$1 = {
+  key: 1
+};
+var _hoisted_4$1 = {
   class: "table-ng__row",
   "aria-rowindex": "1"
 };
-var _hoisted_4$1 = {
+var _hoisted_5$1 = {
   key: 0,
   scope: "col",
   tabindex: "-1",
   class: "table-ng__column"
 };
-var _hoisted_5$1 = {
+var _hoisted_6$1 = {
+  key: 2
+};
+var _hoisted_7$1 = {
   key: 0,
   class: "table-ng__row--empty"
 };
-var _hoisted_6$1 = ["colspan"];
-var _hoisted_7$1 = ["aria-level", "aria-rowindex", "aria-setsize", "aria-posinset", "aria-selected"];
-var _hoisted_8 = {
-  key: 1
+var _hoisted_8 = ["colspan"];
+var _hoisted_9 = ["aria-level", "aria-rowindex", "aria-setsize", "aria-posinset", "aria-selected"];
+var _hoisted_10 = {
+  key: 3
 };
-var _hoisted_9 = ["aria-rowindex"];
-var _hoisted_10 = ["colspan"];
+var _hoisted_11 = ["aria-rowindex"];
+var _hoisted_12 = ["colspan"];
 var _sfc_main$3 = /* @__PURE__ */ defineComponent2({
   __name: "FTable",
   props: /* @__PURE__ */ mergeModels({
@@ -5577,20 +5593,35 @@ var _sfc_main$3 = /* @__PURE__ */ defineComponent2({
       return metaRows.value.length === 0;
     });
     const ariaRowcount = computed3(() => {
-      const headerRow = 1;
       const footerRow = hasFooter.value ? 1 : 0;
-      return getBodyRowCount(keyedRows.value, __props.expandableAttribute) + headerRow + footerRow;
+      if (!hasColumns.value) {
+        return footerRow;
+      }
+      const headerRow = 1;
+      const bodyRows = getBodyRowCount(keyedRows.value, __props.expandableAttribute);
+      return bodyRows + headerRow + footerRow;
     });
-    const columnCount = computed3(() => {
-      const expandCol = isTreegrid.value ? 1 : 0;
-      const selectCol = __props.selectable ? 1 : 0;
-      const count = columns.value.length + expandCol + selectCol;
-      return Math.max(1, count);
+    const fullColspan = computed3(() => {
+      if (!hasColumns.value) {
+        return 0;
+      }
+      let count = columns.value.length;
+      if (isTreegrid.value) {
+        count += 1;
+      }
+      if (__props.selectable) {
+        count += 1;
+      }
+      return count;
+    });
+    const expandedColspan = computed3(() => {
+      return fullColspan.value - 1;
     });
     const hasFooter = computed3(() => {
       return hasSlot("footer");
     });
     const columns = computed3(() => normalizeTableColumns(__props.columns).filter((col) => toValue2(col.enabled)));
+    const hasColumns = computed3(() => columns.value.length > 0);
     const tableClasses = computed3(() => {
       return ["table-ng", {
         "table-ng--striped": __props.striped,
@@ -5754,7 +5785,7 @@ var _sfc_main$3 = /* @__PURE__ */ defineComponent2({
         onFocusout: onTableFocusout,
         onClick,
         onKeydown
-      }, [hasCaption.value ? (openBlock(), createElementBlock("caption", _hoisted_2$1, [renderSlot(_ctx.$slots, "caption")])) : createCommentVNode("", true), _cache[4] || (_cache[4] = createTextVNode()), createElementVNode("thead", null, [createElementVNode("tr", _hoisted_3$1, [isTreegrid.value ? (openBlock(), createElementBlock("th", _hoisted_4$1)) : createCommentVNode("", true), _cache[0] || (_cache[0] = createTextVNode()), __props.selectable ? (openBlock(), createBlock(_sfc_main$d, {
+      }, [hasCaption.value ? (openBlock(), createElementBlock("caption", _hoisted_2$1, [renderSlot(_ctx.$slots, "caption")])) : createCommentVNode("", true), _cache[4] || (_cache[4] = createTextVNode()), hasColumns.value ? (openBlock(), createElementBlock("thead", _hoisted_3$1, [createElementVNode("tr", _hoisted_4$1, [isTreegrid.value ? (openBlock(), createElementBlock("th", _hoisted_5$1)) : createCommentVNode("", true), _cache[0] || (_cache[0] = createTextVNode()), __props.selectable ? (openBlock(), createBlock(_sfc_main$d, {
         key: 1,
         ref: bindCellApiRef,
         state: unref3(selectableHeaderState)(),
@@ -5769,10 +5800,10 @@ var _sfc_main$3 = /* @__PURE__ */ defineComponent2({
           scope: "col",
           onToggleSortOrder
         }, null, 8, ["column", "sort-enabled", "sort-order"]);
-      }), 128))])]), _cache[5] || (_cache[5] = createTextVNode()), createElementVNode("tbody", null, [isEmpty2.value ? (openBlock(), createElementBlock("tr", _hoisted_5$1, [createElementVNode("td", {
-        colspan: columnCount.value,
+      }), 128))])])) : createCommentVNode("", true), _cache[5] || (_cache[5] = createTextVNode()), hasColumns.value ? (openBlock(), createElementBlock("tbody", _hoisted_6$1, [isEmpty2.value ? (openBlock(), createElementBlock("tr", _hoisted_7$1, [createElementVNode("td", {
+        colspan: fullColspan.value,
         class: "table-ng__cell"
-      }, [renderSlot(_ctx.$slots, "empty", {}, () => [createTextVNode(toDisplayString(unref3($t)("fkui.ftable.empty.text", "Tabellen \xE4r tom")), 1)])], 8, _hoisted_6$1)])) : (openBlock(true), createElementBlock(Fragment2, {
+      }, [renderSlot(_ctx.$slots, "empty", {}, () => [createTextVNode(toDisplayString(unref3($t)("fkui.ftable.empty.text", "Tabellen \xE4r tom")), 1)])], 8, _hoisted_8)])) : (openBlock(true), createElementBlock(Fragment2, {
         key: 1
       }, renderList(metaRows.value, ({
         key,
@@ -5802,7 +5833,7 @@ var _sfc_main$3 = /* @__PURE__ */ defineComponent2({
           onToggle: onToggleExpanded
         }, null, 8, ["is-expandable", "is-expanded", "row-key"])) : createCommentVNode("", true), _cache[3] || (_cache[3] = createTextVNode()), level > 1 && hasExpandableSlot.value ? (openBlock(), createBlock(_sfc_main$f, {
           key: 1,
-          colspan: columns.value.length
+          colspan: expandedColspan.value
         }, {
           default: withCtx(() => [renderSlot(_ctx.$slots, "expandable", mergeProps({
             ref_for: true
@@ -5837,14 +5868,14 @@ var _sfc_main$3 = /* @__PURE__ */ defineComponent2({
             key: 1,
             row
           }, null, 8, ["row"])) : createCommentVNode("", true)], 64);
-        }), 128))], 64))], 8, _hoisted_7$1);
-      }), 128))]), _cache[6] || (_cache[6] = createTextVNode()), hasFooter.value ? (openBlock(), createElementBlock("tfoot", _hoisted_8, [createElementVNode("tr", {
+        }), 128))], 64))], 8, _hoisted_9);
+      }), 128))])) : createCommentVNode("", true), _cache[6] || (_cache[6] = createTextVNode()), hasFooter.value ? (openBlock(), createElementBlock("tfoot", _hoisted_10, [createElementVNode("tr", {
         class: "table-ng__row",
         "aria-rowindex": ariaRowcount.value
       }, [createElementVNode("td", {
-        colspan: columnCount.value,
+        colspan: fullColspan.value,
         class: "table-ng__cell--custom"
-      }, [renderSlot(_ctx.$slots, "footer")], 8, _hoisted_10)], 8, _hoisted_9)])) : createCommentVNode("", true)], 42, _hoisted_1$2);
+      }, [renderSlot(_ctx.$slots, "footer")], 8, _hoisted_12)], 8, _hoisted_11)])) : createCommentVNode("", true)], 42, _hoisted_1$2);
     };
   }
 });
@@ -6121,6 +6152,7 @@ function requireArrayBufferTransfer() {
   var structuredClone = globalThis2.structuredClone;
   var ArrayBuffer2 = globalThis2.ArrayBuffer;
   var DataView2 = globalThis2.DataView;
+  var max = Math.max;
   var min = Math.min;
   var ArrayBufferPrototype = ArrayBuffer2.prototype;
   var DataViewPrototype = DataView2.prototype;
@@ -6145,7 +6177,7 @@ function requireArrayBufferTransfer() {
       newBuffer = slice(arrayBuffer, 0, newByteLength);
     } else {
       var options = preserveResizability && !fixedLength && maxByteLength ? {
-        maxByteLength: maxByteLength(arrayBuffer)
+        maxByteLength: max(newByteLength, maxByteLength(arrayBuffer))
       } : void 0;
       newBuffer = new ArrayBuffer2(newByteLength, options);
       var a = new DataView2(arrayBuffer);
@@ -6405,7 +6437,7 @@ function requireArrayBufferViewCore() {
       }
     });
     for (NAME in TypedArrayConstructorsList) if (globalThis2[NAME]) {
-      createNonEnumerableProperty2(globalThis2[NAME], TYPED_ARRAY_TAG, NAME);
+      createNonEnumerableProperty2(globalThis2[NAME].prototype, TYPED_ARRAY_TAG, NAME);
     }
   }
   arrayBufferViewCore = {
@@ -6809,26 +6841,25 @@ function requireUint8FromHex() {
   var uncurryThis = requireFunctionUncurryThis();
   var Uint8Array2 = globalThis2.Uint8Array;
   var SyntaxError = globalThis2.SyntaxError;
-  var parseInt = globalThis2.parseInt;
   var min = Math.min;
-  var NOT_HEX = /[^\da-f]/i;
-  var exec = uncurryThis(NOT_HEX.exec);
-  var stringSlice = uncurryThis("".slice);
+  var stringMatch = uncurryThis("".match);
   uint8FromHex = function(string, into) {
     var stringLength = string.length;
     if (stringLength % 2 !== 0) throw new SyntaxError("String should be an even number of characters");
     var maxLength = into ? min(into.length, stringLength / 2) : stringLength / 2;
     var bytes = into || new Uint8Array2(maxLength);
-    var read = 0;
+    var segments = stringMatch(string, /.{2}/g);
     var written = 0;
-    while (written < maxLength) {
-      var hexits = stringSlice(string, read, read += 2);
-      if (exec(NOT_HEX, hexits)) throw new SyntaxError("String should only contain hex characters");
-      bytes[written++] = parseInt(hexits, 16);
+    for (; written < maxLength; written++) {
+      var result = +("0x" + segments[written] + "0");
+      if (result !== result) {
+        throw new SyntaxError("String should only contain hex characters");
+      }
+      bytes[written] = result >> 4;
     }
     return {
       bytes,
-      read
+      read: written << 1
     };
   };
   return uint8FromHex;
@@ -6943,6 +6974,8 @@ function requireEs_uint8Array_toHex() {
   var anUint8Array2 = requireAnUint8Array();
   var notDetached = requireArrayBufferNotDetached();
   var numberToString = uncurryThis(1.1.toString);
+  var join = uncurryThis([].join);
+  var $Array = Array;
   var Uint8Array2 = globalThis2.Uint8Array;
   var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array2 || !Uint8Array2.prototype.toHex || !(function() {
     try {
@@ -6960,12 +6993,12 @@ function requireEs_uint8Array_toHex() {
     toHex: function toHex() {
       anUint8Array2(this);
       notDetached(this.buffer);
-      var result = "";
+      var result = $Array(this.length);
       for (var i = 0, length = this.length; i < length; i++) {
         var hex = numberToString(this[i], 16);
-        result += hex.length === 1 ? "0" + hex : hex;
+        result[i] = hex.length === 1 ? "0" + hex : hex;
       }
-      return result;
+      return join(result, "");
     }
   });
   return es_uint8Array_toHex;
