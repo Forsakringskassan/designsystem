b2624aeea9b0fbcab7e52a9392e9eabf29437b99
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2026-03-25 06:29:25 +0000

    chore(release): [skip ci] deploy documentation from main branch

--- main/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-content-b66d4f.js
+++ main/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-content-b66d4f.js
@@ -28,7 +28,7 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-content.vue:FMinimizablePanelPageObject-content-debbfb.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FMinimizablePanel/FMinimizablePanelPageObject-content.vue:FMinimizablePanelPageObject-content-b66d4f.js
 import { defineComponent as _defineComponent } from "vue";
 import { FMinimizablePanel, FPageLayout } from "@fkui/vue";
 import { createElementVNode as _createElementVNode, withCtx as _withCtx, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
@@ -72,7 +72,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-debbfb"
+  selector: "#example-b66d4f"
 });
 export {
   render
