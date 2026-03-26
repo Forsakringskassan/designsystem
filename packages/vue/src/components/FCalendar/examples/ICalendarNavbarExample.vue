c8024ad819845d6901b5072ccfa1ca8a2b035857
Udenius Sebastian (66124381) <sebastian.udenius@forsakringskassan.se>
2026-03-26 15:45:05 +0100

    test: new `ICalendarNavbar` screenshots with `forced-colors` media feature (refs SFKUI-7573)

---
+++ packages/vue/src/components/FCalendar/examples/ICalendarNavbarExample.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent, shallowRef } from "vue";
+import { FDate } from "@fkui/date";
+import { ICalendarNavbar } from "../../../internal-components";
+
+export default defineComponent({
+    components: {
+        ICalendarNavbar,
+    },
+    data() {
+        return {
+            minDate: shallowRef(FDate.fromIso("2026-01-01")),
+            maxDate: shallowRef(FDate.fromIso("2026-12-31")),
+        };
+    },
+});
+</script>
+
+<template>
+    <i-calendar-navbar v-model="minDate" :min-date :max-date />
+    <i-calendar-navbar v-model="maxDate" :min-date :max-date :year-selector="true" />
+</template>
