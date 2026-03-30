45c6baa1e75aa027c196f1ec89a39a1632532c1f
udenius <udenius@users.noreply.github.com>
2026-03-30 14:20:29 +0000

    Deploy preview for PR 1178 🛫

---
+++ pr-preview/pr-1178/functions/plugins/FormatPluginOrgnr-c8065e.js
@@ -0,0 +1,63 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:packages/vue/src/plugins/format/examples/FormatPluginOrgnr.vue:FormatPluginOrgnr-c8065e.js
+import { defineComponent as _defineComponent } from "vue";
+import { resolveDirective as _resolveDirective, withDirectives as _withDirectives, openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue";
+var orgnr = "9999999999";
+var exampleComponent = /* @__PURE__ */ _defineComponent({
+  __name: "FormatPluginOrgnr",
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const __returned__ = { orgnr };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _directive_format = _resolveDirective("format");
+  return _withDirectives((_openBlock(), _createElementBlock(
+    "p",
+    null,
+    null,
+    512
+    /* NEED_PATCH */
+  )), [
+    [_directive_format, $setup.orgnr, "orgnr"]
+  ]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-c8065e"
+});
+export {
+  render
+};
