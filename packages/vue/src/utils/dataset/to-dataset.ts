16b526c0c9f8f5489b3b7ec3490b19a2d1dcb506
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-03-31 15:24:23 +0200

    refactor: `toDataset()` optionally accepts a parameter for the original dataset (refs SFKUI-6500)

--- packages/vue/src/utils/dataset/to-dataset.ts
+++ packages/vue/src/utils/dataset/to-dataset.ts
@@ -5,7 +5,71 @@ import {
 } from "./dataset";
 import { type DatasetArrayMetadata } from "./dataset-array-metadata";
 import { type DatasetElementMetadata } from "./dataset-element-metadata";
+import { getDatasetMetadata } from "./get-dataset-metadata";
 import { isDataset } from "./is-dataset";
+
+function createDataset<T extends object>(
+    array: T[],
+    nestedAttribute: DatasetNestedKeyOf<T> | undefined,
+): Dataset<T> {
+    for (const [index, element] of array.entries()) {
+        if (Object.getOwnPropertyDescriptor(element, datasetSymbol)) {
+            continue;
+        }
+        Object.defineProperty(element, datasetSymbol, {
+            value: {
+                rowIndex: index,
+                ariaRowIndex: index + 1,
+                ariaLevel: 1,
+                ariaSetSize: array.length,
+                ariaPosInSet: index + 1,
+            } satisfies DatasetElementMetadata,
+            enumerable: false,
+            configurable: true,
+            writable: true,
+        });
+    }
+
+    const value: DatasetArrayMetadata<T> = {
+        size: array.length,
+        nestedAttribute,
+    };
+
+    /* The `Dataset<T>` interface declares branding with an opaque marker
+     * `[datasetSymbol]: true`. The actual runtime value is an object which
+     * satisfies the truthy branding. Code should use `isDataset()` to test if
+     * an array is actually a `Dataset<T>` or `getDatasetMetadata()` to retrieve
+     * the metadata. */
+    return Object.defineProperty(array, datasetSymbol, {
+        value,
+        enumerable: false,
+        configurable: true,
+        writable: true,
+    }) as Dataset<T>;
+}
+
+function inheritDataset<T extends object>(
+    array: T[],
+    originalDataset: Dataset<T>,
+): Dataset<T> {
+    const metadata = getDatasetMetadata(originalDataset);
+
+    const value: DatasetArrayMetadata<T> = {
+        get size() {
+            return metadata.size;
+        },
+        get nestedAttribute() {
+            return metadata.nestedAttribute;
+        },
+    };
+
+    return Object.defineProperty(array, datasetSymbol, {
+        value,
+        enumerable: false,
+        configurable: false,
+        writable: false,
+    }) as Dataset<T>;
+}
 
 /**
  * Creates a dataset from an array.
@@ -23,45 +87,35 @@ import { isDataset } from "./is-dataset";
  * @since v6.39.0
  */
 export function toDataset<T extends object>(
+    array: T[],
+    nestedAttribute?: DatasetNestedKeyOf<T>,
+): Dataset<T>;
+
+/**
+ * Creates a dataset based on another existing dataset.
+ *
+ * @internal
+ * @since %version%
+ */
+export function toDataset<T extends object>(
     dataset: T[],
-    nestedAttribute: DatasetNestedKeyOf<T> | undefined,
+    originalDataset: Dataset<T>,
+): Dataset<T>;
+
+export function toDataset<T extends object>(
+    array: T[],
+    ...args:
+        | [nestedAttribute?: DatasetNestedKeyOf<T>]
+        | [originalDataset: Dataset<T>]
 ): Dataset<T> {
-    if (isDataset(dataset)) {
-        return dataset;
+    if (isDataset(array)) {
+        return array;
     }
 
-    for (const [index, element] of dataset.entries()) {
-        if (Object.getOwnPropertyDescriptor(element, datasetSymbol)) {
-            continue;
-        }
-        Object.defineProperty(element, datasetSymbol, {
-            value: {
-                rowIndex: index,
-                ariaRowIndex: index + 1,
-                ariaLevel: 1,
-                ariaSetSize: dataset.length,
-                ariaPosInSet: index + 1,
-            } satisfies DatasetElementMetadata,
-            enumerable: false,
-            configurable: true,
-            writable: true,
-        });
+    const [arg] = args;
+    if (Array.isArray(arg)) {
+        return inheritDataset(array, arg);
+    } else {
+        return createDataset(array, arg);
     }
-
-    const value: DatasetArrayMetadata<T> = {
-        size: dataset.length,
-        nestedAttribute,
-    };
-
-    /* The `Dataset<T>` interface declares branding with an opaque marker
-     * `[datasetSymbol]: true`. The actual runtime value is an object which
-     * satisfies the truthy branding. Code should use `isDataset()` to test if
-     * an array is actually a `Dataset<T>` or `getDatasetMetadata()` to retrieve
-     * the metadata. */
-    return Object.defineProperty(dataset, datasetSymbol, {
-        value,
-        enumerable: false,
-        configurable: true,
-        writable: true,
-    }) as Dataset<T>;
 }
