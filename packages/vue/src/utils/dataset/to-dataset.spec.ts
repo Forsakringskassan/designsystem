8fdd33acda5c270917263658743b7489ccb696b1
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-03-31 15:24:23 +0200

    refactor: `toDataset()` optionally accepts a parameter for the original dataset (refs SFKUI-6500)

--- packages/vue/src/utils/dataset/to-dataset.spec.ts
+++ packages/vue/src/utils/dataset/to-dataset.spec.ts
@@ -1,5 +1,21 @@
+import { getDatasetMetadata } from "./get-dataset-metadata";
 import { isDataset } from "./is-dataset";
 import { toDataset } from "./to-dataset";
+import { rowindex, treeSnapshot } from "./tree-snapshot";
+
+interface Row {
+    id: number;
+    children?: Row[];
+}
+
+expect.addSnapshotSerializer({
+    test(value) {
+        return typeof value === "string";
+    },
+    serialize(value) {
+        return String(value);
+    },
+});
 
 describe("toDataset", () => {
     it("should create a dataset from array", () => {
@@ -26,5 +42,48 @@ describe("toDataset", () => {
         expect(Object.keys(result)).toEqual(["0"]); // indices
         expect(Object.keys(result[0])).toEqual(["foo", "spam"]);
         expect(JSON.stringify(result)).toBe(JSON.stringify(dataset));
+    });
+
+    describe("with originalDataset", () => {
+        it("should create a dataset from filtered array", () => {
+            expect.assertions(2);
+            const items: Row[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
+            const original = toDataset(items);
+            const filtered = toDataset(
+                original.filter((it) => it.id !== 2),
+                original,
+            );
+            const before = treeSnapshot(original, rowindex);
+            const after = treeSnapshot(filtered, rowindex);
+            expect(before).toMatchInlineSnapshot(`
+                length=3 size=3
+                ├─ id:1 row-index=0
+                ├─ id:2 row-index=1
+                └─ id:3 row-index=2
+            `);
+            expect(after).toMatchInlineSnapshot(`
+                length=2 size=3
+                ├─ id:1 row-index=0
+                └─ id:3 row-index=2
+            `);
+        });
+
+        it("should inherit size from the original dataset", () => {
+            expect.assertions(2);
+            const original = toDataset([{ id: 1 }, { id: 2 }, { id: 3 }]);
+            const filtered = toDataset([], original);
+            const { size } = getDatasetMetadata(filtered);
+            expect(filtered).toHaveLength(0);
+            expect(size).toBe(3);
+        });
+
+        it("should inherit nestedAttribute from the original dataset", () => {
+            expect.assertions(1);
+            const items: Row[] = [];
+            const original = toDataset(items, "children");
+            const filtered = toDataset([], original);
+            const { nestedAttribute } = getDatasetMetadata(filtered);
+            expect(nestedAttribute).toBe("children");
+        });
     });
 });
