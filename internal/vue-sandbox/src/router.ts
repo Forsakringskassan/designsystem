30537c9faa1ff8be55088a8b84ff0cf552d6f97b
Jacob Godin Forsberg <jacob.godin.forsberg@forsakringskassan.se>
2026-04-17 15:20:06 +0200

    chore: set up prototype sandbox infrastructure (refs SFKUI-6500)
    Converts DefaultView to a prototype index page with auto-generated
    links from the router. Adds prototypes/ convention for new routes
    and updates router to export the prototypes array.

--- internal/vue-sandbox/src/router.ts
+++ internal/vue-sandbox/src/router.ts
@@ -1,9 +1,24 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import DefaultView from "./views/DefaultView.vue";
+import ExamplePrototype from "./views/prototypes/ExamplePrototype.vue";
+import ResetFields from "./views/prototypes/ResetFields.vue";
+
+export const prototypes = [
+    {
+        path: "/prototyp/exempel",
+        name: "Exempelprototyp",
+        component: ExamplePrototype,
+    },
+    {
+        path: "/prototyp/aterstall-belopp",
+        name: "Återställ belopp",
+        component: ResetFields,
+    },
+];
 
 const router = createRouter({
     history: createWebHashHistory(),
-    routes: [{ path: "/", name: "", component: DefaultView }],
+    routes: [{ path: "/", name: "", component: DefaultView }, ...prototypes],
 });
 
 export default router;
