cdace61fc3101f37bf5c6f8859f1f97b09bb12ec
udenius <udenius@users.noreply.github.com>
2026-04-24 09:45:48 +0000

    Deploy preview for PR 1218 🛫

--- pr-preview/pr-1218/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-cell-134056.js
+++ pr-preview/pr-1218/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-cell-134056.js
@@ -28,7 +28,7 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-cell.vue:FInteractiveTablePageObject-cell-310fcc.js
+// virtual-entry:virtual:docs/functions/cypress/pageobjects/FInteractiveTablePageObject/FInteractiveTablePageObject-cell.vue:FInteractiveTablePageObject-cell-134056.js
 import { defineComponent as _defineComponent } from "vue";
 import { FInteractiveTable, FTableColumn } from "@fkui/vue";
 import { createTextVNode as _createTextVNode, toDisplayString as _toDisplayString, withCtx as _withCtx, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
@@ -122,7 +122,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-310fcc"
+  selector: "#example-134056"
 });
 export {
   render
