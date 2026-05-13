d582d124c6d317d2f9ad92c1a1e776235910eaf5
Toresson Agneta (66142540) <agneta.toresson@forsakringskassan.se>
2026-05-13 14:47:54 +0200

    test: updated test(refs SFKUI-7781)

--- packages/vue/src/components/FRadioField/FRadioField.cy.ts
+++ packages/vue/src/components/FRadioField/FRadioField.cy.ts
@@ -324,4 +324,42 @@ describe("FRadioField", () => {
             cy.toMatchScreenshot();
         });
     });
+
+    describe("Visual forcedColor", () => {
+        const forcedColorModes = ["none", "dark", "light"] as const;
+        afterEach(() => {
+            cy.forcedColors("none");
+        });
+        for (const mode of Object.values(forcedColorModes)) {
+            it(`should render correct styling for forced color mode , mode ${mode} (visual)`, () => {
+                cy.forcedColors(mode);
+
+                const template = /* HTML */ `
+                    <f-fieldset name="radio-name" border show-details="always">
+                        <template #label> Label text </template>
+                        <template #default>
+                            <f-radio-field v-model="radioModel" :value="true">
+                                <template #default> Yes </template>
+                                <template #details>
+                                    Value true - Details is always visible
+                                </template>
+                            </f-radio-field>
+                            <f-radio-field v-model="radioModel" :value="false">
+                                <template #default> No </template>
+                                <template #details>
+                                    Value false - Details is always visible
+                                </template>
+                            </f-radio-field>
+                        </template>
+                    </f-fieldset>
+                `;
+                cy.mount(createComponent(template));
+
+                const radio1 = radioGroup.radioButton(radioField.yes);
+                radio1.select();
+
+                cy.get("fieldset").toMatchScreenshot({ baseDelay: 500 });
+            });
+        }
+    });
 });
