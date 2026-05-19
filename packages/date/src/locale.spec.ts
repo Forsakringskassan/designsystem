11df49bb7d617048d7194d230ccefa43189a850d
Carlsson Martin <martin.carlsson@forsakringskassan.se>
2026-05-19 15:47:01 +0200

    test: migrate `@fkui/date` to vitest (refs SFKUI-7787)

--- packages/date/src/locale.spec.ts
+++ packages/date/src/locale.spec.ts
@@ -1,27 +1,30 @@
+import { beforeEach, describe, expect, it } from "vitest";
 import { Locale, getLocale, isLocale, resetLocale, setLocale } from "./locale";
 
 beforeEach(() => {
     resetLocale();
 });
 
-it("isLocale should return false for empty value", () => {
-    expect(isLocale()).toBe(false);
-});
+describe("locale", () => {
+    it("isLocale should return false for empty value", () => {
+        expect(isLocale()).toBe(false);
+    });
 
-it("isLocale should return boolean value for string", () => {
-    expect(isLocale("en")).toBe(true);
-    expect(isLocale("sv")).toBe(true);
-    expect(isLocale("SV")).toBe(false);
-    expect(isLocale("invalidLocale")).toBe(false);
-});
+    it("isLocale should return boolean value for string", () => {
+        expect(isLocale("en")).toBe(true);
+        expect(isLocale("sv")).toBe(true);
+        expect(isLocale("SV")).toBe(false);
+        expect(isLocale("invalidLocale")).toBe(false);
+    });
 
-it("isLocale should return true for locale enum", () => {
-    expect(isLocale(Locale.SWEDISH)).toBe(true);
-    expect(isLocale(Locale.ENGLISH)).toBe(true);
-});
+    it("isLocale should return true for locale enum", () => {
+        expect(isLocale(Locale.SWEDISH)).toBe(true);
+        expect(isLocale(Locale.ENGLISH)).toBe(true);
+    });
 
-it("setLocale should set current locale", () => {
-    expect(getLocale()).toEqual(Locale.SWEDISH);
-    setLocale(Locale.ENGLISH);
-    expect(getLocale()).toEqual(Locale.ENGLISH);
+    it("setLocale should set current locale", () => {
+        expect(getLocale()).toEqual(Locale.SWEDISH);
+        setLocale(Locale.ENGLISH);
+        expect(getLocale()).toEqual(Locale.ENGLISH);
+    });
 });
