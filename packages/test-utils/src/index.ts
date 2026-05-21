41d8021b8c62823a84fbc952f1715fb6049ab20f
MCFK <160876746+MCFK@users.noreply.github.com>
2026-05-21 11:17:21 +0000

    feat: add support for Vitest in @fkui/test-utils (refs SFKUI-7787)

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,12 +1,14 @@
 /**
- * This is a pseudo entrypoint used only to generate API Extractor report, do
- * not put your exports in here but rather in one of the following files:
+ * This is a pseudo entrypoint used only to generate the API Extractor report. Do
+ * not put your exports here, but rather in one of the following files:
  *
  * - `lib.ts` - if the exported symbol works in any environment.
  * - `vue/index.ts` - if the exported symbol is Vue.js specific, e.g. a Vue component.
- * - `jest.ts` - if the exported symbol only works in jest, e.g. jest matchers.
+ * - `jest.ts` - if the symbol only works in Jest, e.g. Jest matchers.
+ * - `vitest.ts` - if the symbol only works in Vitest, e.g. Vitest matchers.
  */
 
 export * from "./lib";
 export * from "./jest";
 export * from "./vue";
+export * from "./vitest";
---
+++ packages/test-utils/src/vitest.ts
@@ -0,0 +1,5 @@
+/* imported for side-effects only */
+import "./matchers/vitest-augmentations";
+import "./matchers";
+
+export * from "./lib";
