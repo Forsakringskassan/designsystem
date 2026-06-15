01e7c25aa43999b0cfa3f580882d83e5631b875f
Stefan-ferm <Stefan-ferm@users.noreply.github.com>
2026-06-15 07:43:43 +0000

    Deploy preview for PR 1354 🛫

--- pr-preview/pr-1354/components/FCalendarLiveExample-ed17a6.js
+++ pr-preview/pr-1354/components/FCalendarLiveExample-ed17a6.js
@@ -5,7 +5,11 @@ var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __commonJS = (cb, mod) => function __require() {
-  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  try {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  } catch (e) {
+    throw mod = 0, e;
+  }
 };
 var __copyProps = (to, from, except, desc) => {
   if (from && typeof from === "object" || typeof from === "function") {
@@ -3252,8 +3256,6 @@ var FValidationForm_default = defineComponent10({
         } else {
           this.$emit("submit", event);
         }
-      } catch (error) {
-        console.error("N\xE5got gick fel i parent-funktionen:", error);
       } finally {
         this.isInflight = false;
       }
