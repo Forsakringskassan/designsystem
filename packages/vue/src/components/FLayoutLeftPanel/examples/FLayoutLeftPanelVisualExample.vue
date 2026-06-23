79c5baf059eb248febfeae93db4248a74ff590f9
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2026-06-23 23:28:03 +0200

    test: add visual test to `FLayoutLeftPanel` (refs SFKUI-6500)

---
+++ packages/vue/src/components/FLayoutLeftPanel/examples/FLayoutLeftPanelVisualExample.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { FLayoutApplicationTemplate, FLayoutLeftPanel } from "@fkui/vue";
+</script>
+
+<template>
+    <f-layout-application-template>
+        <template #header>
+            <!-- [html-validate-disable-next no-inline-style] -->
+            <div style="background-color: green; color: white"><a href="#">[sidhuvud]</a></div>
+        </template>
+
+        <template #top-navigation>
+            <!-- [html-validate-disable-next no-inline-style] -->
+            <div style="background-color: lightgray"><a href="#">[toppnavigering]</a></div>
+        </template>
+
+        <f-layout-left-panel>
+            <template #heading>
+                <h3>Header</h3>
+            </template>
+
+            <template #content>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+            </template>
+
+            <template #default>
+                <div class="container-fluid">
+                    <h1>Header</h1>
+                    <p>Lorem ipsum.</p>
+                </div>
+            </template>
+        </f-layout-left-panel>
+
+        <template #footer>
+            <!-- [html-validate-disable-next no-inline-style] -->
+            <div style="background-color: green; color: white; text-align: center">
+                <a href="#">[sidfot]</a>
+            </div>
+        </template>
+    </f-layout-application-template>
+</template>
