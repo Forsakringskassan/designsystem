2fb38cf9eb56f7b2088ab27105d45157bfe9a841
atfkassan <atfkassan@users.noreply.github.com>
2026-07-03 14:47:02 +0000

    Deploy preview for PR 1470 🛫

--- pr-preview/pr-1470/assets/processors/selectable-version/select-version-f49c40.js
+++ pr-preview/pr-1470/assets/processors/selectable-version/select-version-f49c40.js
@@ -1969,7 +1969,7 @@ function memoize(callback) {
     return cache.get(key);
   };
 }
-var current = "v6.52.0";
+var current = "v6.52.1";
 var message = "Det finns en nyare version";
 function isOutside(rect, point) {
   if (point.y < rect.top || point.y > rect.top + rect.height) {
