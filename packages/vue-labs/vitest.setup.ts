fe7d6ed9c822d4007ff32953f29ec3cb79df24f7
David Sveningsson <ext@sidvind.com>
2026-07-28 17:00:32 +0200

    build: use same warnHandler for vue-labs (refs SFKUI-6500)

--- packages/vue-labs/vitest.setup.ts
+++ packages/vue-labs/vitest.setup.ts
@@ -1,6 +1,10 @@
 import { ElementIdService } from "@fkui/logic";
-import { enableAutoUnmount } from "@vue/test-utils";
+import { config, enableAutoUnmount } from "@vue/test-utils";
 import { afterEach } from "vitest";
+
+config.global.config.warnHandler = (msg, _instance, trace) => {
+    throw new Error(`Vue warning: ${msg}\n${trace}`);
+};
 
 // Enable auto unmount for Vue Test Utils
 enableAutoUnmount(afterEach);
