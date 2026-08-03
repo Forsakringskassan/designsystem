77108d40ae462bc0a4e377631d53c8e1b295bb7b
David Sveningsson <ext@sidvind.com>
2026-08-03 03:22:20 +0200

    refactor(eslint): fix `@typescript-eslint/prefer-nullish-coalescing` (refs SFKUI-6500)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -171,7 +171,6 @@ export default [
         name: "technical-debt",
         files: ["**/*.spec.ts"],
         rules: {
-            "@typescript-eslint/prefer-nullish-coalescing": "off",
             "@typescript-eslint/require-await": "off",
             "@typescript-eslint/restrict-template-expressions": "off",
             "@typescript-eslint/unbound-method": "off",
