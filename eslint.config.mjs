46faf1b18bf14b73425e7982048c1aeed107ad0b
David Sveningsson <ext@sidvind.com>
2026-08-07 19:00:49 +0200

    refactor(eslint): fix `@typescript-eslint/no-unsafe-argument` (refs SFKUI-6500)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -170,7 +170,6 @@ export default [
         name: "technical-debt",
         files: ["**/*.cy.ts"],
         rules: {
-            "@typescript-eslint/no-unsafe-argument": "off",
             "@typescript-eslint/no-unsafe-assignment": "off",
             "@typescript-eslint/no-unsafe-call": "off",
             "@typescript-eslint/no-unsafe-return": "off",
