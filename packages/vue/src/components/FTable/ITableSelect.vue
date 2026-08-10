26472381cdf7ae679e86d16871eff109c7da2fce
dvitamin <dcoding@protonmail.com>
2026-08-10 13:00:25 +0000

    UNFIN

--- packages/vue/src/components/FTable/ITableSelect.vue
+++ packages/vue/src/components/FTable/ITableSelect.vue
@@ -181,7 +181,13 @@ async function onEditBlur(event: FocusEvent): Promise<void> {
 </script>
 
 <template>
-    <td class="table-ng__cell table-ng__cell--select" tabindex="-1" @keydown="onCellKeyDown" @click.stop="onCellClick">
+    <td
+        v-if="column.editable(row)"
+        class="table-ng__cell table-ng__cell--select"
+        tabindex="-1"
+        @keydown="onCellKeyDown"
+        @click.stop="onCellClick"
+    >
         <div v-show="!editing" class="table-ng__editable">
             <span class="table-ng__editable__text">{{ viewValue }}</span>
         </div>
@@ -215,5 +221,8 @@ async function onEditBlur(event: FocusEvent): Promise<void> {
             @select="selectDropdownOption"
             @close="onDropdownClose"
         ></i-combobox-dropdown>
+    </td>
+    <td v-else tabindex="-1" class="table-ng__cell table-ng__cell--static">
+        {{ column.selected(row) }}
     </td>
 </template>
