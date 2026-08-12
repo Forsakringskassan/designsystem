0834d74988835b7945c14ccf0c80c80a1cbb9a94
Wilson Mats (66136604) <mats.wilson@forsakringskassan.se>
2026-08-12 15:36:47 +0200

    test: remove snapshot test from fselectfield (refs SFKUI-8041)

--- packages/vue/src/components/FSelectField/FSelectField.spec.ts
+++ packages/vue/src/components/FSelectField/FSelectField.spec.ts
@@ -1,6 +1,10 @@
 import "html-validate/vitest";
 import { h } from "vue";
-import { type ValidatableHTMLElement, type ValidityEvent } from "@fkui/logic";
+import {
+    type ValidatableHTMLElement,
+    type ValidityEvent,
+    type ValidityMode,
+} from "@fkui/logic";
 import { config, flushPromises, mount, shallowMount } from "@vue/test-utils";
 import { describe, expect, it, vi } from "vitest";
 import FSelectField from "./FSelectField.vue";
@@ -9,9 +13,9 @@ config.global.stubs = {
     FIcon: true,
 };
 
-describe("snapshots", () => {
-    it("should match snapshot with label and select", () => {
-        expect.assertions(1);
+describe("rendering", () => {
+    it("should render with select and icon", () => {
+        expect.assertions(12);
         const wrapper = mount(FSelectField, {
             attrs: { id: "select-field" },
             slots: {
@@ -19,86 +23,196 @@ describe("snapshots", () => {
                     <option>Apple</option>
                     <option>Banana</option>
                 `,
+            },
+        });
+
+        expect(wrapper.get(".select-field").isVisible()).toBeTruthy();
+
+        // Make sure the select wrapper exists
+        const selectWrapper = ".select-field__icon-wrapper";
+        expect(wrapper.get(selectWrapper).isVisible()).toBeTruthy();
+        expect(wrapper.get(selectWrapper).classes()).toContain("i-width-sm-12");
+        expect(wrapper.get(selectWrapper).find("select").exists()).toBeTruthy();
+
+        expect(wrapper.get("select").isVisible()).toBeTruthy();
+        expect(wrapper.get("select").attributes("id")).toBe("select-field");
+        expect(wrapper.get("select").classes()).toContain(
+            "select-field__select",
+        );
+
+        const options = wrapper.get("select").findAll("option");
+        expect(options).toHaveLength(2);
+        expect(options[0].text()).toBe("Apple");
+        expect(options[1].text()).toBe("Banana");
+
+        expect(wrapper.find(".select-field__icon").isVisible()).toBeTruthy();
+        expect(wrapper.find(".select-field__icon").attributes("name")).toBe(
+            "arrow-down",
+        );
+    });
+
+    it("should render select without options", () => {
+        expect.assertions(3);
+        const wrapper = mount(FSelectField);
+
+        expect(wrapper.get("select").isVisible()).toBeTruthy();
+        expect(wrapper.get("select").classes()).toContain(
+            "select-field__select",
+        );
+        expect(wrapper.get("select").find("options").exists()).toBeFalsy();
+    });
+
+    it("should render with label", () => {
+        expect.assertions(6);
+        const wrapper = mount(FSelectField, {
+            attrs: { id: "select-field" },
+            slots: {
                 label: "Fruit",
             },
         });
-        expect(wrapper.element).toMatchSnapshot();
+
+        const labelWrapper = ".i-width-sm-12";
+        expect(wrapper.get(labelWrapper).isVisible()).toBeTruthy();
+        expect(wrapper.get(labelWrapper).find("label").exists()).toBeTruthy();
+        expect(wrapper.get("label").isVisible()).toBeTruthy();
+        expect(wrapper.get("label").text()).toBe("Fruit");
+        expect(wrapper.get("label").classes()).toContain("label");
+        expect(wrapper.get("label").attributes("for")).toBe("select-field");
     });
 
-    it("should match snapshot with label, select and error message", () => {
-        expect.assertions(1);
+    it("should render with error message", () => {
+        expect.assertions(6);
         const wrapper = mount(FSelectField, {
             attrs: { "aria-invalid": true, id: "select-field" },
             slots: {
-                default: /* HTML */ `
-                    <option>Apple</option>
-                    <option>Banana</option>
-                `,
                 label: "Fruit",
                 "error-message": "ERROR_MESSAGE",
             },
         });
-        expect(wrapper.element).toMatchSnapshot();
+
+        expect(wrapper.get("select").isVisible()).toBeTruthy();
+        expect(wrapper.get("select").attributes("aria-invalid")).toBe("true");
+
+        expect(
+            wrapper.get("label").find(".label__message--error").isVisible(),
+        ).toBeTruthy();
+        expect(wrapper.get(".label__message--error").text()).toContain(
+            "ERROR_MESSAGE",
+        );
+
+        const errorIconElement = wrapper
+            .get(".label__message--error")
+            .find("f-icon-stub");
+        expect(errorIconElement.isVisible()).toBeTruthy();
+        expect(errorIconElement.attributes("name")).toBe("error");
     });
 
-    it("should match snapshot with label, tooltip, description, error message and select", () => {
-        expect.assertions(1);
+    it("should render label with tooltip and description", () => {
+        expect.assertions(6);
         const wrapper = mount(FSelectField, {
             attrs: { id: "select-field" },
             slots: {
-                default: /* HTML */ `
-                    <option>Apple</option>
-                    <option>Banana</option>
-                `,
                 label: "Fruit",
                 description: "DESCRIPTION",
-                tooltip: "TOOLTIP",
-                "error-message": "ERROR_MESSAGE",
+                tooltip: /* HTML */ ` <div id="tooltip">TOOLTIP</div> `,
             },
         });
 
-        expect(wrapper.element).toMatchSnapshot();
+        const labelWrapper = wrapper.get(".i-width-sm-12");
+        expect(labelWrapper.findAll("label")).toHaveLength(2);
+
+        const tooltipDiv = labelWrapper.get("#tooltip");
+        expect(tooltipDiv.isVisible()).toBeTruthy();
+        expect(tooltipDiv.text()).toBe("TOOLTIP");
+
+        const descriptionLabel = labelWrapper.get(".label.sr-separator");
+        expect(descriptionLabel.isVisible()).toBeTruthy();
+        expect(descriptionLabel.text()).toBe("DESCRIPTION");
+        expect(descriptionLabel.attributes("for")).toBe("select-field");
+    });
+
+    function createCustomEvent(
+        target: ValidatableHTMLElement,
+        elementId: string,
+        validityMode: ValidityMode,
+        isValid: boolean,
+    ): CustomEvent {
+        return new CustomEvent<ValidityEvent>("validity", {
+            detail: {
+                target,
+                elementId,
+                isValid,
+                validityMode,
+                validationMessage: "Something went wrong.",
+                nativeEvent: "change",
+            },
+        });
+    }
+
+    it("should show errormessage if validityMode ERROR", async () => {
+        expect.assertions(2);
+
+        const id = "select-field";
+        const wrapper = mount(FSelectField, {
+            attrs: { id },
+            slots: {
+                label: "Fruit",
+            },
+        });
+
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-call -- technical debt */
+        wrapper.element.dispatchEvent(
+            createCustomEvent(
+                wrapper.element as ValidatableHTMLElement,
+                id,
+                "ERROR",
+                false,
+            ),
+        );
+
+        await flushPromises();
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-call -- false positive */
+        wrapper.vm.$forceUpdate();
+
+        const errorMessage = wrapper.get(".label__message--error");
+        expect(errorMessage.isVisible()).toBeTruthy();
+        expect(errorMessage.text()).toBe("Something went wrong.");
     });
 
     it.each`
         validityMode | isValid
         ${"VALID"}   | ${true}
-        ${"ERROR"}   | ${false}
-        ${"INTIAL"}  | ${true}
         ${"INITIAL"} | ${false}
+        ${"FOOBAR"}  | ${true}
     `(
-        "should match snapshot when validityMode is $validityMode and isValid is $isValid",
+        "should not show errormessage if validityMode $validityMode and isValid $isValid",
         async ({ validityMode, isValid }) => {
             expect.assertions(1);
+
+            const id = "select-field";
             const wrapper = mount(FSelectField, {
-                attrs: { id: "select-field" },
+                attrs: { id },
                 slots: {
-                    default: /* HTML */ `
-                        <option>Apple</option>
-                        <option>Banana</option>
-                    `,
                     label: "Fruit",
                 },
             });
 
             /* eslint-disable-next-line @typescript-eslint/no-unsafe-call -- technical debt */
             wrapper.element.dispatchEvent(
-                new CustomEvent<ValidityEvent>("validity", {
-                    detail: {
-                        target: wrapper.element as ValidatableHTMLElement,
-                        elementId: "elementId",
-                        isValid,
-                        validityMode,
-                        validationMessage: "Something went wrong.",
-                        nativeEvent: "change",
-                    },
-                }),
+                createCustomEvent(
+                    wrapper.element as ValidatableHTMLElement,
+                    id,
+                    validityMode as ValidityMode,
+                    isValid as boolean,
+                ),
             );
+
             await flushPromises();
             /* eslint-disable-next-line @typescript-eslint/no-unsafe-call -- false positive */
             wrapper.vm.$forceUpdate();
 
-            expect(wrapper.element).toMatchSnapshot();
+            const errorMessage = wrapper.find(".label__message--error");
+            expect(errorMessage.exists()).toBeFalsy();
         },
     );
 });
