3dea3421fb67a150e62d082a6058017833caa3fd
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2026-08-20 12:02:34 +0200

    refactor: entrypoint use button transition (refs SFKUI-6500)

---
+++ packages/vue/src/design-component-tests/Entrypoint/examples/EntrypointVisualTest.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { FIcon } from "@fkui/vue";
+</script>
+
+<template>
+    <div id="visual-wrapper">
+        <a class="entrypoint" href="#">
+            Lorem ipsum
+            <span class="sr-only"> dolor sit amet </span>
+            <f-icon name="arrow-right" class="entrypoint__icon"></f-icon>
+        </a>
+    </div>
+</template>
