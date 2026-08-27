9934aa13956759d20b5562bc6865bb195e0c6dd1
Ida Rosvall (66143551) <ida.rosvall@forsakringskassan.se>
2026-08-27 12:34:13 +0200

    feat: add tests for hiding cells in table (refs SFKUI-7876)

--- packages/vue/src/components/FTable/FTable.cy.ts
+++ packages/vue/src/components/FTable/FTable.cy.ts
@@ -4,7 +4,7 @@ import { FTablePageObject } from "../../cypress";
 import { useDatasetRef } from "../../utils";
 import { FValidationForm } from "../FValidationForm";
 import FTable from "./FTable.vue";
-import { defineTableColumns } from "./table-column";
+import { type TableColumn, defineTableColumns } from "./table-column";
 import FTableSortFilterExample from "./tests/FTableSortFilterExample.vue";
 import FTableTabstopExample from "./tests/FTableTabstopExample.vue";
 
@@ -2862,5 +2862,130 @@ describe("with wrapping text cell values", () => {
         );
 
         table.el().toMatchScreenshot();
+    });
+});
+
+describe("visible", () => {
+    interface Row {
+        static: string;
+        editable: string;
+        select: string;
+        rowheader: string;
+    }
+    let columns: Array<TableColumn<Row, keyof Row>>;
+
+    beforeEach(() => {
+        columns = defineTableColumns<Row>([
+            {
+                type: "text",
+                header: "Static text",
+                key: "static",
+                label: () => "text",
+            },
+            {
+                type: "text",
+                header: "Editable text",
+                key: "editable",
+                label: () => "text",
+            },
+            {
+                type: "button",
+                header: "button",
+                icon: "trashcan",
+                text: () => "button",
+            },
+            {
+                type: "anchor",
+                header: "anchor",
+                href: "",
+                text: () => "text",
+            },
+            {
+                type: "checkbox",
+                header: "checkbox",
+                label: () => "checkbox",
+            },
+            {
+                type: "rowheader",
+                key: "rowheader",
+                header: "row header",
+                text: () => "rowheader",
+            },
+            {
+                type: "menu",
+                header: "Menu",
+                text: () => "text",
+            },
+            {
+                type: "select",
+                header: "Select",
+                key: "select",
+                options: ["op1", "op2"],
+                label: () => "Select",
+            },
+        ]);
+    });
+
+    const rows = useDatasetRef<Row>([
+        {
+            static: "A2",
+            editable: "Attack",
+            select: "op1",
+            rowheader: "header",
+        },
+    ]);
+
+    it("should show cell content when visible is undefined", () => {
+        cy.mount(() => h(FTable<Row>, { rows: rows.value, columns }));
+
+        for (let i = 1; i <= columns.length; i++) {
+            table.cell({ row: 1, col: i }).should("not.be.empty");
+        }
+    });
+
+    it("should not show cell content when visible is false", () => {
+        columns = columns.map((column) => ({ ...column, visible: false }));
+
+        cy.mount(() => h(FTable<Row>, { rows: rows.value, columns }));
+
+        for (let i = 1; i <= columns.length; i++) {
+            table.cell({ row: 1, col: i }).should("be.empty");
+        }
+    });
+
+    it("should show cell content when visible is true", () => {
+        columns = columns.map((column) => ({ ...column, visible: true }));
+
+        cy.mount(() => h(FTable<Row>, { rows: rows.value, columns }));
+
+        for (let i = 1; i <= columns.length; i++) {
+            table.cell({ row: 1, col: i }).should("not.be.empty");
+        }
+    });
+
+    it("should not show cell content when visible is function that returns false", () => {
+        columns = columns.map((column) => ({
+            ...column,
+            visible: () => false,
+        }));
+
+        cy.mount(() => h(FTable<Row>, { rows: rows.value, columns }));
+
+        for (let i = 1; i <= columns.length; i++) {
+            table.cell({ row: 1, col: i }).should("be.empty");
+        }
+    });
+
+    it("should show cell content when visible is function that returns true", () => {
+        columns = columns.map((column) => ({
+            ...column,
+            visible: () => true,
+        }));
+
+        cy.mount(() => h(FTable<Row>, { rows: rows.value, columns }));
+
+        for (let i = 1; i <= columns.length; i++) {
+            table.cell({ row: 1, col: i }).should("not.be.empty");
+        }
     });
 });
