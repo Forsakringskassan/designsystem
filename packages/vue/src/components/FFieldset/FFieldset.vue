5edad6f4be9c23276741aaddb5ca15894a21afa6
Jacob Godin Forsberg <jacob.godin.forsberg@forsakringskassan.se>
2026-08-28 12:33:36 +0200

    docs: correct fieldset and radio API descriptions (refs SFKUI-8123)

--- packages/vue/src/components/FFieldset/FFieldset.vue
+++ packages/vue/src/components/FFieldset/FFieldset.vue
@@ -274,6 +274,11 @@ export default defineComponent({
             -->
             <slot name="description" :description-class :format-description-class></slot>
 
+            <!--
+                @slot Optional slot for displaying one or more error messages.
+                @binding {boolean} hasError Set to true when a validation error is present.
+                @binding {string} validationMessage Descriptive validation error message for the current error.
+            -->
             <slot name="error-message" v-bind="{ hasError, validationMessage: validity.validationMessage }">
                 <template v-if="hasError">
                     <span class="label__message label__message--error">
@@ -311,9 +316,9 @@ export default defineComponent({
                 <slot name="description" :description-class :format-description-class></slot>
 
                 <!--
-                    @slot Slot for displaying single or several error messages.
-                    @binding {boolean} hasError Set to true when a validation error is present
-                    @binding {string} validationMessage Descriptive validation error message for current error
+                    @slot Optional slot for displaying one or more error messages.
+                    @binding {boolean} hasError Set to true when a validation error is present.
+                    @binding {string} validationMessage Descriptive validation error message for the current error.
                 -->
                 <slot name="error-message" v-bind="{ hasError, validationMessage: validity.validationMessage }">
                     <template v-if="hasError">
