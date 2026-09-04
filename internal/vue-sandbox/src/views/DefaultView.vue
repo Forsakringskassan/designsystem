799cd8d3d34c19e4673a9a869675a89b6f5bb301
Ferm Stefan (66132752) <stefan.ferm@forsakringskassan.se>
2026-09-04 12:00:23 +0200

    fix: ´IComboboxDropdown´ add mousedown to li element (refs SFKUI-8129)

--- internal/vue-sandbox/src/views/DefaultView.vue
+++ internal/vue-sandbox/src/views/DefaultView.vue
@@ -7,8 +7,30 @@ export default defineComponent({
     components: { FTextField },
     data() {
         return {
-            awesomeModel: "",
+            valtLand: "",
+            valdStad: "",
+            land: ["Svalbard och Jan Mayen", "Swaziland", "Sverige"],
+            valtLandsStader: [] as string[],
+            kommuner: {
+                Sverige: ["Karlskrona", "Sundsvall", "Östersund", "Stockholm"],
+            },
         };
+    },
+    methods: {
+        onUpdate(value: string): void {
+            console.log("update", value);
+        },
+        onChangeCities(): void {
+            console.log("onChangeCities");
+        },
+        onUpdateCities(): void {
+            console.log("onUpdateCities");
+        },
+        async onChangeCountry(value: string): Promise<void> {
+            console.log("change :", value);
+            await new Promise((resolve) => setTimeout(resolve, 750));
+            this.valtLandsStader = this.kommuner[value as keyof typeof this.kommuner];
+        },
     },
 });
 </script>
@@ -25,14 +47,25 @@ export default defineComponent({
         </p>
         <hr />
         <f-text-field
-            id="awesome-field"
-            v-model="awesomeModel"
-            v-validation.required.maxLength="{ maxLength: { length: 10 } }"
+            v-model="valtLand"
+            v-validation.allowList="{ allowList: { list: land } }"
+            maxlength="100"
+            :options="land"
+            @change="onChangeCountry"
+            @update:model-value="onUpdate"
         >
-            <template #default> Inmatningsfält. </template>
-            <template #description="{ descriptionClass }">
-                <span :class="descriptionClass"> Lorem ipsum dolor sit amet. </span>
-            </template>
+            <template #default> Välj land </template>
+        </f-text-field>
+        <div>valtLand:{{ valtLand }}</div>
+        <f-text-field
+            v-model="valdStad"
+            v-validation.allowList="{ allowList: { list: valtLandsStader } }"
+            maxlength="100"
+            :options="valtLandsStader"
+            @change="onChangeCities"
+            @update:model-value="onUpdateCities"
+        >
+            <template #default> Välj stad </template>
         </f-text-field>
     </div>
 </template>
