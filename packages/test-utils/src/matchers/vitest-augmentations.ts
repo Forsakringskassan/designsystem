285e589a75d66325c64f1990087a5e0907fa8398
David Sveningsson <ext@sidvind.com>
2026-09-10 01:54:26 +0200

    chore(deps): update dependency vitest to v5

--- packages/test-utils/src/matchers/vitest-augmentations.ts
+++ packages/test-utils/src/matchers/vitest-augmentations.ts
@@ -2,9 +2,17 @@
 import "vitest";
 
 declare module "vitest" {
-    /* eslint-disable-next-line @typescript-eslint/no-explicit-any -- to match upstream */
-    interface Matchers<T = any> {
-        toHaveFocus: () => T;
+    /* eslint-disable-next-line @typescript-eslint/no-unused-vars -- to match upstream */
+    interface Assertion<R extends void | Promise<void> = void, T = unknown> {
+        toHaveFocus(): T;
+    }
+
+    interface Matchers<
+        /* eslint-disable-next-line @typescript-eslint/no-unused-vars -- to match upstream */
+        R extends void | Promise<void> = void | Promise<void>,
+        T = unknown,
+    > {
+        toHaveFocus(): T;
     }
 }
 export {};
