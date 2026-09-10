435a37e7caf7ad1b4cbc04493a95689378785774
irosvall <irosvall@users.noreply.github.com>
2026-09-10 08:49:29 +0000

    Deploy preview for PR 1661 🛫

---
+++ pr-preview/pr-1661/components/table-and-list/table/FTableWidthExample-8a5515.js
@@ -0,0 +1,101 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+import {
+  ErrorPlugin,
+  FErrorHandlingApp,
+  FormatPlugin,
+  TestPlugin,
+  TranslationPlugin,
+  ValidationPlugin,
+  setRunningContext
+} from "@fkui/vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(FErrorHandlingApp, { defaultComponent: rootComponent });
+    }
+  });
+  setRunningContext(app);
+  app.use(ErrorPlugin, {
+    captureWarnings: true,
+    logToConsole: true
+  });
+  app.use(ValidationPlugin);
+  app.use(TestPlugin);
+  app.use(TranslationPlugin);
+  app.use(FormatPlugin);
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:packages/vue/src/components/FTable/docs/FTableWidthExample.vue:FTableWidthExample-8a5515.js
+import { defineComponent as _defineComponent } from "vue";
+import { ref } from "vue";
+import { FTable, defineTableColumns, useDatasetRef } from "@fkui/vue";
+import { openBlock as _openBlock, createBlock as _createBlock } from "vue";
+var exampleComponent = /* @__PURE__ */ _defineComponent({
+  __name: "FTableWidthExample",
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const data = [
+      {
+        namn: "Apelsin",
+        land: "Spanien",
+        pris: "30"
+      },
+      {
+        namn: "Banan",
+        land: "Ecuador",
+        pris: "15"
+      },
+      {
+        namn: "\xC4pple",
+        land: "Sverige",
+        pris: "22"
+      }
+    ];
+    const selectedRows = ref([]);
+    const columns = defineTableColumns([
+      {
+        type: "text",
+        header: "Frukt",
+        key: "namn"
+      },
+      {
+        type: "text",
+        header: "Land",
+        key: "land",
+        width: "13rem"
+      },
+      {
+        type: "text:currency",
+        header: "Pris per kilo",
+        key: "pris",
+        size: "shrink"
+      }
+    ]);
+    const rows = useDatasetRef(data);
+    const __returned__ = { data, selectedRows, columns, rows, get FTable() {
+      return FTable;
+    } };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  return _openBlock(), _createBlock($setup["FTable"], {
+    "selected-rows": $setup.selectedRows,
+    "onUpdate:selectedRows": _cache[0] || (_cache[0] = ($event) => $setup.selectedRows = $event),
+    rows: $setup.rows,
+    columns: $setup.columns,
+    selectable: "multi"
+  }, null, 8, ["selected-rows", "rows", "columns"]);
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-8a5515"
+});
+export {
+  render
+};
