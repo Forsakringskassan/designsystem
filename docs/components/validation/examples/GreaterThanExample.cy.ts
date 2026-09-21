c9bd046ba710d281e78079af14137ab65814f481
Toresson Agneta (66142540) <agneta.toresson@forsakringskassan.se>
2026-09-21 11:52:50 +0200

    test: updated validation tests for FTextField(refs SFKUI-8214)

--- docs/components/validation/examples/GreaterThanExample.cy.ts
+++ docs/components/validation/examples/GreaterThanExample.cy.ts
@@ -7,15 +7,17 @@ it("should allow empty input", () => {
     cy.mount(GreaterThanExample);
     cy.get(input).clear();
     cy.get(input).blur();
+    // Set aria-invalid before dispatching the event
+    cy.get(input).should("have.attr", "aria-invalid", "false");
     cy.get(error).should("not.exist");
 });
 
-it("should allow negative value", () => {
+it("should not allow negative value", () => {
     cy.mount(GreaterThanExample);
     cy.get(input).clear();
     cy.get(input).type("-11");
     cy.get(input).blur();
-    cy.get(error).should("not.exist");
+    cy.get(error).and("be.visible");
 });
 
 it("should allow values greater than the limit", () => {
@@ -23,6 +25,8 @@ it("should allow values greater than the limit", () => {
     cy.get(input).clear();
     cy.get(input).type("0.124");
     cy.get(input).blur();
+    // Set aria-invalid before dispatching the event
+    cy.get(input).should("have.attr", "aria-invalid", "false");
     cy.get(error).should("not.exist");
 });
 
@@ -31,7 +35,7 @@ it("should show an error for values equal/below the limit", () => {
     cy.get(input).clear();
     cy.get(input).type("0.123");
     cy.get(input).blur();
-    cy.get(error).should("exist");
+    cy.get(error).should("exist").should("be.visible");
 });
 
 it("should show error when input contains letters", () => {
@@ -39,7 +43,7 @@ it("should show error when input contains letters", () => {
     cy.get(input).clear();
     cy.get(input).type("ÄÄ");
     cy.get(input).blur();
-    cy.get(error).should("exist");
+    cy.get(error).should("be.visible");
 });
 
 it("should show an error for non-leading minus sign", () => {
@@ -47,5 +51,5 @@ it("should show an error for non-leading minus sign", () => {
     cy.get(input).clear();
     cy.get(input).type("45-16");
     cy.get(input).blur();
-    cy.get(error).should("exist");
+    cy.get(error).should("be.visible");
 });
