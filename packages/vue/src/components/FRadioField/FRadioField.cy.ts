ebd92090d3165b574eb84d80750663673d4e0f81
Jacob Godin Forsberg <jacob.godin.forsberg@forsakringskassan.se>
2026-09-21 10:37:46 +0200

    fix: disabled text color for checkbox/radio details (refs SFKUI-6500)

--- packages/vue/src/components/FRadioField/FRadioField.cy.ts
+++ packages/vue/src/components/FRadioField/FRadioField.cy.ts
@@ -176,9 +176,9 @@ describe("FRadioField", () => {
         radioGroup.tooltip.iButton().should("exist");
     });
 
-    it("should not be able to select disabled fields, expanded should not exist", () => {
+    it("should prevent selection, omit expansion and use disabled text color for details", () => {
         const template = /* HTML */ `
-            <f-fieldset name="radio-name">
+            <f-fieldset name="radio-name" show-details="always">
                 <template #label> Label text </template>
                 <f-radio-field v-model="radioModel" :value="true">
                     Yes
@@ -187,7 +187,8 @@ describe("FRadioField", () => {
                     No
                 </f-radio-field>
                 <f-radio-field v-model="radioModel" :value="false" disabled>
-                    Disabled 1
+                    <template #default> Disabled 1 </template>
+                    <template #details> Disabled details </template>
                 </f-radio-field>
                 <f-radio-field v-model="radioModel" :value="false" disabled>
                     Disabled 2
@@ -200,6 +201,16 @@ describe("FRadioField", () => {
             .radioButton(radioField.disabled1)
             .isSelected()
             .should("be.false");
+        radioGroup
+            .radioButton(radioField.disabled1)
+            .label()
+            .then(($label) => {
+                const labelColor = getComputedStyle($label.get(0)).color;
+                radioGroup
+                    .radioButton(radioField.disabled1)
+                    .details()
+                    .should("have.css", "color", labelColor);
+            });
 
         radioGroup.radioButton(radioField.disabled2).select();
         radioGroup
